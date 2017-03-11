85a8b694567540408395644a6e5878547776669e
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-03-11 10:12:22 -0700

    Bug fix

--- Catfish/Models/Fields/ImageTile.cs
+++ Catfish/Models/Fields/ImageTile.cs
@@ -10,19 +10,5 @@ namespace Catfish.Models.Fields
 {
     public class ImageTile : Tile<ImageRegion, string>
     {
-        //[Display(Name = "Image")]
-        //public ImageRegion Image { get; set; }
-
-        //[Display(Name = "Caption")]
-        //public string CaptionHeading { get; set; }
-
-        //[Display(Name = "Description")]
-        //public string CaptionDescription { get; set; }
-
-        //[Display(Name = "Link Text")]
-        //public string ReadMoreLinkText { get; set; }
-
-        //[Display(Name = "Link")]
-        //public string ReadMoreLink { get; set; }
     }
 }
