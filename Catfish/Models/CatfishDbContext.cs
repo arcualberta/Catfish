043924031ca21e1e91a88d2cb4d0ccb5652c6544
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-03-15 17:07:09 -0600

    Enabled code-first migrations.

---
+++ Catfish/Models/CatfishDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Catfish.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Catfish.Models
+{
+    public class CatfishDbContext : DbContext
+    {
+        public CatfishDbContext()
+            :base("piranha")
+        {
+
+        }
+
+      //  public DbSet<Collection> Collections { get; set; }
+
+    }
+}
