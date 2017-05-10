0b8f279061b0de112f916705602d5ada7cf3f105
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-05-10 16:39:25 -0600

    MetadataController.FieldTypes now returns field definition details, including names and types of FieldDefinition properties.

---
+++ Catfish/Areas/Manager/Models/FieldDefinitionViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Catfish.Areas.Manager.Models
+{
+    public class FieldDefinitionViewModel
+    {
+        public ICollection<FieldPropertyViewModel> Properties { get; set; }
+
+        public FieldDefinitionViewModel(Type filedType)
+        {
+            Properties = new List<FieldPropertyViewModel>();
+
+            PropertyInfo[] properties = filedType.GetProperties();
+            foreach(PropertyInfo info in properties)
+            {
+                DataTypeAttribute dataTypeAtt = info.GetCustomAttribute<DataTypeAttribute>(true);
+
+                FieldPropertyViewModel fi = new FieldPropertyViewModel()
+                {
+                    Name = info.Name,
+                    DataType = info.PropertyType.ToString(),
+                    DisplayType = dataTypeAtt == null ? "" : dataTypeAtt.DataType.ToString()
+                };
+
+                Properties.Add(fi);
+            }
+        }
+    }
+}
