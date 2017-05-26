86c38c01af516382b8c1bac03db00955c7122795
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-05-26 17:38:25 -0600

    Removed redundant code from previous implementation of Field Type API call.

--- Catfish/Areas/Manager/Models/FieldPropertyViewModel.cs
+++ Catfish/Areas/Manager/Models/FieldPropertyViewModel.cs
@@ -5,11 +5,11 @@ using System.Web;
 
 namespace Catfish.Areas.Manager.Models
 {
-    public class FieldPropertyViewModel
-    {
-        public string Name { get; set; }
-        public string DataType { get; set; }
-        public string DisplayType { get; set; }
-        public bool IsRequired { get; set; }
-    }
+    //public class FieldPropertyViewModel
+    //{
+    //    public string Name { get; set; }
+    //    public string DataType { get; set; }
+    //    public string DisplayType { get; set; }
+    //    public bool IsRequired { get; set; }
+    //}
 }
