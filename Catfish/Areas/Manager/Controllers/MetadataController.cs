39e006149c9fe9f143268d8f4b2a37c3255c4463
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-06-05 10:00:38 -0600

    Fixed model binding for derived types of metadata fields.

--- Catfish/Areas/Manager/Controllers/MetadataController.cs
+++ Catfish/Areas/Manager/Controllers/MetadataController.cs
@@ -126,7 +126,11 @@ namespace Catfish.Areas.Manager.Controllers
             if(ModelState.IsValid)
             {
                 if (model.Id > 0)
+                {
                     Db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    foreach (var field in model.Fields)
+                        Db.Entry(field).State = System.Data.Entity.EntityState.Modified;
+                }
                 else
                     Db.MetadataSets.Add(model);
 
--- Catfish/Areas/Manager/ModelBinders/MetadataFieldDefinitionBinder.cs
+++ Catfish/Areas/Manager/ModelBinders/MetadataFieldDefinitionBinder.cs
@@ -6,22 +6,38 @@ namespace Catfish.Areas.Manager.ModelBinders
 {
     public class MetadataFieldDefinitionBinder: DefaultModelBinder
     {
-        protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        ////protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        ////{
+        ////    try
+        ////    {
+        ////        var key = bindingContext.ModelName + ".ModelType";
+        ////        var typeStr = bindingContext.ValueProvider.GetValue(key).AttemptedValue;
+        ////        var type = Type.GetType(typeStr);
+        ////        object obj = Activator.CreateInstance(type);
+        ////        return obj;
+        ////    }
+        ////    catch (Exception)
+        ////    {
+        ////        return base.CreateModel(controllerContext, bindingContext, modelType);
+        ////    }
+        ////}
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             try
             {
                 var key = bindingContext.ModelName + ".ModelType";
                 var typeStr = bindingContext.ValueProvider.GetValue(key).AttemptedValue;
                 var type = Type.GetType(typeStr);
-                object obj = Activator.CreateInstance(type);
-                return obj;
+                bindingContext.ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(null, type);
             }
             catch (Exception)
             {
-                return base.CreateModel(controllerContext, bindingContext, modelType);
-            }
 
+            }
+            return base.BindModel(controllerContext, bindingContext);
         }
+
 
     }
 }
