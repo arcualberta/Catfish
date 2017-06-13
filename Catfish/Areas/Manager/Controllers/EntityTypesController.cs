a309a5459e9bee3ade73656178bf17ba736a8c55
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2017-06-13 12:33:00 -0600

    Removed circular refernces using Json.NET
    The circular references are now being handled with properly without removing them.

--- Catfish/Areas/Manager/Controllers/EntityTypesController.cs
+++ Catfish/Areas/Manager/Controllers/EntityTypesController.cs
@@ -10,6 +10,8 @@ using Catfish.Core.Models;
 using Catfish.Core.Services;
 using Catfish.Core.Models.Metadata;
 using System.Web.Script.Serialization;
+using System.Web.Helpers;
+using Newtonsoft.Json;
 
 namespace Catfish.Areas.Manager.Controllers
 {
@@ -116,8 +118,34 @@ namespace Catfish.Areas.Manager.Controllers
             //var metadataSets = this.MetadataService.
             var metadataSets = MetadataService.GetMetadataSets().ToList();
             //var metadataSetViewModels = metadataSets.Select(ft => new MetadataSetDefinitionViewModel(ft));
-            return new JavaScriptSerializer().Serialize(metadataSets);
+            //return new JavaScriptSerializer().Serialize(metadataSets);
+            //var test = Json(metadataSets).Data;
+            //var test = Json(metadataSets);
+            //test.RecursionLimit = 0;
+            //return test.ToString();
+
+            //var test = new JavaScriptSerializer();
+            //test.RecursionLimit = 2;
+            ////st.
+            ////test.
+
+            //return test.Serialize(metadataSets);
+
+            //var test = Json(metadataSets);
+            //test.Data(metadataSets
+            //return test.
+
+            //string result = JsonConvert.SerializeObject(metadataSets);
+
+            //return result;
             //return metadataSets;
+
+            string test = JsonConvert.SerializeObject(metadataSets, Formatting.Indented,
+                new JsonSerializerSettings {
+                    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                });
+
+            return test;
 
         }
     }
