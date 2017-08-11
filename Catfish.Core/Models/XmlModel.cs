721310726f36db0884bb92a306a4b71ad17a9c8b
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-08-10 18:20:45 -0600

    Work in progress of XML data-model based class implementation.

--- Catfish.Core/Models/XmlModel.cs
+++ Catfish.Core/Models/XmlModel.cs
@@ -1,5 +1,6 @@
 ﻿using Catfish.Core.Models.Attributes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,11 +9,172 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Catfish.Core.Models
 {
     public class XmlModel
     {
+       public XElement Data { get; protected set; }
+
+        public string DefaultLanguage { get; set; }
+
+        public XmlModel()
+        {
+        }
+
+        public XmlModel(XElement ele, string lang = "en")
+        {
+            Data = ele;
+            DefaultLanguage = lang;
+        }
+
+        public string GetName(string lang = null)
+        {
+            return GetChildText("name", Data, Lang(lang));
+        }
+        public void SetName(string val, string lang = null)
+        {
+            SetChildText("name", val, Data, Lang(lang));
+        }
+
+        public string GetDescription(string lang = null)
+        {
+            return GetChildText("description", Data, Lang(lang));
+        }
+
+        public void SetDescription(string val, string lang = null)
+        {
+            SetChildText("description", val, Data, Lang(lang));
+        }
+
+        public string GetHelp(string lang = null)
+        {
+            return GetChildText("help", Data, Lang(lang));
+        }
+
+        public void SetHelp(string val, string lang = null)
+        {
+            SetChildText("help", val, Data, Lang(lang));
+        }
+
+        public IEnumerable<string> GetValues(string lang = null)
+        {
+            var matches = GetMatches("value", Data, Lang(lang));
+            return matches.Select(m => (m.FirstNode as XText).Value);
+        }
+
+        public void SetValues(IEnumerable<string> values, string lang = null)
+        {
+            SetChildText("value", values, Data, Lang(lang));
+        }
+
+        protected string Lang(string lang)
+        {
+            return lang == null ? DefaultLanguage : lang;
+        }
+
+        protected string GetChildText(string childTagName, XElement ele, string lang)
+        {
+            var matches = GetMatches(childTagName, ele, lang);
+            return matches.Any() ? (matches.First().FirstNode as XText).Value : null;
+        }
+
+        /// <summary>
+        /// Checks if there is a child element exists with the given tag name that contains a text element with xml:lang set to the 
+        /// given "lang". If exists, then selects the first text element that matches the criteria and sets its content
+        /// to the given value. If no such text element exists, then creates a text element, sets its content value, and inserts it 
+        /// as a child of the given element.
+        /// </summary>
+        /// <param name="childTagName"></param>
+        /// <param name="val"></param>
+        /// <param name="ele"></param>
+        /// <param name="lang"></param>
+        protected void SetChildText(string childTagName, string val, XElement ele, string lang)
+        {
+            var matches = GetMatches(childTagName, ele, lang);
+
+            XElement textEelemnt = null;
+            if (matches.Any())
+                textEelemnt = matches.First();
+            else
+            {
+                textEelemnt = new XElement("text", new XAttribute(XNamespace.Xml + "lang", lang));
+                ele.Add(textEelemnt);
+            }
+
+            textEelemnt.Value = val;
+        }
+        /// <summary>
+        /// Finds the child element specified by the given tag name and then removes all text elements with
+        /// the given lang un it and adds a new set of text elements with the given values.
+        /// </summary>
+        /// <param name="childTagName"></param>
+        /// <param name="values"></param>
+        /// <param name="ele"></param>
+        /// <param name="lang"></param>
+        protected void SetChildText(string childTagName, IEnumerable<string> values, XElement ele, string lang)
+        {
+            RemoveChildTextElements(childTagName, ele, lang);
+            InsertChildText(childTagName, values, ele, lang);
+        }
+
+        /// <summary>
+        /// Inserts the given list of values to set of new text elements inside the specified child element.
+        /// </summary>
+        /// <param name="childTagName"></param>
+        /// <param name="values"></param>
+        /// <param name="ele"></param>
+        /// <param name="lang"></param>
+        protected void InsertChildText(string childTagName, IEnumerable<string> values, XElement ele, string lang)
+        {
+            XElement parent = ele.Elements(childTagName).First();
+            foreach (string val in values)
+            {
+                XElement textEelemnt = new XElement("text", new XAttribute(XNamespace.Xml + "lang", lang));
+                textEelemnt.Value = val;
+                parent.Add(textEelemnt);
+            }
+        }
+
+        /// <summary>
+        /// Removes all the text elements that matches the specified language form the child element specified by the
+        /// given tagName inside the given element
+        /// </summary>
+        /// <param name="childTagName"></param>
+        /// <param name="ele"></param>
+        /// <param name="lang"></param>
+        protected void RemoveChildTextElements(string childTagName, XElement ele, string lang)
+        {
+            var matches = GetMatches(childTagName, ele, lang).ToList();
+            foreach (var text in matches)
+                text.Remove();
+        }
+
+        protected IEnumerable<XElement> GetMatches(string childTagName, XElement ele, string lang)
+        {
+            var xpath = "/" + childTagName + "/text[@xml:lang='" + lang + "']";
+
+            var matches = ((IEnumerable)ele.XPathEvaluate(xpath, NamespaceManager)).Cast<XElement>();
+
+            return matches;
+        }
+
+        protected XmlNamespaceManager NamespaceManager
+        {
+            get
+            {
+                if (mXmlNamespaceManager == null)
+                {
+                    mXmlNamespaceManager = new XmlNamespaceManager(new NameTable());
+                    mXmlNamespaceManager.AddNamespace("xml", XNamespace.Xml.NamespaceName);
+                }
+
+                return mXmlNamespaceManager;
+            }
+        }
+        private XmlNamespaceManager mXmlNamespaceManager;
+
         [Required]
         [TypeLabel("String")]
         public string Name { get; set; }
@@ -20,6 +182,7 @@ namespace Catfish.Core.Models
         [DataType(DataType.MultilineText)]
         [TypeLabel("String")]
         public string Description { get; set; }
+
 
         public virtual XElement ToXml()
         {
---
+++ Catfish.Tests/XmlModelTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Catfish.Core.Models.Metadata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using Catfish.Core.Models;
+using System.Collections.Generic;
+
+namespace Catfish.Tests
+{
+    [TestClass]
+    public class XmlModelTest
+    {
+        public static string GetSampleDataFilePathName(string fileName)
+        {
+            var dir = Directory.GetCurrentDirectory();
+            dir = dir.Substring(0, dir.IndexOf("Catfish.Test"));
+            dir = Path.Combine(dir, "XmlTemplates");
+            return Path.Combine(dir, fileName);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var option_filed_types = typeof(OptionsField).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(OptionsField))).ToList();
+
+            var n = option_filed_types.Count;
+        }
+
+        [TestMethod]
+        public void XPathTest()
+        {
+            string path = GetSampleDataFilePathName("MetadataSet.xml");
+            Assert.IsTrue(File.Exists(path));
+
+            XElement root = XElement.Load(path);
+            Assert.IsNotNull(root);
+
+            XmlModel model = new XmlModel(root);
+
+            var name = model.GetName();
+            Assert.AreEqual("Dublin Core", name);
+
+            var desc = model.GetDescription("fr");
+            Assert.AreEqual("Il s'agit du jeu de métadonnées Dublin Core.", desc);
+
+            var vals = model.GetValues();
+
+            var x = 0;
+        }
+
+        [TestMethod]
+        public void EditTest()
+        {
+            string path = GetSampleDataFilePathName("MetadataSet.xml");
+            Assert.IsTrue(File.Exists(path));
+
+            XElement root = XElement.Load(path);
+            Assert.IsNotNull(root);
+
+            XmlModel model = new XmlModel(root);
+
+            var name = "Test Name 123";
+            model.SetName(name);
+            var name2 = model.GetName();
+            Assert.AreEqual(name, name2);
+
+            model.SetValues(new List<string>() { "a", "b", "c" }); //Default lang "en"
+
+            List<string> evals1 = new List<string>() { "value 1", "value 2", "value 3" };
+            model.SetValues(evals1); ////Default lang "en". This should replace the first set of values "a", "b" and "c"
+
+            List<string> fvals1 = new List<string>() { "valeur 1", "valeur 2", "valeur 3" };
+            model.SetValues(fvals1, "fr");//This should not replace evals1 because this is not the default lang "en"
+
+            IEnumerable<string> evals2 = model.GetValues("en");
+            foreach (var v in evals2)
+                Assert.IsTrue(evals1.Contains(v));
+            foreach (var v in evals1)
+                Assert.IsTrue(evals2.Contains(v));
+
+            IEnumerable<string> fvals2 = model.GetValues("fr");
+            foreach (var v in fvals2)
+                Assert.IsTrue(fvals1.Contains(v));
+            foreach (var v in fvals1)
+                Assert.IsTrue(fvals2.Contains(v));
+
+
+        }
+
+    }
+}
