9ce938717b075a11708f699f121938929d36d860
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-08-28 14:47:10 -0600

    Created initial work for the Collection Controller.

---
+++ Catfish/Models/EntityViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Catfish.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Catfish.Models
+{
+    public class EntityViewModel
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+
+        public EntityViewModel(Entity src)
+        {
+            Id = src.Id;
+            Label = src.GetName();
+        }
+    }
+}
