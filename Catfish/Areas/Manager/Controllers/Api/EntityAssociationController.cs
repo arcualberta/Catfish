c93a14de867451c74491928eca64181c0c8b5204
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-10-30 15:09:20 -0600

    Implemented related object associations between items to items.

--- Catfish/Areas/Manager/Controllers/Api/EntityAssociationController.cs
+++ Catfish/Areas/Manager/Controllers/Api/EntityAssociationController.cs
@@ -23,67 +23,89 @@ namespace Catfish.Areas.Manager.Controllers
         }
 
         [HttpPost]
-        public JsonResult Save(EntityContentViewModel vm)
+        public JsonResult UpdateChildren(EntityContentViewModel vm)
         {
-            if (ModelState.IsValid)
+            try
             {
-                Entity model;
-                if (vm.Id > 0)
+                if (!ModelState.IsValid)
+                    throw new Exception("Model validation failed");
+
+                if (vm.Id == 0)
+                    throw new Exception("Parent model ID mnot specified");
+
+                Aggregation model = Db.XmlModels.Where(x => x.Id == vm.Id).FirstOrDefault() as Aggregation;
+                if (model == null)
+                    throw new Exception("Specified parent entity of type Aggregation not found");
+
+                //Associating children
+                foreach (var c in vm.ChildEntityList)
                 {
-                    model = Db.XmlModels.Where(x => x.Id == vm.Id).FirstOrDefault() as Entity;
-                    if (model == null)
-                        return Json(vm.Error("Specified entity not found"));
-                    else
-                    {
-                        if (typeof(Collection).IsAssignableFrom(model.GetType()))
-                        {
-                            Collection parent = model as Collection;
-                            foreach (var c in vm.ChildEntityList)
-                            {
-                                Aggregation c2 = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Aggregation;
-                                parent.ChildMembers.Add(c2);
-                            }
+                    Aggregation child = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Aggregation;
+                    if (child == null)
+                        throw new Exception("Id=" + c.Id + ": Specified child entity of type Aggregation not found");
 
-                            foreach (var c in vm.RemovalPendingChildEntities)
-                            {
-                                Aggregation c2 = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Aggregation;
-                                parent.ChildMembers.Remove(c2);
-                            }
-                        }
-                        else if (typeof(Item).IsAssignableFrom(model.GetType()))
-                        {
-
-                        }
-                        else
-                            throw new Exception("Save method is not defined for the model type " + model.GetType().ToString());
-
-                        ////vm.UpdateDataModel(model, Db);
-                        Db.Entry(model).State = System.Data.Entity.EntityState.Modified;
-                    }
+                    model.ChildMembers.Add(child);
                 }
 
+                //Removing deleted children
+                foreach (var c in vm.RemovalPendingChildEntities)
+                {
+                    Aggregation child = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Aggregation;
+                    model.ChildMembers.Remove(child);
+                }
+
+                Db.Entry(model).State = System.Data.Entity.EntityState.Modified;
                 Db.SaveChanges();
                 vm.Status = KoBaseViewModel.eStatus.Success;
+                return Json(vm);
             }
-            else
-                return Json(vm.Error("Model validation failed"));
-
-            return Json(vm);
+            catch (Exception ex)
+            {
+                return Json(vm.Error("UpdateChildren Error: " + ex.Message));
+            }
         }
 
-        ////public JsonResult AddSelection(EntityContentViewModel entityVM)
-        ////{
-        ////    entityVM.Associate();
-        ////    //ViewBag.ChildItems = entityVM;
-        ////    return Json(entityVM);
-        ////}
+        [HttpPost]
+        public JsonResult UpdateRelatedItems(EntityContentViewModel vm)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    throw new Exception("Model validation failed");
 
-        ////public JsonResult RemoveItem(EntityContentViewModel entityVM, int itemIndex)
-        ////{
-        ////    entityVM.ChildEntityList.RemoveAt(itemIndex);
-        ////    //ViewBag.ChildItems = entityVM;
-        ////    return Json(entityVM);
-        ////}
+                if (vm.Id == 0)
+                    throw new Exception("Parent model ID mnot specified");
 
+                Item model = Db.XmlModels.Where(x => x.Id == vm.Id).FirstOrDefault() as Item;
+                if (model == null)
+                    throw new Exception("Specified parent Item not found");
+
+                //Associating children
+                foreach (var c in vm.ChildEntityList)
+                {
+                    Item child = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Item;
+                    if (child == null)
+                        throw new Exception("Id=" + c.Id + ": Specified related item not found");
+
+                    model.ChildRelations.Add(child);
+                }
+
+                //Removing deleted children
+                foreach (var c in vm.RemovalPendingChildEntities)
+                {
+                    Item child = Db.XmlModels.Where(x => x.Id == c.Id).FirstOrDefault() as Item;
+                    model.ChildRelations.Remove(child);
+                }
+
+                Db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                Db.SaveChanges();
+                vm.Status = KoBaseViewModel.eStatus.Success;
+                return Json(vm);
+            }
+            catch (Exception ex)
+            {
+                return Json(vm.Error("UpdateRelatedItems Error: " + ex.Message));
+            }
+        }
     }
 }
