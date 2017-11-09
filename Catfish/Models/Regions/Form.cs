fecd29b6d67f2dad9220e006950e6807ae5dc158
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-11-09 00:50:46 -0700

    Working on implementing form page.

---
+++ Catfish/Models/Regions/Form.cs
@@ -0,0 +1,23 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Web;
+
+namespace Catfish.Models.Regions
+{
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "FormExtension")]
+    [ExportMetadata("Name", "Form")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+    public class Form: CatfishRegion
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int EntityTypeId { get; set; }
+        public string Pages { get; set; }
+        public int CollectionId { get; set; }
+    }
+}
