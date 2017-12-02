edccde08a89ae58859ed0ddb0cd7d8c5acacd706
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-12-01 17:29:35 -0700

    Working on uploading files through non-manager forms. Created views and JavaScript functions. Uploading however fails because the  CMS routing blocks direct access to the FormSubmissionController.

---
+++ Catfish.Core/Models/Forms/Attachment.cs
@@ -0,0 +1,23 @@
+﻿using Catfish.Core.Models.Attributes;
+using Catfish.Core.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Core.Models.Forms
+{
+    [TypeLabel("Attachment Field")]
+    public class Attachment: FormField
+    {
+        [NotMapped]
+        public List<DataFile> Files { get; set; }
+
+        public Attachment()
+        {
+            Files = new List<DataFile>();
+        }
+    }
+}
