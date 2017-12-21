bf48759d1c230da49cd1bff1043cfebe0298eb88
Kamal Ranaweera <ranaweer@ualberta.ca>
2017-12-21 14:45:42 -0700

    Creating BulletinBoard region.

---
+++ Catfish/Controllers/Api/StatsController.cs
@@ -0,0 +1,22 @@
+﻿using Catfish.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catfish.Controllers.Api
+{
+    public class StatsController : Controller
+    {
+        // GET: Stats
+        public JsonResult Index()
+        {
+            CatfishDbContext db = new CatfishDbContext();
+            int items = db.XmlModels.Where(x => x is Item).Count();
+            int collections = db.XmlModels.Where(x => x is Collection).Count();
+            Object stats = new { Items = items, Collections = collections };
+            return Json(stats, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Catfish/Models/Regions/BulletinBoard.cs
@@ -0,0 +1,33 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Catfish.Models.Regions
+{
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "CarouselExtension")]
+    [ExportMetadata("Name", "Carousel")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+    public class BulletinBoard: CatfishRegion
+    {
+        [Display(Name ="Collection Id")]
+        public int CollectionId { get; set; }
+
+        [Display(Name ="Item Count")]
+        public int ItemCount { get; set; }
+
+        [Display(Name = "Select Randomly")]
+        public bool SelectAtRandom { get; set; }
+
+        [Display(Name = "Position at Random")]
+        public bool PositionAtRandom { get; set; }
+      
+        [Display(Name = "Refresh Interval")]
+        public int RefreshInterval { get; set; }
+    }
+}
