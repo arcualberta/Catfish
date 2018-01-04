2e0eb4ceac8f38f135b25a8879a7f4fada33bcf7
Mark McKellar <mpm1@ualberta.ca>
2018-01-04 14:31:46 -0700

    Updated to .Net 4.6 in order to support SolrNet

---
+++ Catfish.Core/Helpers/XmlLinqExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Core.Helpers
+{
+    public static class XmlLinqExtensions
+    {
+        public static IQueryable<TSource> FromSolr<TSource>(this DbSet<TSource> set, string q) where TSource : XmlModel
+        {
+            if (SolrService.IsInitialized)
+            {
+                
+                //var solr = ServiceLocator.Current.GetInstance<ISolrOperations<SolrIndex>>();
+            }
+            else
+            {
+                throw new InvalidOperationException("SolrService has not been initialized correctly. Please make sure that you have defined the app parameter SolrServer in your web.config.");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Catfish.Core/Services/SolrService.cs
@@ -0,0 +1,41 @@
+﻿using Catfish.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Core.Services
+{
+    class SolrService
+    {
+        public static bool IsInitialized { get; private set; }
+
+        protected CatfishDbContext Db { get; set; }
+
+
+
+        public static void Init()
+        {
+            IsInitialized = false;
+
+            string server = System.Configuration.ConfigurationManager.AppSettings["SolrServer"];
+            if (!string.IsNullOrEmpty(server))
+            {
+
+                //TODO: Should we update the database here or have it in an external cron job
+            }
+        }
+
+        public SolrService(CatfishDbContext db)
+        {
+            Db = db;
+        }
+    }
+
+    public class SolrIndex
+    {
+        public int Id { get; set; }
+
+    }
+}
