b56587fc5b34b70170e6956d47bd443d97609f6c
mruaini <mruaini@ualberta.ca>
2018-01-25 13:52:46 -0700

    add Entity Group and Entity Group User

--- Catfish.Core/Models/CatfishDbContext.cs
+++ Catfish.Core/Models/CatfishDbContext.cs
@@ -75,6 +75,9 @@ namespace Catfish.Core.Models
                     //t.MapRightKey("EntityTypesId");
                     t.ToTable("EntityTypeHasMetadataSets");
                 });
+
+            //define composite primary key for EntityGroupUser -- Jan 24 2018
+            builder.Entity<EntityGroupUser>().HasKey(t => new { t.EntityGroupId, t.UserId });
         }
 
         public DbSet<XmlModel> XmlModels { get; set; }
@@ -98,5 +101,8 @@ namespace Catfish.Core.Models
         ////public DbSet<SimpleField> MetadataFields { get; set; }
 
         ////public DbSet<FieldValue> FieldValues { get; set; }
+
+        public DbSet<EntityGroup> EntityGroups { get; set; }
+        public DbSet<EntityGroupUser> EntityGroupUsers { get; set; }
     }
 }
---
+++ Catfish.Core/Services/EntityGroupService.cs
@@ -0,0 +1,49 @@
+﻿
+using Catfish.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Core.Services
+{
+    public class EntityGroupService :EntityService
+    {
+        public EntityGroupService(CatfishDbContext db) : base(db) { }
+
+        public IQueryable GetEntityGroupList()
+        {
+            return Db.Entities;
+        }
+
+        public EntityGroup GetEntityGroup(string id)
+        {
+            EntityGroup entityGroup = new EntityGroup();
+
+            if(!string.IsNullOrEmpty(id))
+            {
+                entityGroup = Db.EntityGroups.Where(eg => eg.Id == Guid.Parse(id)).FirstOrDefault();
+            }
+
+            return entityGroup;
+        }
+        public EntityGroup EditEntityGroup(EntityGroup entityGroup)
+        {
+            
+            if (!string.IsNullOrEmpty(entityGroup.Id.ToString()))
+            {
+                //edit existing entityGroup
+                Db.Entry(entityGroup).State = System.Data.Entity.EntityState.Modified;
+            }
+            else
+            {
+                //add new entity group
+                entityGroup.Id = new Guid();
+                Db.EntityGroups.Add(entityGroup);
+            }
+
+            return entityGroup;
+        }
+    }
+}
