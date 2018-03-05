3c7c446b7777738a1890d5f1417ff4e076f8426c
Mark McKellar <mpm1@ualberta.ca>
2018-03-05 09:15:58 -0700

    Started FormContainer Tests

---
+++ Catfish.Tests/Views/Regions/FormContainerTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Tests.Views.Regions
+{
+    [TestFixture(typeof(ChromeDriver))]
+    public class FormContainerTest<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        private IWebDriver Driver;
+        private string ManagerUrl;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            this.Driver = new TWebDriver();
+            this.ManagerUrl = ConfigurationManager.AppSettings["ServerUrl"] + "manager";
+            VerifyFormExists();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.Driver.Close();
+        }
+
+        private void Login()
+        {
+            this.Driver.Navigate().GoToUrl(ManagerUrl);
+            this.Driver.FindElement(By.Id("login")).SendKeys(ConfigurationManager.AppSettings["AdminLogin"]);
+            this.Driver.FindElement(By.Name("password")).SendKeys(ConfigurationManager.AppSettings["AdminPassword"]);
+            this.Driver.FindElement(By.TagName("button")).Click();
+        }
+
+        private void VerifyFormExists()
+        {
+
+        }
+    }
+}
