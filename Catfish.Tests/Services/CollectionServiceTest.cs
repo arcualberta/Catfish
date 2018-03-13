f82604b8ba0d374c65bf0dee81e2e4539e9918ce
Mark McKellar <mpm1@ualberta.ca>
2018-03-13 11:38:06 -0600

    Fixed Test build error

---
+++ Catfish.Tests/Services/CollectionServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Services;
+using Catfish.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Tests.Services
+{
+    [TestClass]
+    public class CollectionServiceTest
+    {
+        private void CreateCollection(string name, string description, IEnumerable<MetadataSet> sets)
+        {
+
+        }
+
+        [TestMethod]
+        public void TestGetCollection()
+        {
+            DatabaseHelper Dh = new DatabaseHelper(true);
+            CollectionService Cs = new CollectionService(Dh.Db);
+
+            // Create the needed Collection
+
+
+            // Test the results
+        }
+    }
+}
