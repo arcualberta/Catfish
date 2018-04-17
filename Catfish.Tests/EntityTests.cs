7852164b21edab048dd67b3f09d2b7b243e758b5
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2018-04-17 13:28:14 -0600

    WIP Testing Entity for AccessGroups

---
+++ Catfish.Tests/EntityTests.cs
@@ -0,0 +1,68 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Models.Access;
+using Catfish.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Catfish.Tests
+{
+    [TestClass]
+    public class EntityTests
+    {
+
+        MockEntity MockEntity;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            MockEntity = new MockEntity();
+        }
+
+        [TestMethod]
+        public void CanUseAccessGroups()
+        {
+            AccessGroup accessGroup1 = new AccessGroup();
+            AccessGroup accessGroup2 = new AccessGroup();
+            List<AccessGroup> accessGroups = new List<AccessGroup>()
+            {
+                accessGroup1,
+                accessGroup2
+            };
+            MockEntity.AccessGroups = accessGroups;
+            //Assert.AreEqual(accessGroups, MockEntity.AccessGroups);
+
+            accessGroups.Clear();
+            MockEntity.AccessGroups = accessGroups;
+            Assert.AreEqual(0, MockEntity.AccessGroups.Count);
+            
+        }
+
+        //[TestMethod]
+        //public void CanUseAccessDefinition()
+        //{
+        //    AccessMode readWriteAccessMode = AccessMode.Read | AccessMode.Write;
+        //    AccessGroup.AccessDefinition.AccessModes = readWriteAccessMode;
+        //    Assert.IsTrue(AccessGroup.AccessDefinition.HasMode(readWriteAccessMode));
+        //}
+
+        //[TestMethod]
+        //public void CanUseAccessGuids()
+        //{
+        //    int guidCount = 5;
+        //    List<Guid> guidList = new List<Guid>();
+
+        //    for (int i = 0; i < guidCount; ++i)
+        //    {
+        //        guidList.Add(Guid.NewGuid());
+        //    }
+
+        //    AccessGroup.AccessGuids = guidList;
+
+        //    for (int i = 0; i < guidCount; ++i)
+        //    {
+        //        Assert.AreEqual(guidList[i], AccessGroup.AccessGuids[i]);
+        //    }
+        //}
+    }
+}
