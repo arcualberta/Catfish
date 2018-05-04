afba759f45749a488587c09c287ab22cf4c8fab0
Mark McKellar <mpm1@ualberta.ca>
2018-05-04 13:29:27 -0600

    Completed security service for the Web Catfish side.

--- Catfish/Services/SecurityService.cs
+++ Catfish/Services/SecurityService.cs
@@ -5,19 +5,36 @@ using System.Linq;
 using System.Web;
 using Catfish.Core.Models.Access;
 using Catfish.Core.Models;
+using System.Configuration;
 
 namespace Catfish.Services
 {
     public class SecurityService : SecurityServiceBase
     {
+        private UserService mUserService;
+        private UserService UserService { get { if (mUserService == null) mUserService = new UserService(); return mUserService; } }
+
         public SecurityService(CatfishDbContext db) : base(db)
         {
-
         }
 
         protected override bool IsAdmin(string userGuid)
         {
-            throw new NotImplementedException();
+            Piranha.Entities.User user = UserService.GetUserById(userGuid);
+
+            if(user != null)
+            {
+                string defaultMode = ConfigurationManager.AppSettings["SecurityAdminRoleName"];
+
+                if (string.IsNullOrEmpty(defaultMode))
+                {
+                    defaultMode = "ADMIN_CONTENT";
+                }
+
+                return user.Group.Permissions.Where(p => p.Name == defaultMode).Any();
+            }
+
+            return false;
         }
     }
 }
