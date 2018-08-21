08fb8e52169469a544b2ced8209d3a3c3a011109
Mark McKellar <mpm1@ualberta.ca>
2018-08-21 11:28:40 -0600

    Added window event to allow for others to trigger url changes.

---
+++ Catfish/Scripts/catfish-global.js
@@ -0,0 +1,24 @@
+﻿{
+    var currentUrl = new URL(window.location.href);
+
+    function updateUrl(parameters) {
+        for (var param in parameters) {
+            if (parameters.hasOwnProperty(param)) {
+                currentUrl.searchParams.set(param, parameters[param]);
+            }
+        }
+
+        console.log(currentUrl);
+    }
+
+    function triggerEvent(parameters) {
+        var event = new CustomEvent('updatedparams', { parameters: parameters });
+
+        window.dispatchEvent(event);
+    }
+
+    window.updateUrlParameters = function (parameters) {
+        updateurl(parameters);
+        triggerEvent(parameters);
+    }
+}
