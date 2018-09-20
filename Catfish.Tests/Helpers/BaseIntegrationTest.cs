e84a7a18620daa06ebad2a6a6e5d498cf98a8c31
Mark McKellar <mpm1@ualberta.ca>
2018-09-20 14:23:28 -0600

    Added missing integration test files.

---
+++ Catfish.Tests/Helpers/BaseIntegrationTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Tests.Helpers
+{
+    abstract class BaseIntegrationTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Remove Database
+
+            // Create Admin Through Login
+            
+            OnSetup();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Delete the database
+            OnTearDown();
+        }
+
+        protected abstract void OnSetup();
+
+        protected abstract void OnTearDown();
+
+        private void DeleteDatabase()
+        {
+
+        }
+
+        private void SetupPiranha()
+        {
+
+        }
+    }
+}
---
+++ Catfish.Tests/IntegrationTests/ObsoleteTests/EntityGroupViewTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Configuration;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Interactions.Internal;
+using System.Collections.Generic;
+
+namespace Catfish.Tests.Views
+{
+    static class TestValues
+    { 
+        public static string EntityGroupName = "Selenium User List";
+        public static string UserLogin = "admin";
+    }
+    [TestFixture(typeof(ChromeDriver))]
+    public class EntityGroupViewTests<TWebDriver> : BaseIntegration<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        private void Login()
+        {
+            this.Driver.Navigate().GoToUrl(ManagerUrl);
+            this.Driver.FindElement(By.Id("login")).SendKeys(ConfigurationManager.AppSettings["AdminLogin"]);
+            this.Driver.FindElement(By.Name("password")).SendKeys(ConfigurationManager.AppSettings["AdminPassword"]);
+            this.Driver.FindElement(By.TagName("button")).Click();
+        }
+
+        [Ignore("Test needs to be corrected")]
+        [Test]
+        public void CanCreateEntityGroup()
+        {
+            
+            this.Driver.Navigate().GoToUrl(ManagerUrl + "/CFUserLists");
+          
+            this.Driver.FindElement(By.LinkText("Add new")).Click();
+            this.Driver.FindElement(By.Name("CFUserListName")).SendKeys(TestValues.EntityGroupName);
+            this.Driver.FindElement(By.Name("userName")).SendKeys(TestValues.UserLogin);
+            this.Driver.FindElement(By.Id("addUser")).Click();
+            this.Driver.FindElement(By.Id("btnSave")).Click();
+           
+            this.Driver.Navigate().GoToUrl(ManagerUrl + "/CFUserLists");
+            Assert.AreEqual(this.FindEntityGroupName(), TestValues.EntityGroupName);
+          
+        }
+
+        private string FindEntityGroupName()
+        {
+            string grpName = "";
+            var cols = this.Driver.FindElements(By.TagName("td"));
+
+            foreach (var col in cols)
+            {
+                if (col.Text == TestValues.EntityGroupName)
+                {
+                    grpName = col.Text;
+                    break;
+                }
+            }
+  
+            return grpName;
+        }
+    }
+}
