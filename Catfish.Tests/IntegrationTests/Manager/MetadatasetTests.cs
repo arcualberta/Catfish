3ace3e512d8f18cce602db77f7f3982cec1d4e91
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2018-09-25 10:43:50 -0600

    Start work on Metadataset integration testing
    This is the first step in using the revamped integration tests for the Metadataset model. This is work in progress.

--- Catfish.Tests/IntegrationTests/Manager/MetadatasetTests.cs
+++ Catfish.Tests/IntegrationTests/Manager/MetadatasetTests.cs
@@ -25,7 +25,19 @@ namespace Catfish.Tests.IntegrationTests.Manager
 
             List<FormField> formFields = new List<FormField>();
             formFields.Add(field1);
-            CreateMetadataSet(MetadataSetName, MetadataSetDescription, formFields.ToArray());
+            int metadataSetId = CreateMetadataSet(MetadataSetName, MetadataSetDescription, formFields.ToArray());
+
+            Driver.FindElement(By.LinkText(SettingsLinkText)).Click();
+            Driver.FindElement(By.LinkText(MetadataSetsLinkText)).Click();
+
+            // find element in list
+            Driver.FindElement(By.ClassName($"object-edit-{metadataSetId.ToString()}")).Click();
+
+            string nameText = Driver.FindElement(By.Id("Name")).Text;
+            string descriptionText = Driver.FindElement(By.Id("Description")).Text;
+
+            Assert.AreEqual(MetadataSetName, nameText);
+            Assert.AreEqual(MetadataSetDescription, descriptionText);
             //Driver.FindElement(By.LinkText(EntityTypesLinkText)).Click();
         }
     }
