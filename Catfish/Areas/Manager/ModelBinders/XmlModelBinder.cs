5fb35cda40541e65a1b6c9797c082578ff6c2abb
Mark McKellar <mpm1@ualberta.ca>
2018-09-26 12:53:51 -0600

    Added code to check the model type when building the text area

--- Catfish/Areas/Manager/ModelBinders/XmlModelBinder.cs
+++ Catfish/Areas/Manager/ModelBinders/XmlModelBinder.cs
@@ -25,6 +25,24 @@ namespace Catfish.Areas.Manager.ModelBinders
             return base.BindModel(controllerContext, bindingContext);
         }
 
+        protected override System.ComponentModel.ICustomTypeDescriptor GetTypeDescriptor(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            try
+            {
+                var key = bindingContext.ModelName + ".ModelType";
+                var typeObj = bindingContext.ValueProvider.GetValue(key);
+                if (typeObj == null)
+                    typeObj = bindingContext.ValueProvider.GetValue("ModelType");
+                var typeStr = typeObj.AttemptedValue;
+                var type = Type.GetType(typeStr);
+                return new System.ComponentModel.DataAnnotations.AssociatedMetadataTypeTypeDescriptionProvider(type).GetTypeDescriptor(type);
+            }
+            catch (Exception ex)
+            {
 
+            }
+
+            return base.GetTypeDescriptor(controllerContext, bindingContext);
+        }
     }
 }
