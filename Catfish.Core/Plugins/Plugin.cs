5ef76c93a9d070dc5bd3aa45405ff1fcda122e0c
Mark McKellar <mpm1@ualberta.ca>
2018-10-16 12:46:00 -0600

    Added plugin support.

---
+++ Catfish.Core/Plugins/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Core.Plugins
+{
+    public abstract class Plugin
+    {
+        public abstract void Initialize();
+    }
+}
