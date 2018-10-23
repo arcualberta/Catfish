b93badd48cdd39e722a69904b10c298a1b9a7c01
Mark McKellar <mpm1@ualberta.ca>
2018-10-23 15:25:55 -0600

    Fixed the ability to load plugins

--- Catfish.Core/Plugins/PluginContext.cs
+++ Catfish.Core/Plugins/PluginContext.cs
@@ -23,6 +23,8 @@ namespace Catfish.Core.Plugins
             }
         }
 
+        private List<Assembly> mAssemblies;
+
         private List<Plugin> mPlugins;
         public IReadOnlyList<Plugin> Plugins
         {
@@ -35,6 +37,7 @@ namespace Catfish.Core.Plugins
         private PluginContext()
         {
             mPlugins = new List<Plugin>();
+            mAssemblies = new List<Assembly>();
         }
 
         public Plugin LoadPlugin(string pluginClass, string assemblyLocation)
@@ -43,10 +46,36 @@ namespace Catfish.Core.Plugins
             Assembly asm = Assembly.Load(name);
 
             Plugin result = asm.CreateInstance(pluginClass) as Plugin;
-
+            
             mPlugins.Add(result);
 
+            if (!mAssemblies.Contains(asm))
+            {
+                mAssemblies.Add(asm);
+            }
+
             return result;
+        }
+
+        public ICollection<Type> GetTypes(IEnumerable<string> namespaces, string className)
+        {
+            IEnumerable<string> check = namespaces.Select(n => n + "." + className);
+            List<Type> result = new List<Type>();
+
+            foreach(Assembly asm in mAssemblies)
+            {
+                foreach(string name in check)
+                {
+                    Type type = asm.GetType(name);
+
+                    if(type != null)
+                    {
+                        result.Add(type);
+                    }
+                }
+            }
+
+            return result.AsReadOnly();
         }
     }
 }
