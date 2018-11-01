ab3d9994c29f788d8a9a5cf5126c67fa0904ceda
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2018-11-01 14:35:46 -0600

    Add AccessDefinition creat method and integratoin test
    The current test is very simple. Create an AccessDefinition with specified
    name and Read AccessMode. This works for the current requirement but we
    should change this to be able to specify different AccessMode and test for
    them.

---
+++ Catfish.Tests/IntegrationTests/Manager/AccessDefinitionsTests.cs
@@ -0,0 +1,41 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Models.Access;
+using Catfish.Core.Models.Forms;
+using Catfish.Tests.Extensions;
+using Catfish.Tests.IntegrationTests.Helpers;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.Tests.IntegrationTests.Manager
+{
+    [TestFixture(typeof(ChromeDriver))]
+    class AccessDefinitionsTests<TWebDriver> : BaseIntegrationTest<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        [Test]
+        public void CanCreateAccessDefinitions()
+        {
+            string accessDefinitionName = "Public read";
+            //XXX Currently only checking AccessMode.Read manually
+            AccessMode accessMode = AccessMode.Read;
+            
+            CreateAccessDefinition(accessDefinitionName, accessMode);
+
+            // Check name
+            string pathToAccessDefinitionName = "(//table[contains(@class, 'list bs')]//td)[1]";
+            string pathToAccessModeName = "(//table[contains(@class, 'list bs')]//td)[2]";
+            string setName = Driver.FindElement(By.XPath(pathToAccessDefinitionName)).Text;
+            string setAccessMode = Driver.FindElement(By.XPath(pathToAccessModeName)).Text;
+            string stringAccessMode = AccessMode.Read.ToString();
+
+            Assert.AreEqual(accessDefinitionName, setName);
+            Assert.AreEqual(stringAccessMode, setAccessMode);
+            //XXX Check if accessMode is set
+        }
+    }
+}
