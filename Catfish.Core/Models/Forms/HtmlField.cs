cb3e93df2d98f728d7b589c082813afe87126043
Kamal Ranaweera <ranaweer@ualberta.ca>
2018-11-05 13:10:20 -0700

    Created CompositeFormField

--- Catfish.Core/Models/Forms/HtmlField.cs
+++ Catfish.Core/Models/Forms/HtmlField.cs
@@ -1,5 +1,4 @@
-﻿using Catfish.Core.Models.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Catfish.Core.Models.Forms
 {
-    [CFTypeLabel("Html field")]
     public class HtmlField : FormField
     {
     }
