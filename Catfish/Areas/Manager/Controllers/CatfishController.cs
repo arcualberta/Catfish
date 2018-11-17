32ce9cba62a906cb6096eb02a175efafc4d265fc
Kamal Ranaweera <ranaweer@ualberta.ca>
2018-11-16 17:47:11 -0700

    Preliminary implementation of SurveyService.ExportFormData.

--- Catfish/Areas/Manager/Controllers/CatfishController.cs
+++ Catfish/Areas/Manager/Controllers/CatfishController.cs
@@ -64,6 +64,7 @@ namespace Catfish.Areas.Manager.Controllers
             }
         }
 
+
         [HttpPost]        
         public JsonResult SelectEntity(SelectEntityTypeViewModel vm)
         {
