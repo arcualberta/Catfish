edf244962554d2a49211064d0bf2a61a3c2c579d
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2018-12-04 14:42:07 -0700

    Test adding git information for Jenkins

---
+++ Catfish/Areas/Manager/Controllers/BuildController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catfish.Areas.Manager.Controllers
+{
+    public class BuildController : Controller
+    {
+        // GET: Build
+        public ActionResult Index()
+        {
+            ViewBag.GitCommmitSha = System.Configuration.ConfigurationManager.AppSettings["GitCommitSha"];
+            ViewBag.GitUrl = System.Configuration.ConfigurationManager.AppSettings["GitUrl"];
+            ViewBag.GitBranch = System.Configuration.ConfigurationManager.AppSettings["GitBranch"];
+
+            string githubUrl = Regex.Replace(ViewBag.GitUrl, @"\.git$", "");
+            ViewBag.CommitUrl = githubUrl + "/commit/" + ViewBag.GitCommmitSha;
+            return View();
+        }
+    }
+}
