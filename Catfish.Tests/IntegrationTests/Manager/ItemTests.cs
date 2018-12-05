a4ca872e0180fa66e52c5e95bbaaadf630d18ad8
mruaini <mruaini@ualberta.ca>
2018-12-05 10:26:50 -0700

    move test method CanCreateItemWithAttachment() from ItemsTest.cs to ItemTest.cs and delete ItemsTest.cs

--- Catfish.Tests/IntegrationTests/Manager/ItemTests.cs
+++ Catfish.Tests/IntegrationTests/Manager/ItemTests.cs
@@ -32,5 +32,42 @@ namespace Catfish.Tests.IntegrationTests.Manager
 
             Assert.AreEqual(ItemValue, savedValue);
         }
+
+        [Test]
+        public void CanCreateItemWithAttachment()
+        {
+            Driver.Manage().Window.Maximize();
+            string EntityTypeName = "Entity type name";
+            string EntityTypeDescription = "Entity type description";
+            string MetadataSetName = "Metadata set name";
+            string MetadataSetDescription = "Metadata set description";
+            string ItemName = "Item name";
+            bool withAttachment = true;
+            TextField fieldName = new TextField();
+            fieldName.Name = "Name";
+            CFEntityType.eTarget[] eTarget = new CFEntityType.eTarget[] { CFEntityType.eTarget.Items };
+
+
+
+            TextArea fieldDescription = new TextArea();
+            fieldDescription.Name = "Description";
+            List<FormField> formFields = new List<FormField>();
+            formFields.Add(fieldName);
+            formFields.Add(fieldDescription);
+
+            CreateMetadataSet(MetadataSetName, MetadataSetDescription, formFields.ToArray());
+
+
+
+            CreateEntityType(EntityTypeName, EntityTypeDescription, new[] {
+                MetadataSetName
+                }, eTarget);
+
+            CreateItem(EntityTypeName, ItemName, withAttachment);
+
+            Assert.IsTrue(Driver.FindElement(By.XPath("//div[@class='img']"), 10).Displayed);
+
+        }
+
     }
 }
