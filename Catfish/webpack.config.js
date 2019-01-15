1beebe7ced4ed257e92475f535fca12a77f5e1f8
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-01-15 15:14:04 -0700

    Configure webpack to transpile ES2015 with babel

---
+++ Catfish/webpack.config.js
@@ -0,0 +1,19 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './Scripts/es6/main.js',
+        file2: './Scripts/es6/file2.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './Scripts/dist'),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
