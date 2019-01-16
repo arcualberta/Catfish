003a5ce5713a4a3a6e44b143f6f496779098ca93
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-01-16 15:04:48 -0700

    Add babel preset environment for jsx

---
+++ Catfish/Scripts/es6/actionableEntityList.js
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+
+export default class ActionableEntityList extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <table>
+                <thead>
+                </thead>
+                <tbody>
+                    "test"
+                </tbody>
+            </table>
+        );
+    }
+
+}
+
+ActionableEntityList.proptypes = {
+    name: PropTypes.string.isRequired,
+    selected: PropTypes.array.isRequired,
+    entities: PropTypes.array.isRequired
+}
