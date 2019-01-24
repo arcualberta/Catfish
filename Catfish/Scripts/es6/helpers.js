46be33fef94395c678bba674849c1dc7c78e1e90
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-01-24 12:16:41 -0700

    Work on pagination

--- Catfish/Scripts/es6/helpers.js
+++ Catfish/Scripts/es6/helpers.js
@@ -1,6 +1,16 @@
 ﻿
+
 export function objectify(toObjectify) {
     return JSON.parse(JSON.stringify(toObjectify))
 }
-    
 
+// XXX improve
+export function range(start, end) {
+    
+    let result = []
+    for (let i = start; i <= end; ++i) {
+        result.push(i)
+    }
+
+    return result
+}
