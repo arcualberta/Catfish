6964de605d860d25929c54fd95ab582ebbe3e084
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-02-01 10:29:39 -0700

    Working view

--- Catfish/Scripts/es6/pages/associationsView.js
+++ Catfish/Scripts/es6/pages/associationsView.js
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
 import ReactDom from 'react-dom';
-import AssociationsLists from './associationsLists.js';
+import AssociationsLists from '../components/associationsLists';
 
 ReactDom.render(<AssociationsLists />, document.getElementById('associations'));
 
