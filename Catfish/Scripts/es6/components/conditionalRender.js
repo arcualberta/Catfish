8f9bc7b8673320db8da18c503aa13ca412aa2632
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-02-27 11:59:51 -0700

    Working on bugs and code cleanup

---
+++ Catfish/Scripts/es6/components/conditionalRender.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+
+const ConditionalRender = (props) => {
+    if (props.condition == true) {
+        return <div>{ props.children }</div>
+    } else {
+        return <div></div>
+    }
+}
+
+export default ConditionalRender
