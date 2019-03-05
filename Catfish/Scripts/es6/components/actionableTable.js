d65124cd52d0b355432fb6b308b93dbdae2f7812
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-03-05 14:14:06 -0700

    WIP

--- Catfish/Scripts/es6/components/actionableTable.js
+++ Catfish/Scripts/es6/components/actionableTable.js
@@ -1,73 +1,121 @@
 ﻿import React from "react"
 import ReactDOM from "react-dom"
 import PropTypes from 'prop-types'
-
+import { range } from '../helpers'
 const ActionableTable = (props) => {
   
     return (
-        <table className="actionable-table">
-            {renderHead({...props})}
-            {renderBody({...props})}            
+        <table className="table actionable-table">
+            {renderHead(props)}
+            {renderBody(props)}            
         </table>
         )
 }
+
 ActionableTable.propTypes = {
     location: PropTypes.string.isRequired,
     data: PropTypes.array.isRequired,
     selected: PropTypes.array.isRequired,
     headers: PropTypes.array.isRequired,
     update: PropTypes.func.isRequired,
-    isEquivalent: PropTypes.func.isRequired
+    isEquivalent: PropTypes.func.isRequired,
+    maxRows: PropTypes.number
+}
+
+ActionableTable.defaultProps = {
+    maxRows: 0
+}
+
+const renderHead = props => {
+    const { headers,
+        data,
+        selected,
+        isEquivalent,
+        update,
+        location } = props
+    return (
+        <thead>
+            <tr>
+
+                <td>
+                    <input
+                        type="checkbox"
+                        checked={isPageChecked({ data, selected, isEquivalent })}
+                        onChange={(event) => {
+                            const checked = event.target.checked
+                            togglePage({ data, selected, isEquivalent, update, location, checked })
+                        }}
+                    />
+                </td>
+
+                {headers.map(x =>
+                    <td key={x.id}>{x.title}</td>
+                )}
+            </tr>
+        </thead>
+        )
 }
 
 
-const renderHead = ({ headers, data, selected, isEquivalent, update, location }) => 
-    <thead>
-        <tr>
 
-            <td>
-                <input
-                    type="checkbox"
-                    checked={isPageChecked({ data, selected, isEquivalent })}
-                    onChange={(event) => {
-                        const checked = event.target.checked
-                        togglePage({ data, selected, isEquivalent, update, location, checked })
-                    } }
-                />
-            </td>
+const getExtraRows = (maxRows, dataLength, headers) => {
+    if (maxRows > dataLength + 1) {
+        return range(dataLength + 1, maxRows)
+            .map(value =>
+                <tr key={value}>
 
-            {headers.map(x =>
-                <td key={x.id}>{x.title}</td>
-            )}
-        </tr>
-    </thead>
-
-
-const renderBody = ({data, headers, selected, isEquivalent, update, location }) => 
-    <tbody>
-        {
-            data.map(datum =>
-                <tr key={datum.id}>
-
-                    <td>
-                        <input
-                            type="checkbox"
-                            checked={isChecked({ datum, selected, isEquivalent })}
-                            onChange={() => {
-                                toggle({ datum, selected, isEquivalent, update, location })
-                            }}
-                        />
-                    </td>
-
-
+                    <td></td>
                     {headers.map(header =>
-                        <td key={header.id}>{datum[header.key]}</td>
+                        <td key={header.id}></td>
                     )}
                 </tr>
             )
-        }
-    </tbody>
+    } 
 
+    return null
+}
+
+const renderBody = props => {
+
+    const {
+        data,
+        headers,
+        selected,
+        isEquivalent,
+        update,
+        location,
+        maxRows = 0
+    } = props
+
+    const restOfTable = getExtraRows(maxRows, data.length, headers)
+    
+    return (             
+        <tbody>
+            {
+                data.map(datum =>
+                    <tr key={datum.id}>
+
+                        <td>
+                            <input
+                                type="checkbox"
+                                checked={isChecked({ datum, selected, isEquivalent })}
+                                onChange={() => {
+                                    toggle({ datum, selected, isEquivalent, update, location })
+                                }}
+                            />
+                        </td>
+
+
+                        {headers.map(header =>
+                            <td key={header.id}>{datum[header.key]}</td>
+                        )}
+                    </tr>
+                )   
+            }
+            { restOfTable }            
+        </tbody>
+        )
+}
 
 const isChecked = ({ datum, selected, isEquivalent }) => 
     selected.some(item => isEquivalent(item, datum))
