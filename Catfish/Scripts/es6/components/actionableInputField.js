bec0b786cac79aff54a927404bb6072984771695
Omar Rodriguez-Arenas <orodrigu@ualberta.ca>
2019-03-18 16:22:55 -0600

    Tests work

--- Catfish/Scripts/es6/components/actionableInputField.js
+++ Catfish/Scripts/es6/components/actionableInputField.js
@@ -6,7 +6,9 @@ const ActionableInputField = props =>
     <input type="text"
         placeholder={props.placeholder}
         onChange={props.handleChange}
-        className="actionable-input-field form-control" />
+        className="actionable-input-field form-control"
+        id={props.id}
+    />
     
 ActionableInputField.propTypes = {
     handleChange: PropTypes.func.isRequired,
