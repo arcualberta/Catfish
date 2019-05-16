04320ea540f15fb53691b06e819cee6dd6f94d48
mruaini <mruaini@ualberta.ca>
2019-05-16 09:01:33 -0600

    ARF-393 Make the collection page more manageable using React

---
+++ Catfish/Scripts/es6/pages/collectionsView.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+import ReactDom from 'react-dom';
+import CollectionsLists from '../components/collectionsLists';
+
+ReactDom.render(<CollectionsLists />, document.getElementById('collections'));
