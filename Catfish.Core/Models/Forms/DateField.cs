64f09e2cb1e902cb5e3fc3a43daaabdd3baea140
Mark McKellar <mpm1@ualberta.ca>
2019-05-21 12:50:56 -0600

    Added ability to set the date value for multiple fields.

--- Catfish.Core/Models/Forms/DateField.cs
+++ Catfish.Core/Models/Forms/DateField.cs
@@ -8,6 +8,22 @@ namespace Catfish.Core.Models.Forms
     [CFTypeLabel("Date")]
     public class DateField : NumberField //FormField
     {
+        protected TextValue FirstValue
+        {
+            get
+            {
+                foreach(TextValue value in Values)
+                {
+                    if(value != null && !string.IsNullOrEmpty(value.Value))
+                    {
+                        return value;
+                    }
+                }
+
+                return null;
+            }
+        }
+
         //[NotMapped]
         //public string Value
         //{
@@ -33,7 +49,7 @@ namespace Catfish.Core.Models.Forms
                 if (!string.IsNullOrEmpty(Values[0].Value))
                 {
                    
-                    string year = Values[0].Value.Substring(0, 4);
+                    string year = FirstValue.Value.Substring(0, 4);
                     return year;
                 }
                 return null;
@@ -49,7 +65,7 @@ namespace Catfish.Core.Models.Forms
                 if (!string.IsNullOrEmpty(Values[0].Value))
                 {
 
-                    string month = Values[0].Value.Substring(4, 2);
+                    string month = FirstValue.Value.Substring(4, 2);
                     return month;
                 }
                 return null;
@@ -65,7 +81,7 @@ namespace Catfish.Core.Models.Forms
                 if (!string.IsNullOrEmpty(Values[0].Value))
                 {
 
-                    string day = Values[0].Value.Substring(6);
+                    string day = FirstValue.Value.Substring(6);
                     return day;
                 }
                 return null;
@@ -89,12 +105,16 @@ namespace Catfish.Core.Models.Forms
                 if (!string.IsNullOrEmpty(val.Value))
                 {
                     List<TextValue> textValues = new List<TextValue>();
-                   
-                    TextValue textVal = new TextValue();
-                    textVal.LanguageCode = "en";
-                    textVal.LanguageLabel = "English";
-                    textVal.Value = value.Replace("-","");
-                    textValues.Add(textVal);
+
+                    foreach (var language in Helpers.ConfigHelper.Languages)
+                    {
+                        TextValue textVal = new TextValue();
+                        textVal.LanguageCode = language.TwoLetterISOLanguageName;
+                        textVal.LanguageLabel = language.Name;
+                        textVal.Value = value.Replace("-", "");
+                        textValues.Add(textVal);
+                    }
+
                     Values = textValues;
                 }
                
