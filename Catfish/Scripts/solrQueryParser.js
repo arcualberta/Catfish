d6560e29217e5a0fffde6110a72ca0385e76e146
mpm1 <mpm1@ualberta.ca>
2019-05-27 15:56:44 -0600

    Updated to use the solrQueryParser.js to read the query string from the server.

--- Catfish/Scripts/solrQueryParser.js
+++ Catfish/Scripts/solrQueryParser.js
@@ -46,6 +46,45 @@ var SolrParser = function (langCode) {
         return outputBuffer;
     }
 
+    var TokenEnumerator = function (inputString) {
+        this.data = [];
+        this.index = -1;
+
+        tokenize(inputString, this.data);
+    }
+    {
+        TokenEnumerator.prototype.push = function (value) {
+            this.data.push(value);
+        }
+
+        TokenEnumerator.prototype.next = function () {
+            var index = this.index;
+            ++index;
+
+            if (index < 0 || index >= this.data.length) {
+                return null;
+            }
+
+            this.index = index;
+
+            return this.data[index];
+        }
+
+        TokenEnumerator.prototype.current = function () {
+            var index = this.index;
+
+            if (index < 0 || index >= this.data.length) {
+                return null;
+            }
+
+            return this.data[index];
+        }
+
+        TokenEnumerator.prototype.reset = function () {
+            this.index = -1;
+        }
+    }
+
     function solrEscapeString(input) {
         if (input == null) {
             return null;
@@ -138,12 +177,205 @@ var SolrParser = function (langCode) {
         return pieces.join(" && ");
     }
 
-    SolrParser.prototype.decode = function (queryString) {
-        if (queryString.trim == "*:*") {
-            return {};
+    function decodeTopLevelBracket(tokens, result) {
+        var name = null;
+        var token = null;
+
+        while ((token = tokens.next()) != null) {
+            switch (token) {
+                case ":":
+                    if (name == null) {
+                        console.error("Error parsing search string. No name befor ':' on token " + tokens.index);
+                    }
+
+                    decodeMultiSelect.call(this, name, tokens, result);
+                    name = null;
+                    break;
+
+                case "\"":
+                    var value = decodeQuotation.call(this, tokens);
+                    if (name == null) {
+                        name = "\"" + token + "\"";
+                    } else {
+                        name += " " + "\"" + token + "\"";
+                    }
+                    break;
+
+                case ")":
+                    if (name != null) {
+                        result['*'] = this.createStringField(name);
+                    }
+
+                    return;
+
+                case "||":
+                case "&&":
+                    break;
+
+                default:
+                    if (name == null) {
+                        name = token;
+                    } else {
+                        name += " " + token;
+                    }
+                    break;
+            }
+        }
+    }
+
+    function decodeMultiSelect(name, tokens, result) {
+        var token = null;
+        var values = [];
+        var value = null;
+        var resultName = name.substring(0, name.length - "_en_ss".length);
+
+        token.next(); // Remove opening bracket.
+
+        while ((token = tokens.next()) != null) {
+            if (token == ")") {
+                break;
+            }
+
+            switch (token) {
+                case "(":
+                    break;
+
+                case "&&":
+                case "||":
+                    values.push(value);
+                    value = null;
+                    break;
+
+                case "\"":
+                    value = decodeQuotation.call(this, tokens);
+                    break;
+
+                default:
+                    if (value == null) {
+                        value = token;
+                    } else {
+                        value += " " + token;
+                    }
+            }
         }
 
-        var tokens = tokenize(queryString);
+        if (value != null) {
+            values.push(value);
+        }
+
+        result[resultName] = this.createMultiSelectField(values);
+    }
+
+    function decodeRange(name, tokens, result) {
+        var from = tokens.next();
+        var resultName = name.substring(0, name.length - "_is".length);
+
+        if (tokens.next().toLowerCase() != "to") {
+            console.error("Invalid range statement at " + tokens.index);
+        }
+
+        var to = tokens.next();
+
+        if (tokens.next() != "]") {
+            console.error("Invalid range statement at " + tokens.index);
+        }
+
+        result[resultName] = this.createRangeField(from, to);
+    }
+
+    function decodeString(name, tokens, result) {
+        var value = "";
+        var token;
+        var resultName = name.substring(0, name.length - "_txts_en".length);
+
+        while ((token = tokens.next()) != null) {
+            if (token == ")") {
+                break;
+            }
+
+            switch (token) {
+                case "||":
+                case "&&":
+                    value += " ";
+                    break;
+
+                case "\"":
+                    value += "\"" + decodeQuotation.call(this, tokens) + "\"";
+                    break;
+
+                default:
+                    value += token;
+            }
+        }
+
+        result[resultName] = this.createStringField(value);
+    }
+
+    function decodeElement(name, tokens, result) {
+        var token = null;
+        var value = null;
+
+        token = tokens.next();
+
+        if (token == "[") {
+            decodeRange.call(this, name, tokens, result);
+        } else if (token == "(") {
+            decodeString.call(this, name, tokens, result);
+        } else {
+            console.error("Invaid token found at index " + tokens.index);
+        }
+    }
+
+    function decodeQuotation(tokens) {
+        var result = "";
+        var token;
+
+        while ((token = tokens.next()) != null) {
+            if (token == "\"") {
+                break;
+            } else {
+                result += token + " ";
+            }
+        }
+
+        return result.trim();
+    }
+    
+    SolrParser.prototype.decode = function (queryString) {
+        var result = {};
+
+        if (queryString == null || queryString.trim() == "*:*") {
+            return result;
+        }
+
+        var tokens = new TokenEnumerator(queryString);
+        var name = null;
+        var token = null;
+
+        while ((token = tokens.next()) != null) {
+            switch (token) {
+                case "(":
+                    decodeTopLevelBracket.call(this, tokens, result);
+                    break;
+
+                case ":":
+                    if (name == null) {
+                        console.error("Error parsing search string. No name befor ':' on token " + tokens.index);
+                    }
+
+                    decodeElement.call(this, name, tokens, result);
+                    break;
+
+                case "\"":
+                    name = decodeQuotation.call(this, tokens);
+                    break;
+                    
+                default:
+                    name = token;
+            }
+        }
+
+        return result;
     }
 
     SolrParser.prototype.createStringField = function (fieldString) {
