4c1961057156a305fc6e150e63a8d7a393be369c
Mark McKellar <mpm1@ualberta.ca>
2019-08-02 15:55:19 -0600

    Modified so HTMLFields will appear in the selectable options of table fields.

--- Catfish.Core/Models/Forms/HtmlField.cs
+++ Catfish.Core/Models/Forms/HtmlField.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catfish.Core.Models.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Catfish.Core.Models.Forms
 {
+    [CFTypeLabel("Html Field")]
     public class HtmlField : FormField
     {
     }
