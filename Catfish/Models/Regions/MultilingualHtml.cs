352a64898d450e496e03fc51327e69807732267a
Mark McKellar <mpm1@ualberta.ca>
2019-08-13 13:47:47 -0600

    Created a MultilingualHtml field.

---
+++ Catfish/Models/Regions/MultilingualHtml.cs
@@ -0,0 +1,28 @@
+﻿using Catfish.Core.Models;
+using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+namespace Catfish.Models.Regions
+{
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "MultilingualHtml")]
+    [ExportMetadata("Name", "Multilingual Html")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+    public class MultilingualHtml : CatfishRegion
+    {
+        public List<TextValue> Content { get; set; }
+
+        public MultilingualHtml()
+        {
+            Content = new List<TextValue>();
+        }
+
+        public string GetContent(string langCode)
+        {
+            return Content.Where(c => c.LanguageCode == langCode).Select(c => c.Value).FirstOrDefault();
+        }
+    }
+}
