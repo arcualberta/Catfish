d73b85de1d34a1ddfdd86aeef9380ef7f3c7fb24
mruaini <mruaini@ualberta.ca>
2019-08-21 10:47:12 -0600

    ARF-472 -- add attribute "issystemcollection" to CFCollection

--- Catfish.Core/Models/CFCollection.cs
+++ Catfish.Core/Models/CFCollection.cs
@@ -1,6 +1,7 @@
 ﻿using Catfish.Core.Models.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -12,6 +13,31 @@ namespace Catfish.Core.Models
     {
         public override string GetTagName() { return "collection"; }
         public override string Label => "Collection";
+
+        
+        [NotMapped]
+        [Display(Name = "Is System Collection")]
+        public bool IsSystemCollection
+        {
+            get
+            {
+                var isSysColl = Data.Attribute("issystemcollection");
+                if (isSysColl == null)
+                {
+                    Data.SetAttributeValue("issystemcollection", false);
+                    return false;
+                }
+                else {
+                    return Convert.ToBoolean(isSysColl.Value) ? true : false;
+                }
+            }
+
+            set
+            {
+                Data.SetAttributeValue("issystemcollection", value);
+            }
+        }
+
 
         [NotMapped]
         [IgnoreDataMember]
