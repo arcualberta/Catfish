7ef1456cdd0f7e53e8eda5029d84f29ea754a13d
Melania Ruaini <melaniar@hotmail.com>
2019-11-15 11:49:53 -0700

    ARF-499: Add Vimeo Panel which will embed Vimeo video to web page

---
+++ Catfish/Models/Regions/VimeoPanel.cs
@@ -0,0 +1,37 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Catfish.Models.Regions
+{
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "VimeoPanel")]
+    [ExportMetadata("Name", "VimeoPanel")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+    public class VimeoPanel : CatfishRegion
+    {
+       
+
+        [Display(Name = "Vimeo Embed Code")]
+        public string VimeoEmbedCode { get; set; }
+        
+        //public override void InitManager(object model)
+        //{
+        //    base.InitManager(model);
+        //}
+
+        //public override object GetContent(object model)
+        //{
+
+        //    return base.GetContent(model);
+        //}
+
+
+    }
+    
+}
