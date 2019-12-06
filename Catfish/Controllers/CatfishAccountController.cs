9703e5ac5fc7e1c61296482465116a992228d0ab
Melania Ruaini <melaniar@hotmail.com>
2019-12-06 13:52:54 -0700

    ARF-91: add ability to login with Gmail account

--- Catfish/Controllers/CatfishAccountController.cs
+++ Catfish/Controllers/CatfishAccountController.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Piranha.Models;
 
-//using System.Diagnostics;
-//using System.Reflection;
-//using System.Web.Mvc;
 using System.Web.Security;
+using Piranha.Entities;
+using Catfish.Models;
 
+using System.Security.Claims;
 namespace Catfish.Controllers
 {
     public class CatfishAccountController : Controller
@@ -18,7 +18,7 @@ namespace Catfish.Controllers
         public ActionResult Login()
         {
             Piranha.Models.LoginModel m = new LoginModel();
-
+            
             return View();
         }
         /// <summary>
@@ -28,6 +28,7 @@ namespace Catfish.Controllers
 		[HttpPost]
         public ActionResult Login(Piranha.Models.LoginModel m)
         {
+            
             // Authenticate the user
             if (ModelState.IsValid)
             {
@@ -69,6 +70,122 @@ namespace Catfish.Controllers
             Session.Abandon();
 
             return Redirect("~/");
+        }
+        /// <summary>
+        /// Login with Google account
+        /// </summary>
+        /// <param name="userModel"></param>
+        /// <returns></returns>
+        public ActionResult ExternalLogin(Piranha.Entities.User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Piranha.Models.LoginModel loginModel = new LoginModel();
+                User user = GetUserByName(userModel.Login);
+                loginModel.Login = userModel.Login;
+                loginModel.Password = userModel.Password;
+                if (user == null)
+                {
+                    if (AllowGoogleLogin())
+                    {
+                        user = CreateNewUser(userModel);
+                        using (var db = new Piranha.DataContext())
+                        {
+                            // Login sysuser into the current context.
+                            db.LoginSys();
+                            db.Users.Add(user);
+                            if (db.SaveChanges() > 0)
+                            {
+                                return Login(loginModel);
+                            }
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    //log he in using his gmail account -- user existed in our system
+
+                    return Login(loginModel);
+                }
+
+            }
+            else {
+                return RedirectToAction("Login");
+            }
+            return Redirect("~/");
+        }
+      
+        // GET: UserAccount
+        public ActionResult AuthenticationFailure()
+        {
+            return View();
+        }
+        /// <summary>
+        /// get piranha User by Login name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// 
+        public User GetUserByName(string name)
+        {
+            User usr = null;
+            using (var db = new Piranha.DataContext())
+            {
+                usr = db.Users.Where(u => u.Login == name).FirstOrDefault();
+            }
+
+            return usr;
+        }
+        /// <summary>
+        /// get piranha User by Login name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public User GetUserByEmail(string email)
+        {
+            User usr = null;
+            using (var db = new Piranha.DataContext())
+            {
+                usr = db.Users.Where(u => u.Email == email).FirstOrDefault();
+            }
+
+            return usr;
+        }
+      
+        public bool AllowGoogleLogin()
+        {
+            return Convert.ToBoolean(System.Configuration.ConfigurationManager.AppSettings["AllowGoogleLogin"]);
+        }
+     
+      
+        /// <summary>
+        /// Create piranha User when user is login from external mean like Google
+        /// </summary>
+        /// <param name="user">Piranha.Entities.User</param>
+        /// <returns></returns>
+        private User CreateNewUser(Piranha.Entities.User user)
+        {
+            //get piranha Group
+            Group group = null;
+            using (var db = new Piranha.DataContext())
+            {
+                string groupName = GetInitialGroupName();
+                group = db.Groups.Where(g => g.Name == groupName).FirstOrDefault();
+            }
+            user.GroupId = group.Id;
+            if (!String.IsNullOrEmpty(user.Password))
+            {
+                user.Password = Piranha.Models.SysUserPassword.Encrypt(user.Password);
+            }
+            return user;
+        }
+      
+        public string GetInitialGroupName()
+        { 
+            return System.Configuration.ConfigurationManager.AppSettings["InitialGroupName"];
         }
     }
 }
