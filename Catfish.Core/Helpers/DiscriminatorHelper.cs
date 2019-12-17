35b1f545beaf03f54352f8f2a763a2342a551f10
Melania Ruaini <melaniar@hotmail.com>
2019-12-17 15:11:47 -0700

    ARF-564: Fixed FindAccessibleByGuid() if the source is CFAggregation type

---
+++ Catfish.Core/Helpers/DiscriminatorHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Catfish.Core.Helpers
+{
+    public static class DiscriminatorHelper
+    {
+        //public static IEnumerable<Type> GetAllDiscriminatorType(Type p)
+        //{
+        //    foreach( var ass in AppDomain.CurrentDomain.GetAssemblies())
+        //    {
+        //        foreach(var t in ass.GetTypes())
+        //        {
+        //            if (t.IsSubclassOf(p))
+        //                yield return t;
+
+        //        }
+        //    }
+        //}
+
+        public static List<string> GetDiscriminatorWhere<T>()
+        {
+            List<string> allDiscriminators = new List<string>();
+            var subclassTypes = Assembly.GetAssembly(typeof(T)).GetTypes().Where(t => t.IsSubclassOf(typeof(T)));
+            foreach(var s in subclassTypes)
+            {
+                allDiscriminators.Add(s.Name);
+            }
+            return allDiscriminators;
+        }
+
+       
+    }
+}
