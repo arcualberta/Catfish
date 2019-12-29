210735a26aa1879bfc062f93445b5c7b47e7dd14
Kamal Ranaweera <ranaweer@ualberta.ca>
2019-12-29 12:01:06 -0700

    Created Catfish.Core library project and imported Catfish.Core.Models.Attributes from Catfish 1.0

---
+++ Catfish.Core/Models/Attributes/CFRankAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Catfish.Core.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CFRankAttribute : Attribute
+    {
+        public int Rank { get; set; }
+
+        public CFRankAttribute(int rank)
+        {
+            Rank = rank;
+        }
+    }
+}
---
+++ Catfish.Core/Models/Attributes/CFTypeLabelAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Catfish.Core.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class CFTypeLabelAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public CFTypeLabelAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
