65afe09da784adf1a7350ad91457642901a0b4bf
Jill <jlovas@ualberta.ca>
2020-03-30 16:27:11 -0600

    now have some ui that doesnt do anything yet

--- Catfish/wwwroot/assets/js/catfish.itemlist.js
+++ Catfish/wwwroot/assets/js/catfish.itemlist.js
@@ -6,7 +6,7 @@ var vm = new Vue({
             loading: true,
             updateBindings: false,
             items: [],
-            sites: [],
+            collections: [],
             pageTypes: [],
             addSiteId: null,
             addSiteTitle: null,
@@ -23,16 +23,113 @@ var vm = new Vue({
                 fetch(piranha.baseUrl + "manager/api/items")
                     .then(function (response) { return response.json(); })
                     .then(function (result) {
-                        self.sites = result.sites;
+                        self.collections = result.collections;
                         self.pageTypes = result.pageTypes;
                         self.updateBindings = true;
                     })
                     .catch(function (error) { console.log("error:", error); });
             });
+        },
+        remove(id) {
+            var self = this;
+
+            piranha.alert.open({
+                title: piranha.resources.texts.delete,
+                body: piranha.resources.texts.deletePageConfirm,
+                confirmCss: "btn-danger",
+                confirmIcon: "fas fa-trash",
+                confirmText: piranha.resources.texts.delete,
+                onConfirm: function () {
+                    fetch(piranha.baseUrl + "manager/api/page/delete/" + id)
+                        .then(function (response) { return response.json(); })
+                        .then(function (result) {
+                            piranha.notifications.push(result);
+
+                            self.load();
+                        })
+                        .catch(function (error) { console.log("error:", error); });
+                }
+            });
+        },
+        bind() {
+            var self = this;
+
+            $(".sitemap-container").each(function (i, e) {
+                $(e).nestable({
+                    maxDepth: 100,
+                    group: i,
+                    callback: function (l, e) {
+                        fetch(piranha.baseUrl + "manager/api/page/move", {
+                            method: "post",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                id: $(e).attr("data-id"),
+                                items: $(l).nestable("serialize")
+                            })
+                        })
+                            .then(function (response) { return response.json(); })
+                            .then(function (result) {
+                                piranha.notifications.push(result.status);
+
+                                if (result.status.type === "success") {
+                                    $('.sitemap-container').nestable('destroy');
+                                    self.sites = [];
+                                    Vue.nextTick(function () {
+                                        self.sites = result.sites;
+                                        Vue.nextTick(function () {
+                                            self.bind();
+                                        });
+                                    });
+                                }
+                            })
+                            .catch(function (error) {
+                                console.log("error:", error);
+                            });
+                    }
+                })
+            });
+        },
+        add(siteId, pageId, after) {
+            var self = this;
+
+            self.addSiteId = siteId;
+            self.addPageId = pageId;
+            self.addAfter = after;
+
+            // Get the site title
+            self.sites.forEach(function (e) {
+                if (e.id === siteId) {
+                    self.addSiteTitle = e.title;
+                }
+            });
+
+            // Open the modal
+            $("#pageAddModal").modal("show");
+        },
+        selectSite(siteId) {
+            var self = this;
+
+            self.addSiteId = siteId;
+
+            // Get the site title
+            self.sites.forEach(function (e) {
+                if (e.id === siteId) {
+                    self.addSiteTitle = e.title;
+                }
+            });
         }
     },
-    created() {
-        console.log("running created. piranha");
+    updated() {
+        if (this.updateBindings) {
+            this.bind();
+            this.updateBindings = false;
+        }
+
+        this.loading = false;
+    },
+    created(){
         this.load();
 	}
 });
