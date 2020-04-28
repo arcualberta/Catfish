d07b9d9bd7e5b1fe43a0d2204514d5d37624868c
Jill <jlovas@ualberta.ca>
2020-04-28 11:35:19 -0600

    removed some dead code

--- Catfish/wwwroot/assets/js/catfish.edititem.js
+++ Catfish/wwwroot/assets/js/catfish.edititem.js
@@ -401,11 +401,8 @@ if (document.getElementById("item-edit-page")) {
                                 this.originalFieldIndexMaster[index][matched[0]].count + 1);
 	                    }
 					}
-
-                    
                     console.log("originalFields:", this.originalFieldIndexMaster);
                     console.log("indices: ", this.originalFields);
-
                 }
 
             },
@@ -443,26 +440,6 @@ if (document.getElementById("item-edit-page")) {
             $(document).ready(function () {
                 $("body").tooltip({ selector: '[data-toggle=tooltip]' });
             });
-
-            //adds eventlistener to form fields for validation purposes
-            /*(function () {
-                'use strict';
-                window.addEventListener('load', function () {
-                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                    var forms = document.getElementsByClassName('edit-form');
-                    // Loop over them and prevent submission
-                    var validation = Array.prototype.filter.call(forms, function (form) {
-                        form.addEventListener('submit', function (event) {
-                            if (form.checkValidity() === false) {
-                                event.preventDefault();
-                                event.stopPropagation();
-                            }
-                            console.log("form validated");
-                            form.classList.add('was-validated');
-                        }, false);
-                    });
-                }, false);
-            })();*/
 		}
     })
 }
