891a6c06de4ad0eb500f7046dffe9dd93fa93310
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-06-21 00:23:03 -0600

    Added ContentType to the FieldContainerListEntry

--- Catfish.Core/Models/Contents/ViewModels/ListEntries/FieldContainerListEntry.cs
+++ Catfish.Core/Models/Contents/ViewModels/ListEntries/FieldContainerListEntry.cs
@@ -10,12 +10,14 @@ namespace Catfish.Core.Models.Contents.ViewModels.ListEntries
         public Guid Id { get; set; }
         public MultilingualText Name { get; protected set; }
         public MultilingualText Description { get; protected set; }
+        public string ContentType { get; set; }
 
         public FieldContainerListEntry(FieldContainer form)
         {
             Id = form.Id;
             Name = form.Name;
             Description = form.Description;
+            ContentType = form.GetType().FullName;
         }
     }
 }
