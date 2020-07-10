1c6bd46bfe95cf4f9e6f83d322fc4e0a00cf2d3e
Melania Ruaini <melaniar@hotmail.com>
2020-07-10 11:47:40 -0600

    Cat-691 - Replace BlogPost/BlogArchive with StandardPost/StandardArcive, Add Comment, allow user to submit comment when enabled. Delete Main page model -- it's for testing initially.

---
+++ Catfish/Models/StandardPost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+
+namespace Catfish.Models
+{
+    [PostType(Title = "Standard post")]
+    public class StandardPost : Post<StandardPost>
+    {
+        /// <summary>
+        /// Gets/sets the available comments if these
+        /// have been loaded from the database.
+        /// </summary>
+        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
