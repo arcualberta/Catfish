42f2cdf32945f9e43a3019aea0e4f642502794b1
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-07-14 10:31:05 -0600

    Started the implementation of the authorization workflow. Created the page-model base class.

---
+++ Catfish/Pages/CatfishPageModel.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catfish.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Catfish.Pages
+{
+    public class CatfishPageModelModel : PageModel
+    {
+        protected IAuthorizationService AuthorizationSertvice { get; set; }
+        public CatfishPageModelModel(IAuthorizationService auth)
+        {
+            AuthorizationSertvice = auth;
+        }
+    }
+}
---
+++ Catfish/Pages/StartPage.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catfish.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Catfish.Pages
+{
+    public class StartPageModel : CatfishPageModelModel
+    {
+        public StartPageModel(IAuthorizationService auth) : base(auth)
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
