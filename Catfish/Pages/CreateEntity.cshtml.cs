6d9cb88ee7fff600a436cb61bff5d4b6ca431914
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-08-11 00:20:34 -0600

    Implementing the OnPost method

--- Catfish/Pages/CreateEntity.cshtml.cs
+++ Catfish/Pages/CreateEntity.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Catfish.Core.Models;
+using Catfish.Core.Models.Contents;
 using Catfish.Core.Models.Contents.Data;
 using Catfish.Core.Models.Contents.Fields;
 using Catfish.ModelBinders;
@@ -16,7 +17,10 @@ namespace Catfish.Pages
     {
         private readonly IWorkflowService _workflowService;
         private readonly IEntityTemplateService _entityTemplateService;
+
+        [BindProperty]
         public DataItem Item { get; set; }
+        [BindProperty]
         public Guid TemplateId { get; set; }
              
         public CreateEntity(IWorkflowService workflow, IEntityTemplateService temp) : base(null, null)
@@ -27,7 +31,7 @@ namespace Catfish.Pages
         public void OnGet(Guid templateId)
         {
             TemplateId = templateId;
-            EntityTemplate template = _entityTemplateService.GetTemplate(templateId);
+            EntityTemplate template = _entityTemplateService.GetTemplate(TemplateId);
 
             _workflowService.SetModel(template);
 
@@ -35,9 +39,14 @@ namespace Catfish.Pages
             Item = _workflowService.GetDataItem("Contract Letter", false);
         }
 
-        public void OnPost(DataItem item)
+        public void OnPost()
         {
-            Item = item;
+            //Creating a clone of the entity
+            EntityTemplate template = _entityTemplateService.GetTemplate(TemplateId);
+            Entity entity = template.Clone<Entity>();
+
+            //updating the data item of the entity
+
             ////var val_1 = Request.Form["DataItem.Fields[0].Values[0].Values[0].Value"];
             ////var val_2 = Request.Form["DataItem.Fields[1].Values[0].Values[0].Value"];
         }
