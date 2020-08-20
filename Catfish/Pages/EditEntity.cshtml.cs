5457903d9b7d518024391499d549167ee5b3f56e
Isuru Wickramasinghe <iwickram@ualberta.ca>
2020-08-20 14:42:34 -0600

    Edit function changes

--- Catfish/Pages/EditEntity.cshtml.cs
+++ Catfish/Pages/EditEntity.cshtml.cs
@@ -14,18 +14,29 @@ namespace Catfish.Pages
     public class EntityEditPageModel : CatfishPageModelModel
     {
         private readonly IWorkflowService _workflowService;
+        private readonly ISubmissionService _submissionService;
         private readonly AppDbContext _db;
 
         [BindProperty]
         public DataItem Item { get; set; }
+        [BindProperty]
+        public Guid ItemId { get; set; }
         public EntityEditPageModel(IAuthorizationService auth, ISubmissionService serv, IWorkflowService workflow, AppDbContext db) : base(auth, serv)
         {
             _workflowService = workflow;
+            _submissionService = serv;
             _db = db;
         }
         public void OnGet(Guid id)
         {
-            Entity Item = _db.Items.Where(it => it.Id == id).FirstOrDefault();
+            ItemId = id;
+
+
+            Item item = _submissionService.GetSubmissionDetails(id);
+            _workflowService.SetModel(item);
+
+            //TODO: dynamically figure out the start-up item
+            Item = item.GetRootDataItem(false);
 
         }
 
