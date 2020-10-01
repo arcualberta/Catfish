dd391d198b087ec275529e3b9c6f81ebb5ae641d
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-10-01 01:17:48 -0600

    Added code to pass on the CollectionId.

--- Catfish/Pages/CreateEntity.cshtml.cs
+++ Catfish/Pages/CreateEntity.cshtml.cs
@@ -26,7 +26,10 @@ namespace Catfish.Pages
 
         [BindProperty]
         public Guid TemplateId { get; set; }
-             
+
+        [BindProperty]
+        public Guid CollectionId { get; set; }
+
         public CreateEntity(IWorkflowService workflow, IEntityTemplateService temp, AppDbContext db) : base(null, null)
         {
             _workflowService = workflow;
@@ -34,12 +37,14 @@ namespace Catfish.Pages
             _db = db;
         }
         
-        public void OnGet(Guid templateId)
+        public void OnGet(Guid templateId, Guid collectionId)
         {
             TemplateId = templateId;
             EntityTemplate template = _entityTemplateService.GetTemplate(TemplateId);
 
             Item = template.GetRootDataItem(false);
+
+            CollectionId = collectionId;
         }
 
         public IActionResult OnPost()
