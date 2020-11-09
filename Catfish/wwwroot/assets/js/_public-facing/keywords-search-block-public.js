5de31ab0ffd7b873e9d588b4d92909f5cd8a2c0b
Jill <jlovas@ualberta.ca>
2020-11-09 11:37:41 -0700

    pagination with just previous and next button working

--- Catfish/wwwroot/assets/js/_public-facing/keywords-search-block-public.js
+++ Catfish/wwwroot/assets/js/_public-facing/keywords-search-block-public.js
@@ -9,38 +9,11 @@
             pagesTotal: 0,
             currentPage: 1,
             cardsPerPage: 3,
-            pagesBeforeEllipsis: 5,
             searchMade: false,
             loadingSearchResults: false,
-            //for when there are a lot of pages, show '...' instead for some in the pagination
-            usePaginationEllipsis: false,
-            //for when there's ellipsis, need to track the position
-            pageAdvancement: 0,
 		}
     },
     methods: {
-
-        clickpreviousButton() { },
-
-        clickNextButton() { },
-
-        /**
-         * 
-         **/
-        clickPageNumber(index) {
-            this.currentPage = index;
-            if (!this.usePaginationEllipsis) {
-                return;
-            } else {
-                if (index > this.pagesBeforeEllipsis - 2) {
-                    this.pageAdvancement++;
-
-
-                } else {
-                    this.pageAdvancement--;
-				}
-			}
-        },
 
         /**
          * Stores/removes clicked filter button to call API again
@@ -88,13 +61,7 @@
                 .then(response => response.json())
                 .then(data => {
                     this.searchResults = data;
-                    let tmp = JSON.parse(JSON.stringify(data));
-                    this.searchResults = this.searchResults.concat(tmp); //tmp
                     this.pagesTotal = Math.ceil(this.searchResults.length / 3);
-                    //check to add ellipsis to pagination if pages > 5
-                    if (this.pagesTotal > this.pagesBeforeEllipsis) {
-                        this.usePaginationEllipsis = true;
-					}
 
                     this.searchMade = true;
                     this.loadingSearchResults = false;
