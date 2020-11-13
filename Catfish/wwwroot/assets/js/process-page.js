0fb4b4a4ffcbcd8ad3d30a90622edc56b57ede3a
Jill <jlovas@ualberta.ca>
2020-11-13 10:55:59 -0700

    made process page index button change to indexing after post call

--- Catfish/wwwroot/assets/js/process-page.js
+++ Catfish/wwwroot/assets/js/process-page.js
@@ -5,17 +5,26 @@
     data() {
         return {
             postApi: "/api/solr/index",
+            currentSite: null,
 
             hasErrorOccurred: false
         }
     },
     methods: {
         /**
+         * Sets the current index of the site possibly being indexed
+         * (possibly because there is a modal that appears next)
+         * @param {any} siteIndex
+         */
+        setCurrentSite(siteIndex) {
+            this.currentSite = siteIndex;
+        },
+        /**
          * Makes a POST call to index the given site 
          * @param siteId the site's Id (GUID)
          * @param siteTypeId the site's type Id
          **/
-        indexTheSite(siteId, siteTypeId) {
+        indexTheSite(siteId, siteTypeId, buttonIndex) {
             let formData = new FormData();
             formData.append("siteId", siteId);
             formData.append("siteTypeId", siteTypeId);
@@ -28,6 +37,10 @@
                     if (!this.hasErrorOccurred) {
                         this.hasErrorOccurred = false;
                     }
+
+                    //set button to 'indexing'
+                    document.getElementById('siteIndex-' + this.currentSite).innerHTML = "Indexing";
+                    document.getElementById("close-modal-button").click();
 
                 })
                 .catch(error => {
