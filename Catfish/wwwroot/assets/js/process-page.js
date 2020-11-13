bf2e245ca521141f3fb5327ab9a33dc34edf7bad
Jill <jlovas@ualberta.ca>
2020-11-12 17:01:37 -0700

    added post call to processes page

---
+++ Catfish/wwwroot/assets/js/process-page.js
@@ -0,0 +1,39 @@
+﻿piranha.processPage = new Vue({
+    el: '#process-page-container',
+    components: {
+    },
+    data() {
+        return {
+            postApi: "/api/solr/index",
+
+            hasErrorOccurred: false
+        }
+    },
+    methods: {
+        /**
+         * Makes a POST call to index the given site 
+         * @param siteId the site's Id (GUID)
+         * @param siteTypeId the site's type Id
+         **/
+        indexTheSite(siteId, siteTypeId) {
+            let formData = new FormData();
+            formData.append("siteId", siteId);
+            formData.append("siteTypeId", siteTypeId);
+
+            fetch(this.postApi, {
+                method: 'POST',
+                body: formData
+            })
+                .then(() => {
+                    if (!this.hasErrorOccurred) {
+                        this.hasErrorOccurred = false;
+                    }
+
+                })
+                .catch(error => {
+                    console.error("Failed to fetch: ", error);
+                    this.hasErrorOccurred = true;
+                })
+        }
+    }
+})
