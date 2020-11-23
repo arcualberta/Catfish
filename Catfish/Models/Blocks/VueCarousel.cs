bd742209f17d110e26162aa3c06b8337fb55c04e
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-11-23 14:59:35 -0700

    Started the initial structure for the VueCarousel block.

---
+++ Catfish/Models/Blocks/VueCarousel.cs
@@ -0,0 +1,23 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Models.Blocks
+{
+    [BlockType(Name = "Carousel", Category = "Media", Component = "vue-carousel", Icon = "fas fa-images")]
+    public class VueCarousel : VueComponent
+    {
+        public List<CarouselSlide> Slides { get; set; }
+        public override object GetData()
+        {
+            return Slides;
+        }
+    }
+
+    public class CarouselSlide
+    {
+
+    }
+}
