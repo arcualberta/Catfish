071584aa6d8308190ea357655ad93938bee53446
Melania Ruaini <melaniar@hotmail.com>
2020-12-01 15:54:08 -0700

    Allow "standandPage" template to be hook to secure page. Add page settings to allow user to control when to published the page

--- Catfish/Models/StandardPage.cs
+++ Catfish/Models/StandardPage.cs
@@ -1,7 +1,10 @@
 using Catfish.Models.Fields;
+using Catfish.Models.Regions;
 using Piranha.AttributeBuilder;
 using Piranha.Extend;
+using Piranha.Extend.Fields;
 using Piranha.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Catfish.Models
@@ -15,6 +18,25 @@ namespace Catfish.Models
         [Region(Title = "Categories", Display = RegionDisplayMode.Setting)]
         public ControlledCategoriesField Categories { get; set; } = new ControlledCategoriesField();
         public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
+
+        [Region(Display = RegionDisplayMode.Setting)]
+        public PublishSettings PublishSettings { get; set; } = new PublishSettings();
+
+        //public StandardPage()
+        //{
+        //    PublishSettings = new PublishSettings() { CustomMessage = "", PublishedEnd = null, PublishedStart = null };
+        //}
+        //[Region(Title = "Publish Settings", Display = RegionDisplayMode.Setting)]
+        //[Field(Title = "Publish Date Start")]
+        //public DateField PublishedStart { get; set; }
+
+        //[Region(Title = "Publish Settings", Display = RegionDisplayMode.Setting)]
+        //[Field(Title = "Publish Date End")]
+        //public DateField PublishedEnd { get; set; }
+
+        //[Region(Title = "Publish Settings", Display = RegionDisplayMode.Setting)]
+        //[Field(Title = "Custom Message", Placeholder = "Custom message to be displayed if the page is still not availabe.")]
+        //public StringField CustomMessage { get; set; }
 
     }
 }
