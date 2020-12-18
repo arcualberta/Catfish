46d019e4b54e282b7baf40241c16a694c0d4ede2
Jill <jlovas@ualberta.ca>
2020-12-17 17:12:08 -0700

    more style changes for example project page, added all js scripts from all pages to custom.js just in case

--- Catfish/wwwroot/assets/js/_public-facing/scripts/custom.js
+++ Catfish/wwwroot/assets/js/_public-facing/scripts/custom.js
@@ -47,4 +47,87 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementsByClassName("footer-arc-text")[0].classList.add('col-md-12');
     [...document.getElementsByClassName("footer-arc-logo")].map(n => n && n.remove());
 });
+
+
+
+//this code is for the example Work page ie Subline
+document.addEventListener("DOMContentLoaded", function(event) {
+//changes the columns in Meet Our Team into col-md-4's, from col-md-3's
+let columns = document.getElementsByClassName('row columnBlock CBflex-container');
+
+columns[0].classList.add('grey-bg');
+columns[0].classList.add('s-work-page');
+
+for(let i = 0; i < columns.length; i++){
+  columns[i].style.marginBottom = "20px";
+  if(i > 0){
+    columns[i].classList.add('box-shadow');
+    columns[i].classList.add('white-background-only');
+  }else{
+    columns[i].children[1].children[0].classList.add('our-features-container');
+  }
+  for(let j = 0; j < columns[i].children.length; j++){
+    //this is the columns section under the wide image
+    if(i > 0){
+      columns[i].children[j].children[0].classList.add('column-container');
+    }
+    columns[i].children[j].children[0].classList.add('no-box-shadow');
+    columns[i].children[j].children[0].classList.remove('col-md-12');
+    columns[i].children[j].classList.add('remove-padding-in-highlights');
+    columns[i].children[j].classList.add('remove-bottom-padding');
+  }
+}
+
+columns[0].children[0].children[0].style.backgroundColor = "unset";
+
+});
+
+
+//this goes into the Our Team page
+document.addEventListener("DOMContentLoaded", function(event) {
+//changes the columns in Meet Our Team into col-md-4's, from col-md-3's
+let columns = document.getElementsByClassName('row columnBlock CBflex-container');
+
+columns[0].classList.add('grey-bg');
+columns[0].classList.add('box-shadow');
+columns[0].classList.add('white-background');
+
+for(let i = 0; i < columns[0].children.length; i++){
+  //this is the columns section under the wide image
+  columns[0].children[i].classList.remove('col-md-3');
+  columns[0].children[i].classList.add('col-md-4');
+  columns[0].children[i].children[0].classList.add('no-box-shadow');
+  columns[0].children[i].children[0].classList.add('column-container');
+  columns[0].children[i].children[0].classList.remove('col-md-12');
+}
+
+});
+
+
+//this is for the Our Work page
+document.addEventListener("DOMContentLoaded", function(event) {
+//changes the background of the keywords block to grey
+let keywordParent = document.getElementsByClassName('row custom-keywords');
+keywordParent[0].classList.add('grey-bg');
+keywordParent[0].children[0].classList.add('box-shadow');
+keywordParent[0].children[0].classList.add('white-background');
+keywordParent[0].children[0].classList.add('add-full-flexbox');
+
+//adds styles for checkboxes part
+let checkboxGroup = document.getElementsByClassName('checkboxgroup');
+checkboxGroup[0].children[0].classList.remove('row');
+let categoriesSection = document.getElementsByClassName('categories-section');
+categoriesSection[0].classList.add('flex-column-no-align');
+let blockTitle = document.getElementsByClassName('block-title');
+blockTitle[0].classList.remove('container');
+let formPart = document.getElementById('controlledVocabularySearchForm');
+formPart.children[0].classList.remove('container');
+
+//changes search result header to regular div to match the one on the left
+let newDiv = document.createElement("div");
+newDiv.innerHTML = "Search Results";
+let searchResultsParent = document.getElementsByClassName('search-header-holder');
+searchResultsParent[0].children[0].replaceWith(newDiv);
+});
+
  */
