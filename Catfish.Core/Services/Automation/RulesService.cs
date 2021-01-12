788b155fc9f2fa8cedc272fcfbab0660b42de8bb
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-01-12 11:57:25 -0700

    Started creating the RulesService class.

---
+++ Catfish.Core/Services/Automation/RulesService.cs
@@ -0,0 +1,19 @@
+﻿using Catfish.Core.Models;
+using ElmahCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catfish.Core.Services.Automation
+{
+    public class RulesService : DbEntityService
+    {
+        public RulesService(AppDbContext db, ErrorLog errorLog)
+        : base(db, errorLog)
+        {
+
+        }
+
+        public 
+    }
+}
