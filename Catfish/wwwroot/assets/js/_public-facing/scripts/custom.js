52b06cdead736f3ab97397e979cd8711021f6f68
Jill <jlovas@ualberta.ca>
2021-02-08 14:59:07 -0700

    custom js updated for website

--- Catfish/wwwroot/assets/js/_public-facing/scripts/custom.js
+++ Catfish/wwwroot/assets/js/_public-facing/scripts/custom.js
@@ -84,13 +84,15 @@ for(let i = 0; i < columns.length; i++){
 columns[0].children[0].children[0].style.backgroundColor = "unset";
 
 });
+
+
+
+
+//This is for the Our Team page
+
 document.addEventListener("DOMContentLoaded", function(event) {
 //changes the columns in Meet Our Team into col-md-4's, from col-md-3's
 let columns = document.getElementsByClassName('row columnBlock CBflex-container');
-
-columns[0].classList.add('grey-bg');
-columns[0].classList.add('box-shadow');
-columns[0].classList.add('white-background');
 
 //for the Our Partners section
 columns[columns.length-1].classList.add('box-shadow');
@@ -100,17 +102,21 @@ columns[columns.length -1].style.marginBottom = "20px";
 
 
 for(let i = 0; i < columns.length; i++){
+  columns[i].classList.add('grey-bg');
+  columns[i].classList.add('box-shadow');
+  columns[i].classList.add('white-background');
+  columns[i].style.marginBottom = "20px";
   for(let j = 0; j < columns[i].children.length; j++){
     columns[i].children[j].classList.remove('col-md-3');
     //if it the title is inside Our Partners, make it full-sized col-12 and remove the grey bg
-    if(i == 1 && j == 0){
+    if(i == 3 && j == 0){
       columns[i].children[j].classList.add('col-md-12');
       columns[i].children[j].children[0].children[0].classList.remove('grey-bg');
     }else{
       columns[i].children[j].classList.add('col-md-4');
     }
     //some custom resizing of Cybera, IST, and WestGrid
-    if( (i == 1) && (j == 2 || j ==4 || j == 6) ){
+    if( (i == 3) && (j == 2 || j ==4 || j == 6) ){
 console.log("here");
       columns[i].children[j].children[0].children[0].style.maxWidth = "75%";
     }
