2841beb79fe51dc20cb440e31f87f07fcce4bb0a
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-02-09 17:30:45 -0700

    Implemented a basic version of the visible-if and required-if functionality.

--- Catfish/wwwroot/assets/js/_public-facing/scripts/expression-functions.js
+++ Catfish/wwwroot/assets/js/_public-facing/scripts/expression-functions.js
@@ -1,8 +1,7 @@
 ﻿function updateFields(triggerFieldId, element) {
 
     //let triggeredFieldValue = $(element).val();
-
-    var visibleIfFields = $("input[data_visible_if], textarea[data_visible_if]");
+    var visibleIfFields = $("input[data_visible_if], textarea[data_visible_if], select[data_visible_if]");
     for (i = 0; i < visibleIfFields.length; ++i) {
         let field = visibleIfFields[i];
         let expression = $(field).attr("data_visible_if");
@@ -20,24 +19,24 @@
     }
 
 
-    //var visibleIfFields = $("input[data_visible_if], textarea[data_visible_if]");
-    //for (i = 0; i < visibleIfFields.length; ++i) {
-    //    let field = visibleIfFields[i];
-    //    let expression = $(field).attr("data_visible_if");
-    //    if (expression) {
-    //        let fieldId = $(field).attr("data_field_id");
-    //        let result = evaluateExprssion(expression, triggerFieldId, element);
-    //        if (result) {
-    //            $("#" + fieldId + " span[.required]").html("*")
-    //        }
-    //        else {
-    //            $("#" + fieldId + " span[.required]").html("")
-    //        }
-    //    }
-    //    console.log("Expression: " + expression);
-    //}
-
-
+    var requiredIfFields = $("input[data_required_if], textarea[data_required_if], select[data_required_if]");
+    for (i = 0; i < requiredIfFields.length; ++i) {
+        let field = requiredIfFields[i];
+        let expression = $(field).attr("data_required_if");
+        if (expression) {
+            let fieldId = $(field).attr("data_field_id");
+            let result = evaluateExprssion(expression, triggerFieldId, element);
+            if (result) {
+                $("#" + fieldId + " span.required").show();
+                $("#" + fieldId + " input[data_required_if]").prop('required', true);
+            }
+            else {
+                $("#" + fieldId + " span.required").hide();
+                $("#" + fieldId + " input[data_required_if]").prop('required', false);
+            }
+        }
+        console.log("Expression: " + expression);
+    }
 }
 
 function evaluateExprssion(exp, triggerFieldId, element) {
