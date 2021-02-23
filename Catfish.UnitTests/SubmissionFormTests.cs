214457efa7d53cb8632805a3fc36e00b4429bc68
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-02-22 17:20:07 -0700

    Implemented required field for the TableField.

--- Catfish.UnitTests/SubmissionFormTests.cs
+++ Catfish.UnitTests/SubmissionFormTests.cs
@@ -233,12 +233,12 @@ namespace Catfish.UnitTests
 
             TableField tf = form.CreateField<TableField>("Product List", lang, false, 2, 10);
             tf.TableHead.CreateField<DateField>("Date", lang);
-            tf.TableHead.CreateField<TextField>("Product", lang);
+            tf.TableHead.CreateField<TextField>("Product", lang, true);
             tf.TableHead.CreateField<TextArea>("Description", lang);
             tf.TableHead.CreateField<DecimalField>("Unit Price", lang);
             tf.TableHead.CreateField<IntegerField>("Quantity", lang);
             tf.TableHead.CreateField<DecimalField>("Total", lang);
-            tf.TableHead.CreateField<RadioField>("Availability", lang, new string[] { "Available", "Not available" });
+            tf.TableHead.CreateField<RadioField>("Availability", lang, new string[] { "Available", "Not available" }, true);
             tf.TableHead.CreateField<CheckboxField>("Category", lang, new string[] { "Health", "Prescription", "Beauty", "Nutrition" });
 
             tf.AppendRows(4);
