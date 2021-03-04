7b63375303fdcbd5e25c501c45bf464312b854ce
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-03-04 15:23:09 -0700

    Bug fixes in deleting table rows. Implemented CompositeFieldSum.

--- Catfish/wwwroot/assets/js/_public-facing/scripts/expression-functions.js
+++ Catfish/wwwroot/assets/js/_public-facing/scripts/expression-functions.js
@@ -67,7 +67,7 @@
         let field = computedFields[i];
         let expression = $(field).attr("data-value-expression");
         if (expression) {
-            console.log(expression)
+            //console.log(expression)
 
             let result = eval(expression);
             $(field).val(result);
