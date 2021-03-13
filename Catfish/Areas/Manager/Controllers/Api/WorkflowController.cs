670ba900651d35b1f8e85e8d19ee55f70f7418a3
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-03-13 10:22:40 -0700

    Created basic code skeletons for the Workflow Editor.

---
+++ Catfish/Areas/Manager/Controllers/Api/WorkflowController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Areas.Manager.Controllers.Api
+{
+    [Route("manager/api/[controller]")]
+    [ApiController]
+    public class WorkflowController : Controller
+    {
+    }
+}
---
+++ Catfish/Areas/Manager/Pages/WorkflowEditor/EntityTemplatePage.cs
@@ -0,0 +1,28 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Areas.Manager.Pages.WorkflowEditor
+{
+    public class EntityTemplatePage : PageModel
+    {
+        public readonly IEntityTypeService _srv;
+        public EntityTemplate Template { get; set; }
+
+        public EntityTemplatePage(IEntityTypeService srv)
+        {
+            _srv = srv;
+        }
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            //Template = _srv
+            throw new NotImplementedException();
+        }
+    }
+}
