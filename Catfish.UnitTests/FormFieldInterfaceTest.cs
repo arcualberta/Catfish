90b26969b4bace107cc1bffd8821373e1ac814d8
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-03-17 00:13:55 -0600

    Started working on implementing selenium interface testing class.

---
+++ Catfish.UnitTests/FormFieldInterfaceTest.cs
@@ -0,0 +1,68 @@
+﻿using Catfish.Core.Models;
+using Catfish.Core.Services;
+using Catfish.Tests.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Catfish.UnitTests
+{
+    class FormFieldInterfaceTest
+    {
+        private string _dataRoot = "..\\..\\..\\Data\\Schemas\\";
+        private AppDbContext _db;
+        private TestHelper _testHelper;
+        private IAuthorizationService _auth;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testHelper = new TestHelper();
+            _db = _testHelper.Db;
+            IAuthorizationService _auth = _testHelper.AuthorizationService;
+        }
+
+        public XElement LoadXml(string fileName)
+        {
+            var path = Path.Combine(_dataRoot, fileName);
+            if (!File.Exists(path))
+                Assert.Fail("File not found at " + path);
+
+            XElement xml = XElement.Parse(File.ReadAllText(path));
+            return xml;
+        }
+
+        public void RefreshDatabase()
+        {
+            //Deleting all entities in the Entity table
+            var entities = _db.Entities.ToList();
+            _db.Entities.RemoveRange(entities);
+            _db.SaveChanges();
+
+            //Reloading default collection
+            _db.Collections.Add(Collection.Parse(LoadXml("collection_1.xml")) as Collection);
+
+            //Reloading Item Templates
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("simple_form.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("visibleIf_requiredIf.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("visibleIf_options.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("composite_field.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("table_field.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("table_field2.xml")) as ItemTemplate);
+            _db.ItemTemplates.Add(ItemTemplate.Parse(LoadXml("grid_table.xml")) as ItemTemplate);
+
+            _db.SaveChanges();
+        }
+
+        [Test]
+        public void RefreshData()
+        {
+            RefreshDatabase();
+        }
+
+    }
+}
