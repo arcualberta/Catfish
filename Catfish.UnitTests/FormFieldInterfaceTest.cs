63cf2ccf5494f71a7a63a4a60a12cffc3b4e7ef5
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-03-23 18:02:09 -0600

    Created the basic code skelton for SeleniumHelper.cs Added Chrome, Firefox and Edge driver nugets but only the Chrome one is working so far.

--- Catfish.UnitTests/FormFieldInterfaceTest.cs
+++ Catfish.UnitTests/FormFieldInterfaceTest.cs
@@ -1,6 +1,5 @@
 ﻿using Catfish.Core.Models;
 using Catfish.Core.Services;
-using Catfish.Tests.Helpers;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -15,6 +14,7 @@ using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Interactions;
 using System.Reflection;
+using Catfish.UnitTests.Helpers;
 
 namespace Catfish.UnitTests
 {
@@ -23,18 +23,25 @@ namespace Catfish.UnitTests
         private string _dataRoot = "..\\..\\..\\Data\\Schemas\\";
         private AppDbContext _db;
         private TestHelper _testHelper;
+        private SeleniumHelper _seleniumHelper;
         private IAuthorizationService _auth;
         private string siteUrl = ConfigurationManager.AppSettings["SiteUrl"];
-        //IWebDriver driver = new ChromeDriver(".");
-        IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory);
         private string itemAtrib;
+
+        public FormFieldInterfaceTest()
+        {
+
+        }
 
         [SetUp]
         public void Setup()
         {
             _testHelper = new TestHelper();
+            _seleniumHelper = new SeleniumHelper(_testHelper.Configuration);
             _db = _testHelper.Db;
             IAuthorizationService _auth = _testHelper.AuthorizationService;
+
+            _seleniumHelper.SetDriver(SeleniumHelper.eDriverType.Chrome);
         }
 
         public XElement LoadXml(string fileName)
@@ -75,90 +82,69 @@ namespace Catfish.UnitTests
             RefreshDatabase();
         }
 
-        public void LoginTest()
-        {
-            IWebElement element = driver.FindElement(By.Name("pageGroup"));
-            Assert.NotNull(element);
-        }
+        //public void LoginTest()
+        //{
+        //    IWebElement element = driver.FindElement(By.Name("pageGroup"));
+        //    Assert.NotNull(element);
+        //}
 
         [Test]    
         public void Login()
         {
-            //driver.Navigate().GoToUrl(siteUrl + "/manager/");
-            driver.Navigate().GoToUrl("https://localhost:44385/manager/"); 
-            driver.FindElement(By.Name("username")).SendKeys("admin");
-            IWebElement element = driver.FindElement(By.Name("password"));
-            element.SendKeys("passwd");
-            element.Submit();
-
+            _seleniumHelper.LoginLocal("admin", "passwd");
         }
 
         [Test]
         public void SimpleFormSubmissionTest()
         {
-  
             RefreshData();
-            // got to home page first to check if logged in (as otherwise forms to be tested not assessable)
-            driver.Navigate().GoToUrl("https://localhost:44385/");
+            _seleniumHelper.LoginLocal();
 
+            //Navigating to the test page
+            _seleniumHelper.GoToUrl("simple-form");
 
-            List<IWebElement> elms = new List<IWebElement>();
-            elms.AddRange(driver.FindElements(By.Id("btnSignIn")));
+            //Selecting Option2 for DD1
+            _seleniumHelper.SelectDropdownOption("48cd8112-beea-4664-b5a9-739a79e652bc", "b8068a1b-a184-47f5-9da1-625e3eb4a2f4");
 
+            //TODO: implement the remaining steps defined in CAT-863
 
-            if (elms.Count > 0)  // sign in element found -  so sign-in but via /manager/pages URL instead of where link goes
-            {
-                Login();
-            }
-
-            //else can assume loggged it
-
-
-
-            //driver.Navigate().GoToUrl(siteUrl + "/simple-form/");
-            driver.Navigate().GoToUrl("https://localhost:44385/simple-form/");
-          
-
-            //Dropdown test
-
-            
             // IWebElement DDelement = driver.FindElement(By.Id("48cd8112-beea-4664-b5a9-739a79e652bc"));
 
 
-            //SelectElement selectDD1 = new SelectElement(DDelement);
-            // select dropdowm option 2
-           // selectDD1.SelectByIndex(1);
+            //////SelectElement selectDD1 = new SelectElement(DDelement);
+            ////// select dropdowm option 2
+            ////// selectDD1.SelectByIndex(1);
 
-            //Radio button test
-            // 
-            //IWebElement RBElement = driver.FindElement(By.Id("ef1c777b-6e80-48f6-b742-548f5226239c"));
+            //////Radio button test
+            ////// 
+            //////IWebElement RBElement = driver.FindElement(By.Id("ef1c777b-6e80-48f6-b742-548f5226239c"));
 
-            // first just select option 2 byID - could use index 1 after finding element.
-            IWebElement RBElement = driver.FindElement(By.Id("9e45b902-dba8-4828-b65d-9b8ac47a0954"));
-            RBElement.Click();
+            ////// first just select option 2 byID - could use index 1 after finding element.
+            ////IWebElement RBElement = driver.FindElement(By.Id("9e45b902-dba8-4828-b65d-9b8ac47a0954"));
+            ////RBElement.Click();
 
-            // test if radio button 2 selected 
-            //Boolean itemSlected = false;
-            //itemSlected = RBElement.isSelected();
-            
-            itemAtrib = RBElement.GetAttribute("checked");
+            ////// test if radio button 2 selected 
+            //////Boolean itemSlected = false;
+            //////itemSlected = RBElement.isSelected();
 
-
-
-            //checkbox test
-            // test if check boxes 3,4 are selected 
-            IWebElement CBElement = driver.FindElement(By.Id("391d611d-d7f5-48a6-b8ae-e52053294616"));
-            CBElement.Click();
-            IWebElement CBElement2 = driver.FindElement(By.Id("c320b902-21f4-4bf1-ba7e-67890f7a0849"));
-            CBElement2.Click();
-
-            //test if selected
+            ////itemAtrib = RBElement.GetAttribute("checked");
 
 
 
-            itemAtrib = CBElement.GetAttribute("checked");
+            //////checkbox test
+            ////// test if check boxes 3,4 are selected 
+            ////IWebElement CBElement = driver.FindElement(By.Id("391d611d-d7f5-48a6-b8ae-e52053294616"));
+            ////CBElement.Click();
+            ////IWebElement CBElement2 = driver.FindElement(By.Id("c320b902-21f4-4bf1-ba7e-67890f7a0849"));
+            ////CBElement2.Click();
 
-            itemAtrib = CBElement2.GetAttribute("checked");
+            //////test if selected
+
+
+
+            ////itemAtrib = CBElement.GetAttribute("checked");
+
+            ////itemAtrib = CBElement2.GetAttribute("checked");
 
 
         }
---
+++ Catfish.UnitTests/Helpers/SeleniumHelper.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Edge.SeleniumTools;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Catfish.UnitTests.Helpers
+{
+    public class SeleniumHelper
+    {
+        public enum eDriverType { Chrome, Firefox, Edge };
+
+        private readonly IConfiguration _configuration;
+        private readonly string _siteUrl;
+
+        public IWebDriver Driver { get; protected set; }
+
+        public SeleniumHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _siteUrl = _configuration.GetSection("SiteUrl").Value.TrimEnd('/');
+        }
+        public IWebDriver SetDriver(eDriverType driverType)
+        {
+            switch (driverType)
+            {
+                case eDriverType.Chrome:
+                    Driver = new ChromeDriver();
+                    break;
+                case eDriverType.Firefox:
+                    Driver = new FirefoxDriver();
+                    break;
+                case eDriverType.Edge:
+                    Driver = new EdgeDriver();
+                    break;
+                default:
+                    throw new Exception("No driver found");
+            }
+
+            return Driver;
+        }
+
+        public void LoginLocal(string username = null, string password = null)
+        {
+            if (string.IsNullOrEmpty(username))
+                username = _configuration.GetSection("Login:LocalAdmin:User").Value;
+
+            if (string.IsNullOrEmpty(password))
+                password = _configuration.GetSection("Login:LocalAdmin:Password").Value;
+
+            Driver.Navigate().GoToUrl(_siteUrl + "/manager");
+            Driver.FindElement(By.Name("username")).SendKeys(username);
+            Driver.FindElement(By.Name("password")).SendKeys(password);
+            Driver.FindElement(By.Name("password")).Submit();
+
+            // got to home page and make sure the sign-out button is there insetad of the sign-in button
+            GoToUrl("/");
+            List<IWebElement> elms = new List<IWebElement>();
+            elms.AddRange(Driver.FindElements(By.Id("btnSignIn")));
+            Assert.IsTrue(elms.Count == 0, "Error: a sign-in button was found after log-in");
+
+        }
+
+        public void GoToUrl(string path)
+        {
+            if (!path.StartsWith('/'))
+                path = "/" + path;
+            Driver.Navigate().GoToUrl(_siteUrl + path);
+        }
+
+        public IWebElement GetElementByDataModelId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebElement GetElementByValue (string val)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// Select the field identified by data-model-id=fieldId and then selects its option
+        /// identified by value=optionId
+        /// </summary>
+        /// <param name="fieldId"></param>
+        /// <param name="optionId"></param>
+        public void SelectDropdownOption(string fieldId, string optionId)
+        {
+            var dd = GetElementByDataModelId(fieldId);
+
+            var option = GetElementByValue(optionId);
+
+        }
+
+        public void SelectCheckboxOption(string fieldId, string optionId)
+        {
+            var chk = GetElementByDataModelId(fieldId);
+
+            var option = GetElementByValue(optionId);
+
+        }
+
+        public void SelectRadioOption(string fieldId, string optionId)
+        {
+            var dd = GetElementByDataModelId(fieldId);
+
+            var option = GetElementByValue(optionId);
+
+        }
+
+        public void SetTextFieldValue(string fieldId, string value)
+        {
+
+        }
+
+        public void SetTextAreaValue(string fieldId, string value)
+        {
+
+        }
+
+        public void ClickButton (string buttonId)
+        {
+
+        }
+
+    }
+}
