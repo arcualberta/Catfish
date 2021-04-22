c1d25a9fdd0c4b290b1dde9a746bb57dcd9e0b49
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-04-22 13:37:51 -0600

    Created code skeleton for the BusinessContinuityPlanFormBuilderTests.cs

---
+++ Catfish.UnitTests/BusinessContinuityPlanFormBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using Catfish.Core.Models;
+using Catfish.Test.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catfish.UnitTests
+{
+    public class BusinessContinuityPlanFormBuilderTests
+    {
+        private protected AppDbContext _db;
+        private protected TestHelper _testHelper;
+        string[] YesNoOptionsText = new string[] { "Yes", "No" };
+
+        [SetUp]
+        public void Setup()
+        {
+            _testHelper = new TestHelper();
+            _db = _testHelper.Db;
+        }
+
+
+    }
+}
