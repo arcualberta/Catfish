4d565a7889e086175173d6dbcf82677b6e10cde4
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-05-07 15:57:33 -0600

    Bug fix: ensures the vue component JS file is added to the layout even if they are nested inside other container blocks (such as column)

---
+++ Catfish/Helper/BlockHelper.cs
@@ -0,0 +1,32 @@
+﻿using Catfish.Models;
+using Catfish.Models.Blocks;
+using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Helper
+{
+    public class BlockHelper: IBlockHelper
+    {
+        public IEnumerable<string> GetVueComponentNames(IList<Block> blocks)
+        {
+            IEnumerable<string> names = blocks.Where(b => typeof(IVueComponent).IsAssignableFrom(b.GetType())) //Selecting all blocks of type IVueConponent or its derived type
+                .Select(b => (b as IVueComponent).Component) //Selecting all those blocks as IVueConponent blocks and getting their names.
+                .Distinct()
+                .ToList();
+
+            var blockGroups = blocks.Where(b => typeof(BlockGroup).IsAssignableFrom(b.GetType()))
+                .Select(b => b as BlockGroup);
+
+            foreach (var blockGroup in blockGroups)
+            {
+                var childNames = GetVueComponentNames(blockGroup.Items);
+                names = names.Union(childNames);
+            }
+
+            return names;
+        }
+    }
+}
