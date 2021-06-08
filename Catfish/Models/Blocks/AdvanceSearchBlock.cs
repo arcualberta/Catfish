2f9f4a74beab4777355e790ff075a18bac4c20f3
mruaini <melaniar@hotmail.com>
2021-06-08 15:28:24 -0600

    Modified AdvanceBlock to include "max word" limit for Slip type Body content when the content is too long

--- Catfish/Models/Blocks/AdvanceSearchBlock.cs
+++ Catfish/Models/Blocks/AdvanceSearchBlock.cs
@@ -30,5 +30,7 @@ namespace Catfish.Models.Blocks
         //link can be link to Title
         public TextAreaField SelectedUrlFieldId { get; set; }
         public TextAreaField SelectedThumbnailFieldId { get; set; }
+
+        public NumberField MaxWords { get; set; } = 100;
     }
 }
