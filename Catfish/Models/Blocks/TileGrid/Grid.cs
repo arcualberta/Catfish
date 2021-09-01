6cbd7d4e2c99cb5e8fde8145f546df8c81beb653
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-08-31 20:04:46 -0600

    Added code skeleton for the TileGrid data models.

---
+++ Catfish/Models/Blocks/TileGrid/Grid.cs
@@ -0,0 +1,13 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Models.Blocks.TileGrid
+{
+    public class Grid : Block
+    {
+        public ICollection<Tile> Tiles { get; set; } = new List<Tile>();
+    }
+}
---
+++ Catfish/Models/Blocks/TileGrid/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catfish.Models.Blocks.TileGrid
+{
+    public class Tile
+    {
+        public Guid Id { get; set; }
+    }
+}
