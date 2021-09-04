cd5442f235199a807706724a3dcc7fdf9ec20d57
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-09-03 20:11:40 -0600

    Task 207: Tiles API Controller Skeleton

---
+++ Catfish/Controllers/Api/TilesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catfish.Models.Blocks.TileGrid;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Catfish.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TilesController : ControllerBase
+    {
+        // GET: api/<TilesController>
+        [HttpGet]
+        public IEnumerable<Tile> Get(int? offset = null, int? max = null)
+        {
+            List<Tile> tiles = new List<Tile>();
+
+            if (!max.HasValue)
+                max = 20;
+
+            if (offset.HasValue && offset.Value > 0)
+                max = 10;
+
+            for (int i = 0; i < max; ++i)
+            {
+                tiles.Add(new Tile()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Item " + (i*max + 1),
+                    Content = "Content " + (i * max + 1),
+                    Date = DateTime.Now.AddDays(i),
+                    Thumbnail = "https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/dahlia-3598551_1920.jpg?itok=XZfJlur2",
+                    ObjectUrl = "https://www.almanac.com/plant/dahlias"
+                });
+            }
+
+            return tiles;
+        }
+    }
+}
--- Catfish/Models/Blocks/TileGrid/Tile.cs
+++ Catfish/Models/Blocks/TileGrid/Tile.cs
@@ -8,5 +8,10 @@ namespace Catfish.Models.Blocks.TileGrid
     public class Tile
     {
         public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime Date { get; set; }
+        public string ObjectUrl { get; set; }
     }
 }
