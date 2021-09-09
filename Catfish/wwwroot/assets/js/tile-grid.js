8dce81ef8fc35f3ac2d3f3e9572cc161f5c8b367
mruaini <melaniar@hotmail.com>
2021-09-09 10:14:40 -0600

    TASK 220: modified TileGrid block to accept a comma separated list keywords

--- Catfish/wwwroot/assets/js/tile-grid.js
+++ Catfish/wwwroot/assets/js/tile-grid.js
@@ -1,8 +1,26 @@
 ﻿Vue.component("tile-grid", {
     props: ["uid", "toolbar", "model"],
+    methods: {
+        onBlur: function (e) {
+            this.model.keywordList.value = e.target.value;
+
+            var content = this.model.keywordList.value;
+            if (content.length > 0) {
+                this.$emit('update-content', {
+                    uid: this.uid,
+                    keywordList: content
+                });
+            }
+
+        }
+    },
     template:
         `<div  class= 'block-body'>
             <h2>Tile Grid</h2>
+           <div>Please list your keywords separated by a comma.</div>
+         <textarea rows='4' cols='100' class='lead ' 
+            v-html='model.keywordList.value' contenteditable='true' v-on:blur='onBlur' >
+        </textarea>
          </div>`
 });
 
