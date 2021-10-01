d9436a1a3bc7264e75b660326846b2ac536a2864
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-10-01 00:23:09 -0600

    Fixed issues identified by Task 269: Verify that the submission data are indexed in the Solr Index

--- Catfish/wwwroot/Vue/tile-grid/src/store/defs/mutations.js
+++ Catfish/wwwroot/Vue/tile-grid/src/store/defs/mutations.js
@@ -1,16 +1,13 @@
-"use strict";
-var _a;
-exports.__esModule = true;
-exports.mutations = exports.Mutations = void 0;
 //Declare MutationTypes
-var Mutations;
+export var Mutations;
 (function (Mutations) {
     Mutations["SET_TILES"] = "SET_TILES";
-})(Mutations = exports.Mutations || (exports.Mutations = {}));
+})(Mutations || (Mutations = {}));
 //Create a mutation tree that implement all mutation interfaces
-exports.mutations = (_a = {},
-    _a[Mutations.SET_TILES] = function (state, payload) {
+export const mutations = {
+    [Mutations.SET_TILES](state, payload) {
         state.items = payload;
         console.log('Payload: ', payload);
     },
-    _a);
+};
+//# sourceMappingURL=mutations.js.map
