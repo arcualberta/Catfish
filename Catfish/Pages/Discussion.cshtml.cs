a33af61c69be5b04967d761a0eb3d3b0fbf6eafc
Isuru Wickramasinghe <iwickram@ualberta.ca>
2021-10-01 11:57:40 -0600

    Added Discussion.cshtml

---
+++ Catfish/Pages/Discussion.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catfish.Models.Blocks.TileGrid;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Catfish.Pages
+{
+    public class DiscussionModel : PageModel
+    {
+        [BindProperty]
+        public Tile Tile { get; set; }
+
+        public void OnGet(Guid id)
+        {
+
+        }
+    }
+}
