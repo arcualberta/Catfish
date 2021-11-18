8119db3b65e3b6728cb7b34390027958c33c3f44
mruaini <melaniar@hotmail.com>
2021-11-18 11:24:51 -0700

    TASK - 517: Update ItemList.vue

--- Catfish/Areas/Applets/UI/src/lib-components/keyword-search/components/ItemList.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/keyword-search/components/ItemList.vue
@@ -20,14 +20,24 @@
 <template>
     <div class="itemList">
         <h1>Result List</h1>
-        <div>
+        <div v-if="items?.length > 0">
             <div v-for="item in items">
                 <div>
                     <div v-for="item in items" :key="item.id">
-                        {{item.title}}
+                        <div class="item">
+                            <div class="itm-heading col-md-12">  {{item.title}}</div>
+                            <div class="itm-subHeading col-md-12">  {{item.subtitle}}</div>
+                            <div class="itm-content col-md-12">  {{item.content}}</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .item{
+      margin: 10px 0;
+    }
+</style>
