b0c51a6aa37f4a03c6dcf48fa05e6f6576ee8ff9
mruaini <melaniar@hotmail.com>
2021-11-19 10:55:56 -0700

    TASK-517 add basic styling

--- Catfish/Areas/Applets/UI/src/lib-components/keyword-search/components/ItemList.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/keyword-search/components/ItemList.vue
@@ -24,7 +24,7 @@
             <div v-for="item in items">
                 <div>
                     <div v-for="item in items" :key="item.id">
-                        <div class="item">
+                        <div v-if="item?.title != null" class="item">
                             <div class="itm-heading col-md-12">  {{item.title}}</div>
                             <div class="itm-subHeading col-md-12">  {{item.subtitle}}</div>
                             <div class="itm-content col-md-12">  {{item.content}}</div>
@@ -39,5 +39,17 @@
 <style scoped>
     .item{
       margin: 10px 0;
+    }
+    .itm-heading{
+        font-size: 1.1em;
+        background-color: LightGrey;
+    }
+    .itm_subheading{
+        font-size: 1em;
+        font-style: italic;
+        font-weight: bold;
+    }
+    itm-content{
+
     }
 </style>
