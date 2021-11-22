47cb26b62f4ab3ff91b82277a75c8c506882d767
Isuru Wickramasinghe <iwickram@ualberta.ca>
2021-11-22 09:39:54 -0700

    item-editor.js updated

--- Catfish/wwwroot/assets/js/item-editor.js
+++ Catfish/wwwroot/assets/js/item-editor.js
@@ -25,8 +25,8 @@
         
     },
     
-    template: "<div  class= 'block-body calendar-block'>" +
-        "<div class='lead row'><label class='form-label col-md-3 required'>Item ID: </label><input class='form-control col-md-8' type='text' name='itemId' v-model='model.itemId.value' contenteditable='true' v-on:blur='onBlur' value='itemIdValue'  :class='{ requiredField: isItemIdEmpty }' /></div>" +
-       // "<div class='lead row'><label class='form-label col-md-3 required'>Collection ID: </label><input class='form-control col-md-8' type='text' name='collectionId' v-model='model.collectionId.value' contenteditable='true' v-on:blur='onBlur' value='collectionIdValue'  :class='{ requiredField: isCollectionEmpty }' /></div>" +
-        "</div>"
+    template:
+        `<div  class= 'block-body'>
+            <h2>Item Editor</h2>
+       </div>`
 });
