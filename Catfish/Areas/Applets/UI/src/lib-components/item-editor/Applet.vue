59d99391f0163e65c7af4058013033114f5aac24
KamalRanaweera <ranaweer@ualberta.ca>
2021-11-28 12:30:31 -0700

    Bug fixes. Wired up the ItemTemplateEditor and ItemEditor applets to the EntityTypes and CollectionContents lists, respectively.

--- Catfish/Areas/Applets/UI/src/lib-components/item-editor/Applet.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/item-editor/Applet.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { defineComponent } from 'vue';
-    import props from '../shared/props'
+    import props, { QueryParameter } from '../shared/props'
 
     export default defineComponent({
         name: "ItemEditor",
@@ -11,16 +11,22 @@
             console.log('Editor setup ...')
             console.log('props: ', p)
             console.log('context: ', ctx)
+
+            const queryParameters = p.queryParameters as QueryParameter;
+
+            return {
+                queryParameters
+            }
         },
         mounted() {
             console.log('Editor mounted ...')
-        }
+      }
     });
 </script>
 
 <template>
     <div>
         <h2>Item Ediror</h2>
-        <div class="row">Item Id: {{itemId}}</div>
+        <div>Item ID: {{queryParameters.id}}</div>
     </div>
 </template>
