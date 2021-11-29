036fa02e372750c59d731725a86412beb25b89b9
KamalRanaweera <ranaweer@ualberta.ca>
2021-11-28 18:25:37 -0700

    Created basic ItemAppletService. Added an action to the ItemEditorController to retrieve an item by ID.

--- Catfish/Areas/Applets/Controllers/ItemEditorController.cs
+++ Catfish/Areas/Applets/Controllers/ItemEditorController.cs
@@ -1,9 +1,12 @@
 ﻿using Catfish.Areas.Applets.Models.Blocks.KeywordSearchModels;
+using Catfish.Areas.Applets.Services;
+using Catfish.Core.Exceptions;
 using Catfish.Core.Models;
 using Catfish.Core.Models.Contents.Workflow;
 using Catfish.Core.Services;
 using Catfish.Services;
 using ElmahCore;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,13 +19,16 @@ namespace Catfish.Areas.Applets.Controllers
     [ApiController]
     public class ItemEditorController : ControllerBase
     {
+        private readonly IItemAppletService _itemAppletService;
         private readonly ISubmissionService _submissionService;
         private readonly IEntityTemplateService _entityTemplateService;
         private readonly IWorkflowService _workflowService;
         private readonly AppDbContext _appDb;
         private readonly ErrorLog _errorLog;
-        public ItemEditorController(ISubmissionService submissionService, IEntityTemplateService entityTemplateService, IWorkflowService workflowService, AppDbContext appDb, ErrorLog errorLog)
+        public ItemEditorController(IItemAppletService itemAppletService, ISubmissionService submissionService, IEntityTemplateService entityTemplateService, IWorkflowService workflowService, AppDbContext appDb, ErrorLog errorLog)
         {
+            _itemAppletService = itemAppletService;
+
             _submissionService = submissionService;
             _entityTemplateService = entityTemplateService;
             _workflowService = workflowService;
@@ -53,7 +59,7 @@ namespace Catfish.Areas.Applets.Controllers
         [Route("{id:Guid}")]
         public Item Get(Guid id)
         {
-            Item item = _submissionService.GetSubmissionDetails(id);
+            Item item = _itemAppletService.GetItem(id, User);
 
             return item;
         }
---
+++ Catfish/Areas/Applets/Services/ItemAppletService.cs
@@ -0,0 +1,32 @@
+﻿using Catfish.Core.Models;
+using Catfish.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Catfish.Areas.Applets.Services
+{
+    public class ItemAppletService : IItemAppletService
+    {
+        private readonly ISubmissionService _submissionService;
+        public readonly AppDbContext _appDb;
+        public ItemAppletService(AppDbContext db, ISubmissionService submissionService)
+        {
+            _appDb = db;
+            _submissionService = submissionService;
+        }
+
+        public Item GetItem(Guid id, ClaimsPrincipal user)
+        {
+            Item item = _submissionService.GetSubmissionDetails(id);
+
+            //TODO #1: If the "user" does not have permission to read the item, throw a Catfish.Core.Exceptions.AuthorizationException
+
+            //TODO #2: Strip-off information from that should not be visible to "user" from the item.
+
+            return item;
+        }
+    }
+}
