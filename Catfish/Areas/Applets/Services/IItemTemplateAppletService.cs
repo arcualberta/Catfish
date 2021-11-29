b27dc85a9fc4837b77f92f47c03cb2592410f775
KamalRanaweera <ranaweer@ualberta.ca>
2021-11-28 19:24:03 -0700

    Created basic ItemTemplateAppletService an its interface. Renamed ItemTemplateEditorController to ItemTemplatesController.

---
+++ Catfish/Areas/Applets/Services/IItemTemplateAppletService.cs
@@ -0,0 +1,14 @@
+﻿using Catfish.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Catfish.Areas.Applets.Services
+{
+    public interface IItemTemplateAppletService
+    {
+        public ItemTemplate GetItemTemplate(Guid id, ClaimsPrincipal user);
+    }
+}
