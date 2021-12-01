5661f99ad22bdf40e10996b6ad6a26d399e74e57
mruaini <melaniar@hotmail.com>
2021-12-01 10:16:18 -0700

    TASK 606: update ItemTemplate/Field/FieldContainer models

--- Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/fieldContainer.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/fieldContainer.ts
@@ -1,14 +1,23 @@
 ﻿import { Guid } from "guid-typescript";
-import { Text } from "./textModels";
+import { Text, TextCollection} from "./textModels";
 
 export interface Field {
     id: Guid;
     modelType: string;
     values: Text[];
+    name: TextCollection | null;
+    required: boolean;
+    allowMultipleValues: boolean;
+    readonly: boolean;
+    refId: string;
+    description: TextCollection | null;
 }
 
 export interface FieldContainer {
     id: Guid;
     modelType: string;
     fields: Field[];
+    isRoot: boolean
+    name: TextCollection | null
+    description: TextCollection | null
 }
--- Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/itemTemplate.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/itemTemplate.ts
@@ -1,5 +1,6 @@
 ﻿import { Guid } from "guid-typescript";
-import { FieldContainer } from  "./fieldContainer";
+import { FieldContainer } from "./fieldContainer";
+import { TextCollection } from "./textModels"
 
 export interface ItemTemplate {
     id: Guid;
@@ -8,4 +9,7 @@ export interface ItemTemplate {
     modelType: string;
     metadatSets: FieldContainer[];
     dataContainer: FieldContainer[];
+    name: TextCollection | null;
+    description: TextCollection | null;
+    statusId: Guid | null;
 }
