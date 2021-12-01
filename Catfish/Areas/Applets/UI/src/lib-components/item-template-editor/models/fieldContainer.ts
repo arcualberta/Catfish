f4e69b0e83d014825dbd7429df99704a0b9855f6
mruaini <melaniar@hotmail.com>
2021-12-01 10:07:55 -0700

    TASK-605:Create Text Collection model

--- Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/fieldContainer.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/item-template-editor/models/fieldContainer.ts
@@ -1,16 +1,5 @@
 ﻿import { Guid } from "guid-typescript";
-
-
-export interface Text {
-    id: Guid;
-    modelType: string;
-    value: string;
-    format: string;
-    language: string;
-    rank: number;
-    created: Date;
-    updated: Date;
-}
+import { Text } from "./textModels";
 
 export interface Field {
     id: Guid;
@@ -22,5 +11,4 @@ export interface FieldContainer {
     id: Guid;
     modelType: string;
     fields: Field[];
-
 }
