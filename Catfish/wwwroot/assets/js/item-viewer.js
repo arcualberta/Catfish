cb1274d2584c8003446feafb53dd816a641ebcc7
KamalRanaweera <ranaweer@ualberta.ca>
2021-12-05 07:56:36 -0700

    Implemented Task 642: Add Piranha Block model and templates.

---
+++ Catfish/wwwroot/assets/js/item-viewer.js
@@ -0,0 +1,9 @@
+﻿Vue.component("item-viewer", {
+    props: ["uid", "toolbar", "model"],
+    methods: {
+    },
+    template:
+        `<div  class= 'block-body'>
+            <h2>Item Viewer</h2>
+       </div>`
+});
