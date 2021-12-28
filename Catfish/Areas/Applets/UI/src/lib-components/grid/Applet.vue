a2b42db60f07d54703ea01243f72b48b5ddf3a19
KamalRanaweera <ranaweer@ualberta.ca>
2021-12-27 17:56:07 -0700

    Work in progress of Task 731: Create basic Vue applet structure

---
+++ Catfish/Areas/Applets/UI/src/lib-components/grid/Applet.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import props from '../shared/props'
+
+    export default defineComponent({
+        name: "Grid",
+        components: {
+        },
+        props,
+        setup(p, ctx) {
+            console.log('Grid setup ...')
+            console.log('props: ', p)
+            console.log('context: ', ctx)
+        },
+        mounted() {
+            console.log('Grid mounted ...')
+        }
+    });
+</script>
+
+<template>
+    <div>
+        <h2>Grid</h2>
+        <div class="row">Page Id: {{pageId}}</div>
+        <div class="row">Block Id: {{blockId}}</div>
+        <div class="row">Data Attributes {{dataAttributes}}</div>
+    </div>
+</template>
