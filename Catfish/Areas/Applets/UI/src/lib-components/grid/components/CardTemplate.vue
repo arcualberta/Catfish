dc34e43a591b1e344becf4202f9ca147cf98c62d
KamalRanaweera <ranaweer@ualberta.ca>
2021-12-28 14:56:05 -0700

    Work-in progress of implementing CardTemplate.vue.

---
+++ Catfish/Areas/Applets/UI/src/lib-components/grid/components/CardTemplate.vue
@@ -0,0 +1,38 @@
+﻿<script lang="ts">
+	import { defineComponent, toRef, computed } from "vue";
+    import { Card } from "../../shared/models/cmsModels";
+
+
+    export default defineComponent({
+		name: "CardTemplate",
+
+        props: {
+            model: Card
+        },
+		setup(props) {
+
+            const model = toRef(props, 'model');
+
+            return {
+                model,
+                imageUrl: computed(() => model.value?.cardImage?.media?.publicUrl?.substring(1)),
+				imgStyle: computed(() => {
+                    const url = model.value?.cardImage?.media?.publicUrl;
+                    return { backgroundImage: "url(" + url?.substring(1) + ")" }
+				}),
+                popupImageUrl: computed(() => model.value?.modalImage?.media?.publicUrl),
+                containerHeight: 250
+            }
+        }
+    });
+</script>
+
+<template>
+    <div class="col-md-4 m-5">
+        <div v-bind:style="{backgroundImage:`url(${imageUrl})`, height:`${containerHeight}px`}" style="background-size:cover"> </div>
+
+        {{JSON.stringify(model)}}
+    </div>
+</template>
+
+
---
+++ Catfish/Areas/Applets/UI/src/lib-components/shared/models/cmsModels.ts
@@ -0,0 +1,93 @@
+﻿import { Guid } from "guid-typescript";
+
+export class Page {
+	id: Guid;
+	typeId: string;
+	title: string;
+	created: Date;
+	lastModified: Date;
+	permissions: string[];
+	isPublished: boolean;
+	published: Date;
+	permalink: string;
+	slug: string;
+	isCommentsOpen: boolean;
+	commentCount: number;
+	blocks: Block[];
+	isHidden: boolean;
+	excerpt: string;
+	primaryImage: ImageField;
+	sortOrder: number;
+	parentId: Guid | null;
+	siteId: Guid;
+	isStartPage: boolean;
+}
+
+
+export class Block {
+	id: Guid;
+	type: string;
+}
+
+export class Grid extends Block {
+	items: Card[];
+}
+
+export class Card extends Block {
+	cardImage: ImageField;
+	cardTitle: TextField;
+	cardSubTitle: TextField;
+	hasAModal: CheckBoxField;
+	modalSize: SelectField;
+	isModalCenteredOnTheScreen: CheckBoxField;
+	modalImage: ImageField;
+	imagePosition: SelectField;
+	modalTitle: TextField;
+	modalSubTitle: TextField;
+	modalDescription: TextField;
+	emailAddress: TextField;
+	buttonLink: TextField;
+	buttonText: TextField;
+	buttonColor: TextField;
+	preventUserFromExitingOnOutsideClick: CheckBoxField;
+}
+
+
+export interface TextField {
+	value: string;
+}
+
+export interface CheckBoxField {
+	value: boolean;
+}
+
+export interface SelectField {
+	value: number | string;
+}
+
+export interface ImageField {
+	id: Guid;
+	media: Media;
+	hasValue: boolean;
+}
+
+export interface Media {
+	id: Guid;
+	type: number;
+	filename: string;
+	contentType: string;
+	title: string;
+	altText: string;
+	description: string;
+	size: number;
+	publicUrl: string;
+	width: number; height: number;
+	created: Date;
+	lastModified: Date;
+}
+
+export interface DataSource {
+	pageId: Guid;
+	blockId: Guid | null;
+}
+
