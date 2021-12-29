c9425fa2a485e25014f52a0b7e60ad585d55180c
KamalRanaweera <ranaweer@ualberta.ca>
2021-12-28 20:00:08 -0700

    Work in progress of CardTemplate.vue

--- Catfish/Areas/Applets/UI/src/lib-components/grid/components/CardTemplate.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/grid/components/CardTemplate.vue
@@ -1,5 +1,5 @@
 ﻿<script lang="ts">
-	import { defineComponent, toRef, computed } from "vue";
+	import { defineComponent, toRef, ref, computed } from "vue";
     import { Card } from "../../shared/models/cmsModels";
 
 
@@ -13,11 +13,28 @@
 
             const model = toRef(props, 'model');
 
+            const hasCardImage = computed(() => model.value?.cardImage?.hasValue);
+            const cardImageUrl = computed(() => model.value?.cardImage?.media?.publicUrl?.replace(/^~+/g, '')); //NOTE: the REGEXP replaces any leading ~ characters
+            const cardImageaWidth = computed(() => hasCardImage.value ? model.value?.cardImage?.media?.width : 1);
+            const cardImageaHeight = computed(() => hasCardImage.value ? model.value?.cardImage?.media?.height : 1);
+            const cardImageaAspectRatio = computed(() => cardImageaWidth.value && cardImageaHeight.value ? cardImageaWidth.value / cardImageaHeight.value : 0);
+            const cardImgDiv = ref(null);
+            const cardImageHeight = 250; //computed(() => cardImgDiv?.value?.cardImgDiv * cardImageaAspectRatio.value);
+
+            const cardImageStyles = computed(() => {
+                return {
+                    backgroundImage: `url(${cardImageUrl.value})`,
+					height: `${cardImageHeight}px`
+                }
+            });
+
             return {
                 model,
-				imageUrl: computed(() => model.value?.cardImage?.media?.publicUrl?.replace(/^~+/g, '')), //NOTE: the REGEXP replaces any leading ~ characters
+				cardImgDiv,
+                hasCardImage,
+                cardImageStyles,
+				cardImageaAspectRatio,
                 popupImageUrl: computed(() => model.value?.modalImage?.media?.publicUrl),
-                containerHeight: 250
             }
         }
     });
@@ -25,7 +42,7 @@
 
 <template>
     <div class="col-md-4 m-5">
-        <div v-bind:style="{backgroundImage:`url(${imageUrl})`, height:`${containerHeight}px`}" style="background-size:cover"> </div>
+        <div ref="cardImgDiv" v-if="hasCardImage" v-bind:style="cardImageStyles" style="background-size:cover" v-bind:id="model.cardImage.id"> </div>
 
         {{JSON.stringify(model)}}
     </div>
