d945cfa1f8e737770958ee46de7a0edc08089e46
KamalRanaweera <ranaweer@ualberta.ca>
2021-12-29 12:45:24 -0700

    Binds the card image to a ref.

--- Catfish/Areas/Applets/UI/src/lib-components/grid/components/CardTemplate.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/grid/components/CardTemplate.vue
@@ -1,5 +1,5 @@
 ﻿<script lang="ts">
-	import { defineComponent, PropType, toRefs/*, ref, onMounted*/, computed } from "vue";
+	import { defineComponent, PropType, toRefs, ref, onMounted, computed } from "vue";
     import { Card } from "../../shared/models/cmsModels";
 
 
@@ -23,13 +23,13 @@
             const cardImageaAspectRatio = computed(() => cardImageaWidth.value && cardImageaHeight.value ? cardImageaWidth.value / cardImageaHeight.value : 0);
             const cardImageHeight = 250; //computed(() => cardImgDiv?.value?.cardImgDiv * cardImageaAspectRatio.value);
 
-   //         const cardImgDiv = ref<HTMLDivElement>();
+            const cardImgDiv = ref<HTMLDivElement>();
 
-			//onMounted(() => {
-			//	// the DOM element will be assigned to the ref after initial render
-			//	console.log(JSON.stringify(cardImgDiv.value)) // <div>This is a root element</div>
-			//	console.log("Width: " , cardImgDiv.value?.clientWidth) // <div>This is a root element</div>
-   //         })
+			onMounted(() => {
+				// the DOM element will be assigned to the ref after initial render
+				//console.log(JSON.stringify(cardImgDiv.value)) // <div>This is a root element</div>
+				console.log("Width: " , cardImgDiv.value?.clientWidth) // <div>This is a root element</div>
+            })
 
             const cardImageStyles = computed(() => {
                 return {
@@ -40,7 +40,7 @@
 
             return {
                 model,
-				//cardImgDiv,
+				cardImgDiv,
                 hasCardImage,
                 cardImageStyles,
 				cardImageaAspectRatio,
