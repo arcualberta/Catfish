f3712820ffdcaced1df21ac9eb5aa20fd682efe1
mruaini <melaniar@hotmail.com>
2022-01-06 13:30:29 -0700

    TASK-479 Create basic ChildFormSubmission Applet.vue

---
+++ Catfish/Areas/Applets/UI/src/lib-components/child-form-submission/Applet.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import props, { QueryParameter } from '../shared/props'
+
+    export default defineComponent({
+        name: "ChildFormSubmission",
+        components: {
+        },
+        props,
+        setup(p) {
+           
+            console.log('props: ', JSON.stringify(p));
+            const queryParams = p.queryParameters as QueryParameter;
+            return {
+               queryParams
+            }
+        }
+    });
+</script>
+
+<template>
+    <div>
+        <h2>Child Form Submission</h2>
+        
+    </div>
+</template>
