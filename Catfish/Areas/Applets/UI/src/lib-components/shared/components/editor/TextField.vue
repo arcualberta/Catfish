e6ec9076ee8d83f79c9ea557e395e0d68c2d14ed
mruaini <melaniar@hotmail.com>
2022-01-07 14:10:33 -0700

    TASK-758: modified TextField.vue, text.vur and add TextArea.vue

--- Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/TextField.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/TextField.vue
@@ -2,6 +2,8 @@
     import { defineComponent, PropType } from 'vue'
     import { MultilingualTextField } from '../../models/fieldContainer'
     import TextCollection from './text/TextCollection.vue'
+    import TextArea from './text/textArea.vue'
+    import TextInput from './text/text.vue'
 
     export default defineComponent({
         name: "TextField",
@@ -24,16 +26,31 @@
 
         },
         components: {
-            TextCollection
+            TextCollection,
+            TextArea,
+            TextInput
+        },
+        setup(p) {
+
+            const type = p.model.modelType;
+            return {
+                type
+            }
         }
     });
 </script>
 
 <template>
-    <div>Text Field</div>
-    <div>{{JSON.stringify(model)}}</div>
-   <!--<div v-for="val in model.values" :key="val.id">
-       <TextCollection :model="val" />
-   </div>-->
+    <!--<div>{{type}} : {{JSON.stringify(model)}}</div>-->
+    <label>{{model.name.concatenatedContent}} <span v-if="model.required" class="requiredField"></span></label>
+    <div v-if="type.includes('Catfish.Core.Models.Contents.Fields.TextArea')">
+
+        <TextArea v-for="val in model.values"  :key="val.id" :isRequired="model.required" />
+    </div>
+    <div v-else>
+      <TextInput  v-for="val in model.values"  :key="val.id" :isRequired="model.required" />
+    </div>
+
+    
 </template>
 
--- Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/text/TextArea.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/text/TextArea.vue
@@ -3,22 +3,25 @@
     import { Text } from '../../../models/textModels'
 
     export default defineComponent({
-        name: "Text",
+        name: "TextArea",
         props: {
             model: {
                 type: null as PropType<Text> | null,
                 required: true
+            },
+            isRequired:
+            {
+                type: Boolean,
+                required: false,
+                default: false
             }
         }
-        //methods: {
-        //    isUrl(text: string): boolean {
-        //        return text?.startsWith("http://") || text?.startsWith("https://")
-        //    }
-        //}
     });
 </script>
 
 <template>
-    <input v-model="model"  />
+    <div>
+        <textarea v-model="model" placeholder="add multiple lines" required="{isRequired ? 'required' : ''}" class="form-control"></textarea>
+    </div>
 </template>
 
