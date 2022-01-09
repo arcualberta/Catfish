9bd9341b693b53c6b4b3539830ecde01c5fc791c
KamalRanaweera <ranaweer@ualberta.ca>
2022-01-09 10:58:54 -0700

    Code clean-up.

--- Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/Field.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/Field.vue
@@ -1,5 +1,5 @@
 ﻿<script lang="ts">
-    import { defineComponent, PropType } from 'vue'
+	import { defineComponent, PropType } from 'vue'
 	import { Field, eFieldType } from '../../models/fieldContainer'
     import { FieldContainerUtils } from '../../store/form-submission-utils'
 
@@ -8,7 +8,6 @@
 	import DateField from './DateField.vue'
 	import DecimalField from './DecimalField.vue'
 	import EmailField from './EmailField.vue'
-	//import FieldContainerReference from './FieldContainerReference.vue'
 	import InfoField from './InfoField.vue'
 	import IntegerField from './IntegerField.vue'
 	import RadioField from './RadioField.vue'
@@ -39,10 +38,10 @@
         setup(p) {
             const fieldType: eFieldType = FieldContainerUtils.getFieldType(p.model);
             const isRequired: boolean = p.model.required;
-            const cssClass: string = FieldContainerUtils.cssClass(p.model);
+			const cssClass: string = FieldContainerUtils.cssClass(p.model);
 
 			return {
-				eFieldType,
+				FieldTypes: eFieldType,
                 fieldType,
                 isRequired,
 				cssClass
@@ -52,45 +51,32 @@
 </script>
 
 <template>
-    <InfoField v-if="fieldType === eFieldType.InfoSection" :model="model" :class="cssClass" />
-    <!--<FieldContainerReference v-else-if="fieldType === eFieldType.FieldContainerReference" :model="model" :class="cssClass" />-->
+	<!--<FieldContainerReference v-else-if="fieldType === eFieldType.FieldContainerReference" :model="model" :class="cssClass" />-->
+
+	<div v-if="fieldType === FieldTypes.FieldContainerReference">
+		TODO: Implement editor template for FieldContainerReference
+	</div>
+	<InfoField v-else-if="fieldType === FieldTypes.InfoSection" :model="model" :class="cssClass" />
 	<div v-else :class="cssClass + ' row'">
 		<div class="col-md-3 field-name">
 			{{model.name.concatenatedContent}}
 		</div>
 		<div class="col-md-9 field-value">
-			<AttachmentField v-if="fieldType === eFieldType.AttachmentField" :model="model" />
-			<CheckboxField v-else-if="fieldType === eFieldType.CheckboxField" :model="model" />
-			<DateField v-else-if="fieldType === eFieldType.DateField" :model="model" />
-			<DecimalField v-else-if="fieldType === eFieldType.DecimalField" :model="model" />
-			<EmailField v-else-if="fieldType === eFieldType.EmailField" :model="model" />
-			<IntegerField v-else-if="fieldType === eFieldType.IntegerField" :model="model" />
-			<RadioField v-else-if="fieldType === eFieldType.RadioField" :model="model" />
-			<SelectField v-else-if="fieldType === eFieldType.SelectField" :model="model" />
-			<TextField v-else-if="fieldType === eFieldType.TextArea" :model="model" />
-			<TextField v-else-if="fieldType === eFieldType.TextField" :model="model" />
+			<AttachmentField v-if="fieldType === FieldTypes.AttachmentField" :model="model" />
+			<CheckboxField v-else-if="fieldType === FieldTypes.CheckboxField" :model="model" />
+			<DateField v-else-if="fieldType === FieldTypes.DateField" :model="model" />
+			<DecimalField v-else-if="fieldType === FieldTypes.DecimalField" :model="model" />
+			<EmailField v-else-if="fieldType === FieldTypes.EmailField" :model="model" />
+			<IntegerField v-else-if="fieldType === FieldTypes.IntegerField" :model="model" />
+			<RadioField v-else-if="fieldType === FieldTypes.RadioField" :model="model" />
+			<SelectField v-else-if="fieldType === FieldTypes.SelectField" :model="model" />
+			<TextField v-else-if="fieldType === FieldTypes.TextArea" :model="model" />
+			<TextField v-else-if="fieldType === FieldTypes.TextField" :model="model" />
 
-			<div v-else-if="fieldType === eFieldType.CompositeField"> TODO: CompositeField</div>
-			<div v-else-if="fieldType === eFieldType.TableField"> TODO: TableField</div>
-			<div v-else-if="fieldType === eFieldType.MonolingualTextField"> TODO: Check if we really need an editor template for MonolingualTextField</div>
+			<div v-else-if="fieldType === FieldTypes.CompositeField"> TODO: Implement editor template for the CompositeField</div>
+			<div v-else-if="fieldType === FieldTypes.TableField"> TODO: Implement editor template for the TableField</div>
+			<div v-else-if="fieldType === FieldTypes.MonolingualTextField"> TODO: Check if we really need an editor template for MonolingualTextField</div>
 		</div>
-
-		<!--<div v-switch="fieldType" class="col-md-9 field-value">
-		<AttachmentField v-case="eFieldType.AttachmentField" :model="field" />
-		<CheckboxField v-case="eFieldType.CheckboxField" :model="field" />
-		<DateField v-case="eFieldType.DateField" :model="field" />
-		<DecimalField v-case="eFieldType.DecimalField" :model="field" />
-		<EmailField v-case="eFieldType.EmailField" :model="field" />
-		<IntegerField v-case="eFieldType.IntegerField" :model="field" />
-		<RadioField v-case="eFieldType.RadioField" :model="field" />
-		<SelectField v-case="eFieldType.SelectField" :model="field" />
-		<TextField v-case="eFieldType.TextArea" :model="field" />
-		<TextField v-case="eFieldType.TextField" :model="field" />
-
-		<div v-case="eFieldType.CompositeField"> TODO: CompositeField</div>
-		<div v-case="eFieldType.TableField"> TODO: TableField</div>
-		<div v-case="eFieldType.MonolingualTextField"> TODO: Check if we really need an editor template for MonolingualTextField</div>
-	</div>-->
 	</div>
 </template>
 
