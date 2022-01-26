d0f7ca35dbe98c8a8dd041f6bb86b89733ae094b
KamalRanaweera <ranaweer@ualberta.ca>
2022-01-26 14:50:32 -0700

    Created empty applet main file skeleton for the Audio Recorder Field.

---
+++ Catfish/Areas/Applets/UI/src/lib-components/shared/components/editor/AudioRecorderField.vue
@@ -0,0 +1,22 @@
+﻿<script lang="ts">
+    import { defineComponent } from 'vue'
+      
+
+    export default defineComponent({
+		name: "AudioRecorder",
+        components: {
+            
+        },
+        props: {
+            model: {
+           }
+        },
+        methods: {
+		}
+    });
+</script>
+
+<template>
+    <div>Audio Recorder</div>
+    <div>{{JSON.stringify(model)}}</div>
+</template>
