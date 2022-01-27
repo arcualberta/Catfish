d1fda63fd0414a19b4d30dd87eae3ca4ba3ea7f5
KamalRanaweera <ranaweer@ualberta.ca>
2022-01-26 17:26:50 -0700

    Work in progress on Form Submission applet.

---
+++ Catfish/Areas/Applets/UI/src/lib-components/form-submission/Applet.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { Guid } from 'guid-typescript'
+	import { defineComponent, computed } from 'vue';
+	import { useStore } from 'vuex';
+	import props, { QueryParameter, DataAttribute } from '../shared/props'
+	import { eValidationStatus } from '../shared/models/fieldContainer'
+	import { state } from './store/state'
+	import { actions, Actions } from './store/actions'
+	import { getters } from './store/getters'
+    import { mutations, Mutations, SubmissionStatus as eSubmissionStatus } from './store/mutations'
+	
+
+	import SubmissionForm from '../shared/components/editor/FieldContainer.vue'
+
+    export default defineComponent({
+        name: "FormSubmission",
+		components: {
+			SubmissionForm
+        },
+        props,
+        setup(p) {
+           
+            console.log('props: ', JSON.stringify(p));
+			const queryParameters = p.queryParameters as QueryParameter;
+			const dataAttributes = p.dataAttributes as DataAttribute;
+
+			const itemId = Guid.parse(queryParameters.iid as string);
+			const itemTemplateId = Guid.parse(dataAttributes["template-id"] as string);
+			const childFormId = Guid.parse(dataAttributes["child-form-id"] as string);
+
+            const store = useStore();
+
+			store.commit(Mutations.SET_IDS, [itemId, itemTemplateId, childFormId]);
+
+			//load the data
+			store.dispatch(Actions.LOAD_FORM);
+			//const submissionStatus = store.state.submissionStatus as SubmissionStatus;
+           // const submissionStatus: eSubmissionStatus = store.state.submissionStatus as eSubmissionStatus;
+			//console.log("initial status " + JSON.stringify(submissionStatus));
+			return {
+				submissionForm: computed(() => store.state.form),
+				store,
+				submissionStatus: computed(() => store.state.submissionStatus),
+				eSubmissionStatus,
+				eValidationStatus
+              
+            }
+		},
+		storeConfig: {
+			state,
+			actions,
+			mutations,
+			getters
+        },
+        methods: {
+			submitForm() {
+				this.store.dispatch(Actions.SUBMIT_FORM);
+            }
+        }
+    });
+</script>
+
+<template>
+	<div v-if="submissionForm && Object.keys(submissionForm).length > 0">
+		<SubmissionForm :model="submissionForm" />
+
+		<div v-if="submissionForm?.validationStatus === eValidationStatus.INVALID" class="alert alert-danger">Form validation failed.</div>
+		<div v-else>
+			<div v-if="submissionStatus === eSubmissionStatus.InProgress" class="alert alert-info">Submitting...</div>
+			<div v-if="submissionStatus === eSubmissionStatus.Success" class="alert alert-info">Submission successful</div>
+			<div v-if="submissionStatus === eSubmissionStatus.Fail" class="alert alert-danger">Submission failed</div>
+		</div>
+		<button class="btn btn-primary" @click="submitForm()">Submit</button>
+	</div>
+</template>
---
+++ Catfish/Areas/Applets/UI/src/lib-components/form-submission/store/mutations.ts
@@ -0,0 +1,56 @@
+﻿import { MutationTree } from 'vuex';
+import { State } from './state';
+import { Guid } from 'guid-typescript'
+import { eValidationStatus, FieldContainer } from '../../shared/models/fieldContainer';
+import { flattenFieldInputs, FlattenedFormFiledMutations } from '../../shared/store/form-submission-utils'
+import { validateFields } from '../../shared/store/form-validators';
+
+export enum SubmissionStatus {
+    None = "None",
+    InProgress = "InProgress",
+    Success = "Success",
+    Fail = "Fail"
+}
+//Declare MutationTypes
+export enum Mutations {
+    SET_IDS = 'SET_IDS',
+    SET_FORM = 'SET_FORM',
+    SET_SUBMISSION_STATUS='SET_SUBMISSION_STATUS'
+}
+
+//Create a mutation tree that implement all mutation interfaces
+export const mutations: MutationTree<State> = {
+
+    [Mutations.SET_IDS](state: State, payload: Guid[]) {
+        state.itemInstanceId = payload[0];
+        state.itemTemplateId = payload[1];
+        state.formId = payload[2];
+    },
+    [Mutations.SET_FORM](state: State, payload: FieldContainer) {
+        state.form = payload
+
+        state.flattenedTextModels = {};
+        state.flattenedOptionModels = {};
+        flattenFieldInputs(state.form, state)
+
+    //    console.log("state\n", JSON.stringify(state))
+    },
+    [FlattenedFormFiledMutations.SET_TEXT_VALUE](state: State, payload: { id: Guid; val: string }) {
+        //console.log("payload id:", payload.id, "   payload value: ", payload.val)
+        state.flattenedTextModels[payload.id.toString()].value = payload.val;
+        //console.log("state flattenedTextModels", JSON.stringify(state.flattenedTextModels))
+
+        //Re-validating the form
+        if (state.form?.validationStatus === eValidationStatus.INVALID)
+            validateFields(state.form);
+
+    },
+    [FlattenedFormFiledMutations.SET_OPTION_VALUE](state: State, payload: { id: Guid; isSelected: boolean }) {
+        state.flattenedOptionModels[payload.id.toString()].selected = payload.isSelected;
+    },
+    [Mutations.SET_SUBMISSION_STATUS](state: State, status: string) {
+
+        //const fieldType: eFieldType = eFieldType[fieldTypeStr as keyof typeof eFieldType];
+        state.submissionStatus = SubmissionStatus[status as keyof typeof SubmissionStatus];
+    },
+}
