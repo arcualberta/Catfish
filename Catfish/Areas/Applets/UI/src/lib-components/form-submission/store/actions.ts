3b55eedf3926798bfe33d2aa9e467924a778a570
KamalRanaweera <ranaweer@ualberta.ca>
2022-02-26 13:28:32 -0700

    Implemented file uploading with Vue AttachmentField fields.

--- Catfish/Areas/Applets/UI/src/lib-components/form-submission/store/actions.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/form-submission/store/actions.ts
@@ -1,6 +1,7 @@
 ﻿import { ActionTree } from 'vuex';
 import { State } from './state';
 import { Mutations } from './mutations';
+import { clearForm, FlattenedFormFiledState } from '../../shared/store/form-submission-utils';
 //import { clearForm, FlattenedFormFiledState } from '../../shared/store/form-submission-utils'
 //import { validateFields } from '../../shared/store/form-validators'
 
@@ -37,27 +38,46 @@ export const actions: ActionTree<State, any> = {
         store.commit(Mutations.SET_SUBMISSION_STATUS, "InProgress");
 
        
-        const api = window.location.origin + `/applets/api/itemeditor/?itemTemplateId=${store.state.itemTemplateId}&groupId=${store.state.groupId}&collectionId=${store.state.collectionId}`;
+        const api = window.location.origin + `/applets/api/itemeditor/?itemTemplateId=${store.state.itemTemplateId}&groupId=${store.state.groupId ? store.state.groupId : ""}&collectionId=${store.state.collectionId}`;
 
         const formData = new FormData();
+
+        //Setting the serialized JSON form model to the datamodel variable in formData
         formData.append('datamodel', JSON.stringify(store.state.form));
+
+        //Adding all attachments uploaded to the files variable in formData
+        for (const key in store.state.flattenedFileModels) {
+            if (store.state.flattenedFileModels[key].length > 0) {
+                store.state.flattenedFileModels[key].forEach(file => {
+                    console.log("File: ", file.name)
+                    formData.append('files', file);
+                    formData.append('fileKeys', key);
+				})
+			}
+        }
 
         fetch(api,
             {
                 body: formData,
-                method: "post"
+                method: "post",
+                headers: {
+                    //"Content-Type": "multipart/form-data"
+                    "encType": "multipart/form-data"
+                }
             }).then(response =>
                 response.json())
             .then(data => {
                 console.log(JSON.stringify(data));
-               // const flattenModel: FlattenedFormFiledState ={          
-               //     flattenedOptionModels : store.state.flattenedOptionModels,
-               //     flattenedTextModels : store.state.flattenedTextModels,
-               // };
-               // //clear the form content
-               // clearForm(flattenModel);
-               //store.commit(Mutations.SET_SUBMISSION_STATUS, "Success");
-                
+                const flattenModel: FlattenedFormFiledState = {
+                    flattenedOptionModels: store.state.flattenedOptionModels,
+                    flattenedTextModels: store.state.flattenedTextModels,
+                    flattenedFileModels: store.state.flattenedFileModels
+                };
+
+                //clear the form content
+                clearForm(flattenModel);
+                store.commit(Mutations.SET_SUBMISSION_STATUS, "Success");
+
             })
             .catch(error => {
                 store.commit(Mutations.SET_SUBMISSION_STATUS, "Fail");
