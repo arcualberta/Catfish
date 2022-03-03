ba3f062a5b28fdfb099923145a5dac33d5fc2b5b
KamalRanaweera <ranaweer@ualberta.ca>
2022-03-03 09:14:21 -0700

    Made minor layout updates

--- Catfish/Areas/Applets/UI/src/manager-js/item-layout.js
+++ Catfish/Areas/Applets/UI/src/manager-js/item-layout.js
@@ -8,7 +8,7 @@
         }
     },
     methods: {
-        onBlur: function (e) {  
+        onBlur: function (e) {
             this.model.selectedComponents.value = JSON.stringify(this.layoutComponents);
         },
         selectItemTemplate: function (selected) {
@@ -25,12 +25,12 @@
             var filtered = this.layoutComponents.filter(function (field, index, arr) {
 
                 return field.id !== componentId;
-             });
+            });
             this.layoutComponents = filtered;
             this.model.selectedComponents.value = JSON.stringify(this.layoutComponents);
 
         },
-        
+
         getFormFields: function (itemTemplateId, formId) {
 
             let uniqueFormIds = [...new Set(this.layoutComponents.map(com => com.formId))];
@@ -40,7 +40,7 @@
                 .then((data) => {
                     this.itemFields = data;
                 });
-           
+
         },
         onAddComponent: function (componentLabel) {
             console.log(componentLabel);
@@ -58,7 +58,7 @@
             return this.allForms;
         },
         getSelectedFormFields(formId) {
-           
+
             return this.itemFields[formId];
         }
 
@@ -74,19 +74,19 @@
 
                 });
         }
-        
+
         if (this.model.selectedComponents?.value) {
             this.layoutComponents = JSON.parse(this.model.selectedComponents.value);
 
             let uniqueFormIds = [...new Set(this.layoutComponents.map(com => com.formId))];
-           
-            fetch('/applets/api/itemtemplates/getMutipleFormFields/' + this.model.selectedItemTemplateId?.value + "/" + uniqueFormIds)
-                        .then(response => response.json())
-                        .then((data) => {
-                            this.itemFields = data;
 
-                           // console.log("multiple " + JSON.stringify(this.itemFields));
-             });
+            fetch('/applets/api/itemtemplates/getMutipleFormFields/' + this.model.selectedItemTemplateId?.value + "/" + uniqueFormIds)
+                .then(response => response.json())
+                .then((data) => {
+                    this.itemFields = data;
+
+                    // console.log("multiple " + JSON.stringify(this.itemFields));
+                });
         }
     },
 
@@ -104,45 +104,47 @@
         
         <div>
             <div v-for="component in layoutComponents" :key="component.id" class="itemLayout" >
-             <div class="text-right"><span class="fas fa-times-circle text-danger" @click="removeComponent(component.id)"></span></div>
-            <div class='lead row'>
-                <label class='form-label col-md-1'>Type:</label>
-                <select class='form-control col-md-2' name="modalSize"  v-model='component.type' @change="updateSelectedComponents()">
-                    <option value="h1">H1</option>
-                    <option value="h2">H2</option>
-                    <option value="h3">H3</option>
-                    <option value="h4">H4</option>
-                    <option value="h5">H5</option>
-                    <option value="div">Div</option>
-                    <option value="p">Paragraph</option>
-                    <option value="img">IMG</option>
-                    <option value="file">AttachmentField</option>
-                 </select>
-                 <label class='form-label'  style="margin-left: 10px; margin-right: 10px">Class(es):</label>
-                 <input type="text" v-model="component.cssClasses" class="col-md-2"  v-on:blur="onBlur" />
-                   <label class='form-label' style="margin-left: 10px; margin-right: 10px">Id:</label>
-                 <input type="text" v-model="component.elementId"  class="col-md-2" v-on:blur="onBlur" />
-                  <label class='form-label' style="margin-left: 10px; margin-right: 10px">Style:</label>
-                 <textarea v-model="component.cssStyle" cols="20" rows="2"  class="col-md-2" v-on:blur="onBlur" />
-            </div>
-            <div v-if="component.label === 'Form Field'" class='lead row'>
-                   <label class='form-label col-md-1 '>Form: </label>
-                    <select v-model="component.formId" class="form-control" style="width:auto;" @change="getFormFields(model.selectedItemTemplateId.value,component.formId)">
+                <div class="text-right"><span class="fas fa-times-circle text-danger" @click="removeComponent(component.id)"></span></div>
+                <div v-if="component.label === 'Form Field'" class='lead row'>
+                    <div class="col-md-5 row" style="margin:5px">
+                        <label class='form-label' style="margin:5px">Form: </label>
+                        <select v-model="component.formId" class="form-control col-md-9" @change="getFormFields(model.selectedItemTemplateId.value,component.formId)">
+                            <option disabled value="">Please select one</option>
+                            <option v-for="form in getFormsOfSelectedTemplate()" :value="form.value">{{form.text}}</option>
+                        </select>
+                    </div>
+                    <div class="col-md-5 row" style="margin-left:15px;">
+                    <label class='form-label' style="margin:5px;">Select Field: </label>
+                    <select v-model="component.fieldId" class="form-control col-md-8" style="width:auto;" @change="updateSelectedComponents()">
                         <option disabled value="">Please select one</option>
-                        <option v-for="form in getFormsOfSelectedTemplate()" :value="form.value">{{form.text}}</option>
+                        <option v-for="fld in getSelectedFormFields(component.formId)" :value="fld.fieldId">{{fld.fieldName}}</option>
                     </select>
-                <label class='form-label col-md-2'>Select Field: </label>
-                <select v-model="component.fieldId" class="form-control" style="width:auto;" @change="updateSelectedComponents()">
-                    <option disabled value="">Please select one</option>
-                    <option v-for="fld in getSelectedFormFields(component.formId)" :value="fld.fieldId">{{fld.fieldName}}</option>
-                 </select>
-             </div>
-             <div v-if="component.label === 'Static Text'" class='lead row'>
-                    <label class='form-label col-md-2 '>Content: </label>
-                     <textarea v-model="component.content" cols="50" rows="2"  class="col-md-9" v-on:blur="onBlur" />
+                    </div>
                 </div>
-
-                </hr />
+                <div v-if="component.label === 'Static Text'" class='lead row'>
+                   <label class='form-label col-md-2 '>Content: </label>
+                   <textarea v-model="component.content" cols="50" rows="2"  class="col-md-9" v-on:blur="onBlur" />
+                </div>
+                <div class='lead row'>
+                    <label class='form-label col-md-1'>Type:</label>
+                    <select class='form-control col-md-2' name="modalSize"  v-model='component.type' @change="updateSelectedComponents()">
+                        <option value="h1">H1</option>
+                        <option value="h2">H2</option>
+                        <option value="h3">H3</option>
+                        <option value="h4">H4</option>
+                        <option value="h5">H5</option>
+                        <option value="div">Div</option>
+                        <option value="p">Paragraph</option>
+                        <option value="img">Image</option>
+                        <option value="file">File</option>
+                     </select>
+                     <label class='form-label'  style="margin-left: 10px; margin-right: 10px">Class(es):</label>
+                     <input type="text" v-model="component.cssClasses" class="col-md-2"  v-on:blur="onBlur" />
+                       <label class='form-label' style="margin-left: 10px; margin-right: 10px">Id:</label>
+                     <input type="text" v-model="component.elementId"  class="col-md-2" v-on:blur="onBlur" />
+                      <label class='form-label' style="margin-left: 10px; margin-right: 10px">Style:</label>
+                     <textarea v-model="component.cssStyle" cols="20" rows="2"  class="col-md-2" v-on:blur="onBlur" />
+                </div>
             </div>
         </div>
 
