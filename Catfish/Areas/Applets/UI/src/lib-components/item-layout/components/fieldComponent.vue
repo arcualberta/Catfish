fa513fd086267dbfd1640c9f4f8ece5e0c7aaab2
mruaini <melaniar@hotmail.com>
2022-03-04 16:32:59 -0700

    TASK 940-941 Add fieldComponent and getter to get all the selected fields -- working progress

---
+++ Catfish/Areas/Applets/UI/src/lib-components/item-layout/components/fieldComponent.vue
@@ -0,0 +1,49 @@
+﻿<script lang="ts">
+    import { defineComponent, PropType /*, ref , computed */} from 'vue'
+//import { MultilingualTextField } from '../../shared/models/fieldContainer';
+    import { ComponentField } from "../models/componentField"
+ // import { Field, MultilingualTextField } from "../../shared/models/fieldContainer"
+
+
+   export default defineComponent({
+            name: "FieldComponent",
+        components: {
+
+        },
+        props: {
+            model: {
+            type: null as PropType<ComponentField> | null,
+            required: true
+            },
+            //fieldlayout:{
+            //    type: null as PropType<ComponentLayout> | null,
+            //    required: false
+            //}
+
+        },
+        setup() {
+            //  const store = useStore();
+          //  const field = p.model.field;
+          //  const layout = p.model.component;
+            
+         //  console.log("field in fieldComponent: " + JSON.stringify(field));
+            return {
+                 //   store,
+            //   field,
+              //  layout
+            }
+        }
+       
+    });
+</script>
+
+<template>
+   
+    <div class="fieldType">
+        <h3>Field Component</h3>
+        {{JSON.stringify(model.field)}}
+        <h3>HTML field type</h3>
+        {{JSON.stringify(model.component.type)}}
+    </div>
+</template>
+
--- Catfish/Areas/Applets/UI/src/lib-components/item-layout/store/getters.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/item-layout/store/getters.ts
@@ -1,6 +1,9 @@
 ﻿import { Guid } from 'guid-typescript';
 import { GetterTree } from 'vuex';
+import { FieldLayout } from '../models/fieldLayout';
 import { State } from './state';
+import { ComponentField } from "../models/componentField"
+import {Field } from "../../shared/models/fieldContainer"
 
 export const getters: GetterTree<State, State> = {
    
@@ -10,5 +13,26 @@ export const getters: GetterTree<State, State> = {
     field: (state) => (itemTemplateId: Guid, fieldId: Guid) => {
 
         return (state.item?.dataContainer?.$values?.filter(dc => dc.templateId === itemTemplateId)[0])?.fields.$values?.filter(fd => fd.id === fieldId);
+    },
+    fields: (state) => (components: FieldLayout[]) => {
+        let flds:ComponentField[] = [];
+       // console.log("Item: " + JSON.stringify(state.item));
+        for (let i = 0; i < components?.length; i++) {
+
+            let frmTemplateId = components[i].formTemplateId;
+            let fldId = components[i].fieldId;
+           // console.log("form template Id : " + frmTemplateId + "field id: " + fldId);
+           
+            //let fld: Field;
+            let fld = (state.item?.dataContainer?.$values?.filter(dc => dc.templateId === frmTemplateId)[0])?.fields.$values?.filter(fd => fd.id === fldId);
+       //     console.log("the field: " + JSON.stringify(fld));
+            let comField: ComponentField = { component: components[i], field: fld as unknown as Field };
+           
+
+            flds.push(comField);
+        }
+        //console.log("fields:")
+      //  console.log(JSON.stringify(flds));
+        return flds;
     }
 }
