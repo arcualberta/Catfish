c32cbcced0cc6e1d2a7834b196385d35e531a853
mruaini <melaniar@hotmail.com>
2022-03-10 12:20:19 -0700

    Add display other fields type

--- Catfish/Areas/Applets/UI/src/lib-components/item-layout/components/fieldComponent.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/item-layout/components/fieldComponent.vue
@@ -1,9 +1,9 @@
 ﻿<script lang="ts">
     import { defineComponent, PropType, computed  /*, ref  */} from 'vue'
-
+    import dayjs from "dayjs";
     import { ComponentField } from "../models/componentField"
-    import {/* Field,*/ MultilingualTextField } from "../../shared/models/fieldContainer"
-
+    import {/* Field,*/ MultilingualTextField, OptionsField, OptionsFieldMethods} from "../../shared/models/fieldContainer"
+   
   
    
    export default defineComponent({
@@ -26,7 +26,19 @@
 				fieldType: computed(() => p.model.field.$type.split(',')[0]),
                 multiTextField: computed(() => (p.model.field as MultilingualTextField))
             }
-        }
+       },
+       methods: {
+           formatDate(dateString: string) {
+               const date = dayjs(dateString);
+               return date.format('MMM DD, YYYY');
+           },
+           formatToDecimal: (value: number, decimalPlaces: number) => {
+               return Number(value).toFixed(decimalPlaces);
+           },
+           getSelectedFieldLabels(field: OptionsField) {
+               return OptionsFieldMethods.getSelectedFieldLabels(field.options.$values);
+           }
+       }
        
     });
 </script>
@@ -35,21 +47,49 @@
 
    
     <div class="fieldType">
-        {{JSON.stringify(model)}} <br />
-        {{htmlWrapperTag}}: {{compType}}
+        <!--{{JSON.stringify(model)}} <br />
+        {{htmlWrapperTag}}: {{compType}}-->
         <hr />
         <component :is="htmlWrapperTag" v-if="componentType === 'Catfish.Areas.Applets.Models.Blocks.ItemLayout.StaticText'"> {{model.component.content}} </component>
-        <div  v-else v-if="fieldType === 'Catfish.Core.Models.Contents.Fields.TextArea' || fieldType === 'Catfish.Core.Models.Contents.Fields.TextField'">
-			<div v-for="multilingualValue in field.values.$values">
-                <component :is="htmlWrapperTag" v-for="valueInOneLanguage in multilingualValue.values.$values">
-                    {{valueInOneLanguage.value}}
+        <div v-else>
+            <div v-if="fieldType === 'Catfish.Core.Models.Contents.Fields.TextArea' || fieldType === 'Catfish.Core.Models.Contents.Fields.TextField'">
+                <div v-for="multilingualValue in field.values.$values">
+                    <component :is="htmlWrapperTag" v-for="valueInOneLanguage in multilingualValue.values.$values">
+                        {{valueInOneLanguage.value}}
+                    </component>
+                </div>
+            </div>
+            <div v-else-if="fieldType === 'Catfish.Core.Models.Contents.Fields.EmailField' || fieldType === 'Catfish.Core.Models.Contents.Fields.MonolingualTextField' || fieldType === 'Catfish.Core.Models.Contents.Fields.IntegerField'">
+                <component :is="htmlWrapperTag" v-for="val in field.values.$values">
+                    {{val.value}}
                 </component>
-			</div>
+            </div>
+            <div v-else-if="fieldType === 'Catfish.Core.Models.Contents.Fields.DecimalField'">
+                <component :is="htmlWrapperTag" v-for="val in field.values.$values">
+                    {{formatToDecimal(val.value, 2)}}
+                </component>
+            </div>
+            <div v-else-if="fieldType === 'Catfish.Core.Models.Contents.Fields.DateField'">
+                <component :is="htmlWrapperTag" v-for="val in field.values.$values">
+                    {{formatDate(val.value)}}
+                </component>
+            </div>
+            <div v-else-if="fieldType === 'Catfish.Core.Models.Contents.Fields.OptionsField' || fieldType === 'Catfish.Core.Models.Contents.Fields.CheckboxField' || fieldType === 'Catfish.Core.Models.Contents.Fields.RadioField' || fieldType === 'Catfish.Core.Models.Contents.Fields.SelectField'">
+                <component :is="htmlWrapperTag">
+                    {{getSelectedFieldLabels(field)}}
+                </component>
+
+            </div>
+            <!--   <div v-else-if="fieldType === 'Catfish.Core.Models.Contents.Fields.AttachmentField' || fieldType === 'Catfish.Core.Models.Contents.Fields.AudioRecorderField'">
+           <component :is="htmlWrapperTag" >
+               TODO
+           </component>
+             </div>
+         -->
         </div>
         <br />
         <hr />
-        <hr />
-        <!--<component :is="type"> {{field[0].values.$values[0].concatenatedContent}} </component>-->
+
     </div>
 </template>
 
