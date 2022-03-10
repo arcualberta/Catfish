5ad7e6a3098f06d9ac0256cf55abaae24d828809
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-03-10 15:46:01 -0700

    Get method changed to PostAsync

--- Catfish/Areas/Applets/UI/src/lib-components/report/store/actions.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/report/store/actions.ts
@@ -3,14 +3,12 @@ import { State } from './state';
 import { Mutations } from './mutations';
 
 export enum Actions {
-    LOAD_DATA = 'LOAD_DATA',
-    INIT_FILTER = 'INIT_FILTER',
-    FRESH_SEARCH = 'FRESH_SEARCH'
+    LOAD_DATA = 'LOAD_DATA'
 }
 export const actions: ActionTree<State, any> = {
     [Actions.LOAD_DATA](store) {
 
-        //console.log('Store: ', JSON.stringify(store.state))
+        console.log('Store: ', JSON.stringify(store.state))
 
         const api = window.location.origin +
             `/applets/api/items/GetReportData/${store.state.groupId}/template/${store.state.itemTemplateID}/collection/${store.state.collectionID}`;
@@ -20,7 +18,6 @@ export const actions: ActionTree<State, any> = {
             .then(response => response.json())
             .then(data => {
                 store.commit(Mutations.SET_REPORT_DATA, data)
-
             });
     },
 
