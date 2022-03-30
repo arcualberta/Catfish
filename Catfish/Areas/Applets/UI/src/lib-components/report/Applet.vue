eaa4e7d4e8769af920d019a4fe9a598ae37ed18a
KamalRanaweera <ranaweer@ualberta.ca>
2022-03-30 17:00:51 -0600

    Quick fix on serialization and de-serialization of status values.

--- Catfish/Areas/Applets/UI/src/lib-components/report/Applet.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/report/Applet.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { Guid } from 'guid-typescript'
 
-	import { defineComponent, computed } from 'vue';
+	import { defineComponent, computed, ref } from 'vue';
 	import { useStore } from 'vuex';
     import props, { DataAttribute, QueryParameter } from '../shared/props'
 
-	import { state } from './store/state'
+	import { state, SystemStatus } from './store/state'
 	import { actions, Actions } from './store/actions'
 	import { getters } from './store/getters'
 	import { mutations, Mutations } from './store/mutations'
@@ -41,12 +41,10 @@
             store.commit(Mutations.SET_REPORT_FIELDS, selectedFields)
 			store.state.reportFields = selectedFields;
 
-			const templateStatus = JSON.parse(dataAttributes["status"] as string);
-            console.log("templateStatus  " + templateStatus)
+			const templateStatus = JSON.parse(dataAttributes["status"] as string) as SystemStatus[];
             store.commit(Mutations.SET_STATUS, templateStatus)
             store.state.templateStatus = templateStatus;
 			
-
             const queryParams = p.queryParameters as QueryParameter;
             store.commit(Mutations.SET_ID, queryParams.iid);
 
@@ -59,7 +57,7 @@
             //console.log("selected Fields: " + selectedFields)
 			
 
-			
+			const startDate = ref("");
 
 
 			return {
@@ -70,6 +68,7 @@
 				queryParams,
                 templateStatus,
 				detailedViewURL: (id: Guid) => { const url = detailedViewUrlPath + id; return url; },
+				startDate
 			}
 		},
 		storeConfig: {
@@ -92,8 +91,10 @@
 	<div class="row col-md-12">
 		<div class="col-md-5 form-group">
 			<label>From:</label>
-			<input type="date" name="startDate" id="startDate" class="form-control" />
+			<!--<input type="date" name="startDate" id="startDate" v-model="startDate" class="form-control" />-->
+			<input type="text" name="startDate" id="startDate" v-model="startDate" class="form-control" />
 		</div>
+		Start Date: {{startDate}}
 		<div class="col-md-5 form-group">
 			<label class="form-label">To:</label>
 			<input type="date" name="endDate" id="endDate" class="form-control" />
@@ -101,76 +102,76 @@
 		<!--<button onclick="filterItems('@entityTemplateId', '@collectionId', $('#startDate').val(), $('#endDate').val(), 'itemListBlockTable',@reportTemplateId);" class="btn btn-default btn-primary" style="margin-top:30px; height:fit-content;" value="Execute">Go<i class="fa fa-arrow-right" style="padding-left:5px;"></i></button>-->
 	</div>
 	<button class="btn btn-primary" @click="loadData()">Execute</button>
-	
-	<!--<div v-for="status in templateStatus">
+
+	<div v-for="status in templateStatus">
 		<div>{{status.status}}</div>
-	</div>-->
-		<table class="table">
-			<thead>
-				<tr>
-					<th></th>
-					<th v-for="field in selectedFields">{{field.fieldName}}</th>
-					<th>Submitted Date</th>
-					<th>Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="reportRow in reportRows">
-					<td>
-						<a :href="detailedViewURL(reportRow.itemId)" class="fa fa-eye" target="_blank"></a>
-					</td>
-					<td v-for="cell in reportRow.cells">
-						<div v-for="cellValue in cell.values">
-							<div v-if="cellValue.renderType === 'MultilingualText'">
-								<div v-for="txt in cellValue.values">
-									{{txt.value}}
-								</div>
-							</div>
-							<ul v-if="cellValue.renderType === 'Options'">
-								<li v-for="txt in cellValue.values">
-									{{txt.value}}
-								</li>
-							</ul>
-							<div v-if="cellValue.renderType === 'MonolingualText'">
-								<div v-for="txt in cellValue.values">
-									<!--<div v-if="txt"></div>-->
-									{{txt.value}}
-								</div>
-							</div>
-							<div v-if="cellValue.renderType === 'Attachment'">
-								<div v-for="txt in cellValue.values">
-									{{txt.value}}
-								</div>
-							</div>
-							<div v-if="cellValue.renderType === 'Audio'">
-								<div v-for="txt in cellValue.values">
-									{{txt.value}}
-								</div>
+	</div>
+	<table class="table">
+		<thead>
+			<tr>
+				<th></th>
+				<th v-for="field in selectedFields">{{field.fieldName}}</th>
+				<th>Submitted Date</th>
+				<th>Status</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="reportRow in reportRows">
+				<td>
+					<a :href="detailedViewURL(reportRow.itemId)" class="fa fa-eye" target="_blank"></a>
+				</td>
+				<td v-for="cell in reportRow.cells">
+					<div v-for="cellValue in cell.values">
+						<div v-if="cellValue.renderType === 'MultilingualText'">
+							<div v-for="txt in cellValue.values">
+								{{txt.value}}
 							</div>
 						</div>
-					</td>
-					<td>
-						<div>{{reportRow.created}}</div>
-					</td>
-					<td>
-						<div>{{reportRow.status}}</div>
-					</td>
+						<ul v-if="cellValue.renderType === 'Options'">
+							<li v-for="txt in cellValue.values">
+								{{txt.value}}
+							</li>
+						</ul>
+						<div v-if="cellValue.renderType === 'MonolingualText'">
+							<div v-for="txt in cellValue.values">
+								<!--<div v-if="txt"></div>-->
+								{{txt.value}}
+							</div>
+						</div>
+						<div v-if="cellValue.renderType === 'Attachment'">
+							<div v-for="txt in cellValue.values">
+								{{txt.value}}
+							</div>
+						</div>
+						<div v-if="cellValue.renderType === 'Audio'">
+							<div v-for="txt in cellValue.values">
+								{{txt.value}}
+							</div>
+						</div>
+					</div>
+				</td>
+				<td>
+					<div>{{reportRow.created}}</div>
+				</td>
+				<td>
+					<div>{{reportRow.status}}</div>
+				</td>
 
-				</tr>
-			</tbody>
-		</table>
+			</tr>
+		</tbody>
+	</table>
 
-		<!--<div v-for="reportRow in reportRows">
-			<h3>Row</h3>
-			<div v-for="cell in reportRow.cells.$values">
-				<h4>Cell</h4>
-				<div v-for="cellValue in cell.values.$values">
-					<h5>Cell Value</h5>
-					{{cellValue.$type}}
-					{{cellValue.formInstanceId}}
-				</div>
+	<!--<div v-for="reportRow in reportRows">
+		<h3>Row</h3>
+		<div v-for="cell in reportRow.cells.$values">
+			<h4>Cell</h4>
+			<div v-for="cellValue in cell.values.$values">
+				<h5>Cell Value</h5>
+				{{cellValue.$type}}
+				{{cellValue.formInstanceId}}
 			</div>
+		</div>
 
-		</div>-->
+	</div>-->
 </template>
 
