6b64915ece4eef6bc3bc70060cfc7aabef139e12
mruaini <melaniar@hotmail.com>
2022-04-06 12:19:04 -0600

    TASK-1022: Modified IG test for import data

--- Catfish.UnitTests/IGResearchDirectoryTest.cs
+++ Catfish.UnitTests/IGResearchDirectoryTest.cs
@@ -750,7 +750,7 @@ Any public disclosures of information from the directory will be in aggregate fo
             bool clearCurrentData = false;
 
             //Set maxEntries to a positive value to limit the maximum number of data entries to be imported.
-            int maxEntries = -1;
+            int maxEntries = 1;
 
             // string multiValueSeparator = ";";
 
@@ -834,7 +834,7 @@ Any public disclosures of information from the directory will be in aggregate fo
 
                                         for (int j = 0; j < (f as RadioField).Options.Count; j++)//foreach(Option op in (f as CheckboxField).Options)
                                         {
-                                            if (v == (f as RadioField).Options[j].OptionText.GetContent("en"))
+                                            if (v.Contains((f as RadioField).Options[j].OptionText.GetContent("en"), StringComparison.OrdinalIgnoreCase))
                                             {
                                                 (_newDataItem.Fields[k] as RadioField).Options[j].SetAttribute("selected", true);
                                                 break;
@@ -855,8 +855,10 @@ Any public disclosures of information from the directory will be in aggregate fo
 
                                         for (int j = 0; j < (f as CheckboxField).Options.Count; j++)//foreach(Option op in (f as CheckboxField).Options)
                                         {
-                                            if (v == (f as CheckboxField).Options[j].OptionText.GetContent("en"))
+
+                                            if (v.Contains((f as CheckboxField).Options[j].OptionText.GetContent("en"), StringComparison.OrdinalIgnoreCase))
                                             {
+
                                                 (_newDataItem.Fields[k] as CheckboxField).Options[j].SetAttribute("selected", true);
                                                 break;
                                             }
@@ -867,10 +869,11 @@ Any public disclosures of information from the directory will be in aggregate fo
                                 }
                                 else if (f.ModelType.Contains("FieldContainerReference"))
                                 {
-                                    string[] vals = colValue.Split(","); //THIS NEED TO BE REDO -- CONSIDERING ALSO SPLIT BY A ";"
+                                    string[] vals = colValue.Split("-"); //THIS NEED TO BE REDO -- CONSIDERING ALSO SPLIT BY A ";"
                                     //check the checkbox in the metadataset
-                                    foreach (var fld in ms.Fields)
+                                   for(int l=0; l< ms.Fields.Count; l++)// foreach (var fld in ms.Fields)
                                     {
+                                        var fld = ms.Fields[l];
                                         if (fld.ModelType.Contains("CheckboxField"))
                                         {
                                             foreach (string v in vals)
@@ -878,9 +881,9 @@ Any public disclosures of information from the directory will be in aggregate fo
 
                                                 for (int j = 0; j < (fld as CheckboxField).Options.Count; j++)//foreach(Option op in (f as CheckboxField).Options)
                                                 {
-                                                    if (v == (fld as CheckboxField).Options[j].OptionText.GetContent("en"))
+                                                    if (v.Contains((fld as CheckboxField).Options[j].OptionText.GetContent("en"), StringComparison.OrdinalIgnoreCase))
                                                     {
-                                                        (ms.Fields[k] as CheckboxField).Options[j].SetAttribute("selected", true);
+                                                        (ms.Fields[l] as CheckboxField).Options[j].SetAttribute("selected", true);
                                                         break;
                                                     }
                                                 }
@@ -891,28 +894,28 @@ Any public disclosures of information from the directory will be in aggregate fo
                                     }
 
                                 }
-
+                            }// end if matched field
                                 //_newDataItem.Fields.Add(f);
-                            }//end of each field
-                        }
+                        }//end of each field
+                     }// if spread sheet col content wanted
 
-
+                    
 
                         i++;
-                    }
+             }//end of each col
 
-                    item.DataContainer.Add(_newDataItem);
-                    // _db.Items.Add(item);
+              item.DataContainer.Add(_newDataItem);
+               _db.Items.Add(item);
 
-                    if (maxEntries > 0 && rowCount == maxEntries)
-                        break;
+             if (maxEntries > 0 && rowCount == maxEntries)
+                   break;
 
-                    rowCount++;
-                }
+             rowCount++;
+         }//end of each row
 
-                //  _db.SaveChanges();
-            }
-        }
+                 _db.SaveChanges();
+     }
+   
 
         [Test]
         public void ReIndex()
@@ -971,7 +974,7 @@ Any public disclosures of information from the directory will be in aggregate fo
 
         private string[] GetColHeaders()
         {
-            //Mr Maarch 29 2022
+            //Mr March 29 2022
             //header on the sheet
             //* marked the column contain that we're interested
             return new string[] {
@@ -982,9 +985,9 @@ Any public disclosures of information from the directory will be in aggregate fo
                 "email*",
                 "Sub Code",
                 "position*",
-                "category",
+                "position - other keywords?",
                 "main_disciplines",
-                "faculty_or_department",
+                "faculty or department*",
                 "primary_area_of_research",
                 "long_description",
                 "Length",
@@ -998,20 +1001,17 @@ Any public disclosures of information from the directory will be in aggregate fo
                 "fee_id",
                 "expires_on",
                 "Layout",
-                "race",
-                "ethnicity",
-                "gender",
-                "pronouns",
-                "living with disability",
-                "Intersectional",
-                "keywords",
-                "intersectional",
-                "Research",
-                "description under 50 words",
+                "race*",
+                "ethnicity*",
+                "gender*",
+                "pronouns*",
+                "living with disability*",
+                "Identify keywords*",
+                "under 50 words*",
                 "Community based projects",
-                "Collaborations",
-                "Consent to share profile",
-                "Photo",
+                "Collaborations*",
+                "public profile*",
+                "consent*",
                 "Profile Link"
             };
 
