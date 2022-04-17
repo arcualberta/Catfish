d86288baf3cac6dc30f13743226320c99800ed0b
KamalRanaweera <ranaweer@ualberta.ca>
2022-04-17 00:19:14 -0600

    Bug fix: Added a metadata-set getter to allow displaying reference fields properly.

--- Catfish/Areas/Applets/UI/src/lib-components/item-details/Applet.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/item-details/Applet.vue
@@ -5,9 +5,11 @@
     import { state } from './store/state'
     import { actions, Actions } from './store/actions'
     import { mutations, Mutations } from './store/mutations'
+    import { getters } from './store/getters'
     import props, { QueryParameter, DataAttribute } from '../shared/props'
 
     import FieldContainer from '../shared/components/display/FieldContainer.vue'
+    import { FieldContainer as FieldContainerModel } from '../shared/models/fieldContainer'
 
 
     export default defineComponent({
@@ -31,18 +33,23 @@
             store.dispatch(Actions.GET_USER_ACTIONS);
             store.dispatch(Actions.LOAD_ITEM);
 
+            const getContainerName = (fc: FieldContainerModel) => {
+                return fc.name?.values.$values.map(txt => txt.value).join(" | ");
+            }
+
             return {
                 store,
                 queryParams,
                 dataItem: computed(() => store.state.item),
+                getContainerName,
                 isAdmin
             }
         },
         storeConfig: {
             state,
             actions,
-            mutations
-
+            mutations,
+            getters
         },
         methods: {
         }
@@ -53,29 +60,13 @@
     
     <div>
 
-        <div v-for="ms in dataItem.metadataSets.$values">
+        <div v-for="ms in dataItem?.metadataSets?.$values">
+            <h4>{{getContainerName(ms)}}</h4>
             <FieldContainer :model="ms" />
-            <!--<div v-for="metadataName in value.name.values.$values">
-                <h4>Metadata Set : {{metadataName.value}}</h4>
-                <div v-for="fieldValue in value.fields">
-                    {{fieldValue}}
-                    <FieldContainer :model="fieldValue" />
-                </div>
-            </div>-->
-
-
         </div>
-        <!--{{dataItem.dataContainer}}-->
         <div v-for="di in dataItem.dataContainer.$values">
+            <h4>{{getContainerName(di)}}</h4>
             <FieldContainer :model="di" />
-            <!--<div v-for="dataContainer in value.name.values.$values">
-        <h4> Data Container : {{dataContainer.value}}</h4>
-        <div v-for="fieldValue in dataContainer.fields">
-            {{fieldValue}}-->
-            <!--<FieldContainer :model="fieldValue" />-->
-            <!--</div>
-        </div>-->
-
         </div>
     </div>
 </template>
