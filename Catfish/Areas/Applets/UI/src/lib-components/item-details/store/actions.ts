8841911ecc45ac19e1f80626131479c3bd3f160f
KamalRanaweera <ranaweer@ualberta.ca>
2022-04-23 11:46:16 -0600

    Working on implementing the save API call.

--- Catfish/Areas/Applets/UI/src/lib-components/item-details/store/actions.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/item-details/store/actions.ts
@@ -1,6 +1,7 @@
 ﻿import { ActionTree } from 'vuex';
 import { State } from './state';
 import { Mutations } from './mutations';
+import { FlattenedFormFiledMutations } from '../../shared/store/flattened-form-field-mutations';
 
 //Declare ActionTypes
 export enum Actions {
@@ -39,14 +40,51 @@ export const actions: ActionTree<State, any> = {
     },
     [Actions.SAVE](store) {
 
-        const api = window.location.origin +
-            `/applets/api/items/${store.state.id}`;
-        console.log('Item Load API: ', api)
+        const item = store.state.item;
 
-        fetch(api)
-            .then(response => response.json())
+        const api = window.location.origin +
+            `/applets/api/items/update/${item?.id}`;
+        console.log('Item Update API: ', api)
+
+
+        //Validating the forms
+        if (!item)
+            return;
+
+        const formData = new FormData();
+
+        //Setting the serialized JSON form model to the datamodel variable in formData
+        formData.append('item', JSON.stringify(item));
+
+        //Adding all attachments uploaded to the files variable in formData
+        for (const key in store.state.flattenedFileModels) {
+            if (store.state.flattenedFileModels[key].length > 0) {
+                store.state.flattenedFileModels[key].forEach(file => {
+                    console.log("File: ", file.name)
+                    formData.append('files', file);
+                    formData.append('fileKeys', key);
+                })
+            }
+        }
+
+        fetch(api,
+            {
+                body: formData,
+                method: "post",
+                headers: {
+                    //"Content-Type": "multipart/form-data"
+                    "encType": "multipart/form-data"
+                }
+            }).then(response =>
+                response.json())
             .then(data => {
-                store.commit(Mutations.SET_ITEM, data);
+                console.log(JSON.stringify(data));
+                store.commit(FlattenedFormFiledMutations.REMOVE_FIELD_CONTAINERS);
+                store.commit(Mutations.SET_ITEM, null);
+
+            })
+            .catch(error => {
+                console.log(error)
             });
     },
 }
