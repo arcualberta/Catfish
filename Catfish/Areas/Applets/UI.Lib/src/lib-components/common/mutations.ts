4c599ca70e1372124088f590201fc8f13ad41180
KamalRanaweera <ranaweer@ualberta.ca>
2022-05-03 19:50:24 -0600

    Basic implementation of the common module.

---
+++ Catfish/Areas/Applets/UI.Lib/src/lib-components/common/mutations.ts
@@ -0,0 +1,30 @@
+﻿import { MutationTree } from 'vuex';
+import { State } from './state';
+
+//Declare MutationTypes
+export enum Mutations {
+    SET_SOURCE = 'SET_SOURCE',
+    SET_DATA_SERVICE_API_ROOT = 'SET_DATA_SERVICE_API_ROOT',
+    SET_PAGE_SERVICE_API_ROOT = 'SET_PAGE_SERVICE_API_ROOT',
+    SET_SOLR_SERVICE_API_ROOT = 'SET_SOLR_SERVICE_API_ROOT',
+}
+
+//Create a mutation tree that implement all mutation interfaces
+export const mutations: MutationTree<State> = {
+
+    [Mutations.SET_DATA_SERVICE_API_ROOT](state: State, apiRoot: string) {
+        state.dataServiceApiRoot = trimTrailingSlash(apiRoot);
+    },
+
+    [Mutations.SET_PAGE_SERVICE_API_ROOT](state: State, apiRoot: string) {
+        state.pageServiceApiRoot = trimTrailingSlash(apiRoot);
+    },
+
+    [Mutations.SET_SOLR_SERVICE_API_ROOT](state: State, apiRoot: string) {
+        state.solrServiceApiRoot = trimTrailingSlash(apiRoot);
+    },
+}
+
+function trimTrailingSlash(url: string) {
+    return url?.endsWith('/') ? url?.substring(0, url.length - 1) : url;
+}
--- Catfish/Areas/Applets/UI.Lib/src/lib-components/index.ts
+++ Catfish/Areas/Applets/UI.Lib/src/lib-components/index.ts
@@ -1,6 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 export { default as props } from './shared/props';
 
+/* common module */
+export * as common from './common/';
+
 /* search module */
 export * as search from './search/';
 export { default as FreeTextSearch } from './search/components/FreeTextSearch.vue'
