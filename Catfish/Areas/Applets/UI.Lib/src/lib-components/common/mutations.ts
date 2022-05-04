74a380c1d958f4ccf7f18886a9070b15182e4622
KamalRanaweera <ranaweer@ualberta.ca>
2022-05-04 08:12:16 -0600

    Commented out debug console logs.

--- Catfish/Areas/Applets/UI.Lib/src/lib-components/common/mutations.ts
+++ Catfish/Areas/Applets/UI.Lib/src/lib-components/common/mutations.ts
@@ -16,27 +16,27 @@ export const mutations: MutationTree<State> = {
 
     [Mutations.SET_PAGE_ID](state: State, id: Guid) {
         state.pageId = id;
-        console.log('SET_PAGE_ID: ', id)
+        //console.log('SET_PAGE_ID: ', id)
     },
 
     [Mutations.SET_BLOCK_ID](state: State, id: Guid) {
         state.blockId = id;
-        console.log('SET_BLOCK_ID: ', id,)
+        //console.log('SET_BLOCK_ID: ', id,)
     },
 
     [Mutations.SET_DATA_SERVICE_API_ROOT](state: State, apiRoot: string) {
         state.dataServiceApiRoot = trimTrailingSlash(apiRoot);
-        console.log('SET_DATA_SERVICE_API_ROOT: ', apiRoot)
+        //console.log('SET_DATA_SERVICE_API_ROOT: ', apiRoot)
     },
 
     [Mutations.SET_PAGE_SERVICE_API_ROOT](state: State, apiRoot: string) {
         state.pageServiceApiRoot = trimTrailingSlash(apiRoot);
-        console.log('SET_PAGE_SERVICE_API_ROOT: ', apiRoot)
+        //console.log('SET_PAGE_SERVICE_API_ROOT: ', apiRoot)
     },
 
     [Mutations.SET_SOLR_SERVICE_API_ROOT](state: State, apiRoot: string) {
         state.solrServiceApiRoot = trimTrailingSlash(apiRoot);
-        console.log('SET_SOLR_SERVICE_API_ROOT: ', apiRoot)
+        //console.log('SET_SOLR_SERVICE_API_ROOT: ', apiRoot)
     },
 }
 
