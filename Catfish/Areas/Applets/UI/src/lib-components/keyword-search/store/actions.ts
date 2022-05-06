16dd35ad41de107558394fd3afdbb73a7337b882
mruaini <melaniar@hotmail.com>
2022-05-06 10:36:57 -0600

    Removed action.SEARCH_FREE_TEXT from UI/KeywordSearch/action and UI.lib/Search/action -- don't need it

--- Catfish/Areas/Applets/UI/src/lib-components/keyword-search/store/actions.ts
+++ Catfish/Areas/Applets/UI/src/lib-components/keyword-search/store/actions.ts
@@ -107,39 +107,6 @@ export const actions: ActionTree<State, any> = {
         // console.log("set serch text: " + text);
         store.commit(Mutations.SET_FREE_TEXT_SEARCH, text);
     },
-    [Actions.SEARCH_FREE_TEXT](store) {
-        console.log("executing search for: " + store.state.freeSearchText?.toString());
-        const api = window.location.origin + `/applets/api/keywordsearch/items/`;
-        // console.log("Item Load API: ", api)
-
-        const formData = new FormData();
-        if (store.state.pageId)
-            formData.append("pageId", store.state.pageId.toString());
-        if (store.state.blockId)
-            formData.append("blockId", store.state.blockId.toString());
-
-        formData.append("offset", store.state.offset.toString());
-        formData.append("max", store.state.max.toString());
-        formData.append("queryParams", JSON.stringify(store.state.keywordQueryModel));
-
-        //MR April 27 2022, add freetextsearch
-        let freeText = store.state.freeSearchText ? store.state.freeSearchText : "";
-        formData.append("searchText", freeText);
-
-
-        fetch(api, {
-            method: 'POST', // or 'PUT'
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-              //  store.commit(Mutations.SET_RESULTS, data);
-            console.log("free search result");
-            console.log(JSON.stringify(data));
-        })
-        .catch((error) => {
-                console.error('Item Load API Error:', error);
-        });
-    },
+  
 }
 
