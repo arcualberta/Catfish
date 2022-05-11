50930f0ca5ae7801e4e315c38613297fbc4a3dab
mruaini <melaniar@hotmail.com>
2022-05-11 09:30:32 -0600

    add import all keyword related models

--- Catfish/Areas/Applets/UI.Lib/src/lib-components/search/index.ts
+++ Catfish/Areas/Applets/UI.Lib/src/lib-components/search/index.ts
@@ -9,3 +9,30 @@ export { ResultItem };
 
 import type { SearchOutput } from './models';
 export { SearchOutput };
+
+import type { KeywordIndex } from './models';
+export { KeywordIndex };
+
+import type { Keyword } from './models';
+export { Keyword };
+
+import type { SearchParams } from './models';
+export { SearchParams };
+
+
+import type { KeywordSource } from './models';
+export { KeywordSource };
+
+
+import type { KeywordField } from './models';
+export { KeywordField };
+
+import type { KeywordFieldContainer } from './models';
+export { KeywordFieldContainer };
+
+
+import type { KeywordQueryModel } from './models';
+export { KeywordQueryModel };
+
+import type { SolrField } from './models';
+export { SolrField };
