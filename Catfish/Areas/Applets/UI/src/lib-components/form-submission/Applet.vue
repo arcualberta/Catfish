5d556df20cffac8d4db8ad982099a8d7dd782cf9
mruaini <melaniar@hotmail.com>
2022-06-22 14:54:01 -0600

    TASK 1395-1396 add pinia to applet/UI

--- Catfish/Areas/Applets/UI/src/lib-components/form-submission/Applet.vue
+++ Catfish/Areas/Applets/UI/src/lib-components/form-submission/Applet.vue
@@ -1,71 +1,80 @@
 <script lang="ts">
-	import { Guid } from 'guid-typescript'
-	import { defineComponent, computed } from 'vue';
-	import { useStore } from 'vuex';
-	import props, { DataAttribute } from '../shared/props'
-	import { eValidationStatus } from '../shared/models/fieldContainer'
-	import { state, State } from './store/state'
-	import { actions, Actions } from './store/actions'
-    import { mutations, Mutations } from './store/mutations'
-    import { eSubmissionStatus } from '../shared/store/form-submission-utils'
-    import { FlattenedFormFiledMutations } from '../shared/store/flattened-form-field-mutations'
+	
 
+    import { Guid } from 'guid-typescript'
+    import {  defineComponent } from 'vue';
+    import { /*getActivePinia,*/ createPinia } from 'pinia'
+  
 
-	import SubmissionForm from '../shared/components/editor/FieldContainer.vue'
+    import { form, FormSubmission as SubmissionForm } from '@arcualberta/catfish-ui';
+
+    import { useFormStore } from './store/FormStore';
+    import props,{ DataAttribute } from '../shared/props'
 
     export default defineComponent({
         name: "FormSubmission",
+       props,
+		modules: {
+			form
+        },
 		components: {
-			SubmissionForm
+            SubmissionForm
         },
-        props,
+     
         setup(p) {
-           
-            console.log('props: ', JSON.stringify(p));
-			const dataAttributes = p.dataAttributes as DataAttribute;
 
-			const itemTemplateId = Guid.parse(dataAttributes["template-id"] as string);
-			const formId = Guid.parse(dataAttributes["form-id"] as string);
-			const collectionId = Guid.parse(dataAttributes["collection-id"] as string);
-			const groupId = dataAttributes["group-id"] ? Guid.parse(dataAttributes["group-id"] as string) : null;
+            
+         
+          
+            console.log("setup")
+            const pinia = createPinia();// getActivePinia();
+            console.log(JSON.stringify(pinia))
+            const dataAttributes = p.dataAttributes as DataAttribute;
 
-            const store = useStore();
+            const itemTemplateId = Guid.parse(dataAttributes["template-id"] as string);
+            const formId = Guid.parse(dataAttributes["form-id"] as string);
+            const collectionId = Guid.parse(dataAttributes["collection-id"] as string);
+            const groupId = dataAttributes["group-id"] ? Guid.parse(dataAttributes["group-id"] as string) : null;
+            const apiServiceRoot = dataAttributes["site-url"] as string;
 
-			store.commit(FlattenedFormFiledMutations.REMOVE_FIELD_CONTAINERS);
-			store.commit(Mutations.SET_ITEM_TEMPLATE_ID, itemTemplateId);
-			store.commit(Mutations.SET_FORM_ID, formId);
-			store.commit(Mutations.SET_COLLECTION_ID, collectionId);
-			store.commit(Mutations.SET_GROUP_ID, groupId);
-			//load the data
-			store.dispatch(Actions.LOAD_FORM);
+              const formStore =useFormStore(pinia)
 
-			return {
-				store,
-				submissionForm: computed(() => (store.state as State).form),
-				submissionStatus: computed(() => (store.state as State).submissionStatus),
-				eSubmissionStatus,
-				eValidationStatus,
-                submitForm: () => store.dispatch(Actions.SUBMIT_FORM)
-              
+           formStore.setFormId(formId);
+            formStore.setTemplateId(itemTemplateId);
+            formStore.setCollectionId(collectionId);
+            if (groupId)
+                formStore.setGroupId(groupId);
+            formStore.setApiRoot(apiServiceRoot);
+
+
+           // console.log("settings: api: " + formStore.apiRoot + " - frmId: " + formStore.formId + " -tmpId:  " + formStore.itemTemplateId + " - collId" + formStore.collectionId)
+            formStore.fetchData();
+            const submitForm = () => {
+                
+                if (form.helpers.validateForm(formStore.form as form.models.FieldContainer)) {
+                   // console.log('Submitting form ...')
+                    formStore.submitForm();
+                }
+                else
+                    console.log('Form validation failed ...')
             }
-		},
-		storeConfig: {
-			state,
-			actions,
-			mutations
+            return {
+               formStore,
+               submitForm,
+               pinia,
+            }
         },
+       
+		
     });
 </script>
 
 <template>
-	<div v-if="submissionForm && Object.keys(submissionForm).length > 0">
-		<SubmissionForm :model="submissionForm" />
-		<button class="btn btn-primary" @click="submitForm()">Submit</button>
-	</div>
-	<div v-if="submissionForm?.validationStatus === eValidationStatus.INVALID" class="alert alert-danger">Form validation failed.</div>
-	<div v-else>
-		<div v-if="submissionStatus === eSubmissionStatus.InProgress" class="alert alert-info">Submitting...</div>
-		<div v-if="submissionStatus === eSubmissionStatus.Success" class="alert alert-info">Submission successful</div>
-		<div v-if="submissionStatus === eSubmissionStatus.Fail" class="alert alert-danger">Submission failed</div>
-	</div>
+    <div class="page-body">
+         <SubmissionForm :model="formStore.form" :pinia-instance="pinia" />
+         <button class="submit-button" @click="submitForm()">Submit</button>
+    <div v-if="formStore.submissionFailed" class="alert alert-danger">Sorry, the form submission failed.</div>
+       
+
+    </div>
 </template>
