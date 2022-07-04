74c441088bcb26b73611561d8e9d9bb36b8a4357
KamalRanaweera <ranaweer@ualberta.ca>
2022-07-04 16:36:25 -0600

    Cleaned up the project folder

--- NPM.UI.Lib/src/lib-components/form/index.ts
+++
@@ -1,6 +0,0 @@
-
-export { eRefType, eValidationStatus } from './enumerations'
-export * as models from './models'
-export * as helpers from './helpers'
-
-
--- NPM.UI.Lib/src/lib-components/index.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-/* search module */
-export * as search from './search/';
-
-/* Form module */
-export * as form from './form/'
-export { default as FormSubmission } from './form/components/editor/FormSubmission.vue';
