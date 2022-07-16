2b00227409d7fb25ffa77061d3967c4e27c8d068
KamalRanaweera <ranaweer@ualberta.ca>
2022-07-16 00:36:14 -0600

    Implemented Task 1492: Create the router-based dev application

--- catfish-ui/src/App.vue
+++ catfish-ui/src/App.vue
@@ -2,25 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
-    import { getActivePinia } from 'pinia'
-
-    import { FormEditor } from './components'
 </script>
 
 <template>
-    <FormEditor :pinia-instance="getActivePinia()" repository-root="https://localhost:5020/" />
+    <div>
+        <nav class="navigationalmenu">
+            <router-link to="/" class="navigation-menu-box">Home</router-link> | 
+            <router-link to="/form-builder" class="navigation-menu-box">Form Builder</router-link>
+        </nav>
+    </div>
+
+    <router-view />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
