fc4eaff8d9494c0f318393ffd5e188def9376350
KamalRanaweera <ranaweer@ualberta.ca>
2022-07-17 09:54:21 -0600

    Updated appsettings.example.json Created CorsPolicies.cs constants.

---
+++ Catfish/CatfishExtensions/Constants/CorsPolicies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatfishExtensions.Constants
+{
+    public static class CorsPolicies
+    {
+        public static readonly string General = "General";
+        public static readonly string Restricted = "Restricted";
+    }
+}
