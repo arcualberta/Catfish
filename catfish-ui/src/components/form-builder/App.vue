1367c95181c9527d615a8554e88024effc31c3ce
KamalRanaweera <ranaweer@ualberta.ca>
2022-07-17 00:15:43 -0600

    Completed Task 1494: Add a save button

--- catfish-ui/src/components/form-builder/App.vue
+++ catfish-ui/src/components/form-builder/App.vue
@@ -36,7 +36,7 @@
                 FieldTypes,
                 newForm: () => store.newForm(),
                 saveForm: () => store.saveForm(),
-                isDisabled: computed(() => !store.form),
+                hasForm: computed(() => store.form ? true : false),
             }
         }
 
@@ -48,23 +48,23 @@
 <template>
     <h2>Form Builder</h2>
     <div class="control">
-        <button id="new-form" :disabled="!isDisabled" @click="newForm">New Form</button>
-        <button id="save-form" :disabled="isDisabled" @click="saveForm">Save</button>
+        <button type="button" class="btn btn-primary" :disabled="hasForm" @click="newForm">New Form</button>
+        <button type="button" class="btn btn-success" :disabled="!hasForm" @click="saveForm">Save</button>
     </div>
     <div class="toolbar">
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.SingleLine)">+ TextField</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.Paragraph)">+ Paragraph</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.RichText)">+ Rich Text</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.Date)">+ Date</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.DateTime)">+ Date/Time</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.Decimal)">+ Decimal</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.Integer)">+ Integer</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.Email)">+ Email</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.CheckBoxes)">+ Checkboxes</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.DataList)">+ Data List</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.RadioButtons)">+ Radio Buttons</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.DropDown)">+ Drop Down</button>
-        <button :disabled="isDisabled" @click="store.newField(FieldTypes.InfoSection)">+ Info Section</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.SingleLine)">+ TextField</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.Paragraph)">+ Paragraph</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.RichText)">+ Rich Text</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.Date)">+ Date</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.DateTime)">+ Date/Time</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.Decimal)">+ Decimal</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.Integer)">+ Integer</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.Email)">+ Email</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.CheckBoxes)">+ Checkboxes</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.DataList)">+ Data List</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.RadioButtons)">+ Radio Buttons</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.DropDown)">+ Drop Down</button>
+        <button :disabled="!hasForm" @click="store.newField(FieldTypes.InfoSection)">+ Info Section</button>
     </div>
     <hr />
     {{store.form}}
