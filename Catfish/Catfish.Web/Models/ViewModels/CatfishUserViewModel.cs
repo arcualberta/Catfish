8ac76d4ab5d081e6a771b5adcb93fa2f66ed4c20
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-07-18 12:20:28 -0600

    Google login moved back to catfish web

---
+++ Catfish/Catfish.Web/Models/ViewModels/CatfishUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Catfish.Web.Models.ViewModels
+{
+    public class CatfishUserViewModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+    }
+}
