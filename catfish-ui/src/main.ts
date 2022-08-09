d3b6bb77f58be1be5a92950c7592fda9b6787018
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-08-09 09:56:25 -0600

    Error with bootstrap vue component

--- catfish-ui/src/main.ts
+++ catfish-ui/src/main.ts
@@ -3,8 +3,14 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap"
 import vue3GoogleLogin from 'vue3-google-login'
 
 /* import the fontawesome core */
@@ -27,6 +33,10 @@ createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .use(createPinia())
     .use(router)
+    // Make BootstrapVue available throughout your project
+    .use(BootstrapVue)
+    // Optionally install the BootstrapVue icon components plugin
+    .use(IconsPlugin)
     .use(vue3GoogleLogin, {
         clientId: '589183038778-u256nlels7v2443j3h1unvtp367f80s4.apps.googleusercontent.com'
     })
