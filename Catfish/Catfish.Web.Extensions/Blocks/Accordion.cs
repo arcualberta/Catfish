6436e8e011afb12cf9df9240e55175e654a2a2ad
KamalRanaweera <ranaweer@ualberta.ca>
2022-08-19 00:35:12 -0600

    Implemented Story 1689: Accordion block

---
+++ Catfish/Catfish.Web.Extensions/Blocks/Accordion.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CatfishWebExtensions.Blocks
+{
+    [BlockGroupType(Name = "Accordion", Category = "Content", Icon = "fas fas-image")]
+    public class Accordion : BlockGroup
+    {
+        [Display(Name = "CSS Classes")]
+        public StringField CSSClasses { get; set; }
+
+    }
+}
---
+++ catfish-ui/src/appsettings.ts
@@ -0,0 +1,5 @@
+export default {
+    googleApiKey: "AIzaSyDJspy_ravgEMwaDCDtgerl3WWp9O3_7LM",
+    googleCalendarId: "ualberta.ca_kdp9enkplai8s5ipu2efknjels@group.calendar.google.com",
+    maxEvents:10
+}
