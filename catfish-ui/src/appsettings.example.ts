6ea61d5841e02a012858ac680b68a9ff394f6e29
KamalRanaweera <ranaweer@ualberta.ca>
2022-08-19 16:59:54 -0600

    Added appsettings.example.ts

---
+++ catfish-ui/src/appsettings.example.ts
@@ -0,0 +1,5 @@
+export default {
+    googleApiKey: "Use dev credentials from https://docs.google.com/document/d/1N_y4aQupxPKPGh2eaxpOqCmc_75QionPp4U_MoY3gZQ/edit",
+    googleCalendarId: "Use dev credentials from https://docs.google.com/document/d/1N_y4aQupxPKPGh2eaxpOqCmc_75QionPp4U_MoY3gZQ/edit",
+    maxEvents:10
+}
