da370d47469a54a0b59c1587d1b055ba13a776f1
KamalRanaweera <ranaweer@ualberta.ca>
2022-08-19 16:57:40 -0600

    Removed appsettings.ts from git repo

--- catfish-ui/src/appsettings.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    googleApiKey: "AIzaSyDJspy_ravgEMwaDCDtgerl3WWp9O3_7LM",
-    googleCalendarId: "ualberta.ca_kdp9enkplai8s5ipu2efknjels@group.calendar.google.com",
-    maxEvents:10
-}
