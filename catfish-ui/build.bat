2e18e7db724e2e1941351becb74eb4b8d0dabe35
mruaini <melaniar@hotmail.com>
2022-08-31 10:42:34 -0600

    change vite to npm run

--- catfish-ui/build.bat
+++ catfish-ui/build.bat
@@ -1,5 +1,5 @@
 
 :: Run this batch file as: cmd /c build.bat
 
-vite build && xcopy dist\*.js ..\Catfish\Catfish.Web\wwwroot\assets\applets\ /Y && xcopy dist\*.css ..\Catfish\Catfish.Web\wwwroot\assets\applets\ /Y
+npm run build && xcopy dist\*.js ..\Catfish\Catfish.Web\wwwroot\assets\applets\ /Y && xcopy dist\*.css ..\Catfish\Catfish.Web\wwwroot\assets\applets\ /Y
 
