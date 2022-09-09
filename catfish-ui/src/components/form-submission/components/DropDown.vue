945411dc05fda36c1d9ed9ad19190bd9fec70fb4
KamalRanaweera <ranaweer@ualberta.ca>
2022-09-09 15:36:55 -0600

    Fixed Typescript checking

--- catfish-ui/src/components/form-submission/components/DropDown.vue
+++ catfish-ui/src/components/form-submission/components/DropDown.vue
@@ -13,7 +13,7 @@
 
     const fieldData = computed(() => store.formData.fieldData?.find(fd => fd.fieldId == props.model.id) as FieldData)
     const selectedOptionId = computed({
-        get: () => fieldData?.value?.selectedOptionIds && fieldData.value.selectedOptionIds.length > 0 ? fieldData.value.selectedOptionIds[0] : Guid.EMPTY,
+        get: () => fieldData?.value?.selectedOptionIds && fieldData.value.selectedOptionIds.length > 0 ? fieldData.value.selectedOptionIds[0] as unknown as string : Guid.EMPTY as unknown as string,
         set: optId => fieldData.value.selectedOptionIds = [optId as unknown as Guid]
     })
 </script>
@@ -21,7 +21,7 @@
 <template>
     <div class="col-sm-3">
         <select v-model="selectedOptionId" class="form-select">
-            <option v-for="opt in model.options" :key="opt.id" :value="opt.id">{{formHelper.getOptionText(opt, store.lang)}}</option>
+            <option v-for="opt in model.options" :key="opt.id" :value="(opt.id as unknown as string)">{{formHelper.getOptionText(opt, store.lang)}}</option>
         </select>
     </div>
     
