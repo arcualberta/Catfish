968ab45c8754703a5d46c3d93c08eed9fb8069a0
mruaini <melaniar@hotmail.com>
2022-09-16 10:42:12 -0600

    modified store/index and add button to entityTempateBuilder.vue

--- catfish-ui/src/components/entity-template-builder/store/index.ts
+++ catfish-ui/src/components/entity-template-builder/store/index.ts
@@ -9,7 +9,22 @@ export const useEntityTemplateBuilderStore = defineStore('EntityTemplateBuilderS
         
     }),
     actions: {
-       
+        newTemplate() {
+            let api = `https://localhost:5020/api/entityTemplate/`;
+            console.log(api)
+            fetch(api, {
+                method: 'POST'
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    //this.form = data;
+                })
+                .catch((error) => {
+                    console.error('create Entity Template :', error);
+                });
+
+        },
     },
     
 
