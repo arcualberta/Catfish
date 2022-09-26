95e1289a4a46b0fbee0a678a72e99664201e1698
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-09-25 22:03:06 -0600

    TransientMessage.vue added

---
+++ catfish-ui/src/components/shared/components/transient-message/TransientMessage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import { TransientMessage} from './models';
+
+    const props = defineProps<{
+        model: TransientMessage,
+    }>();
+</script>
+
+<template>
+    <transition name="fade">
+        <p v-if="model.transientMessage" :class="'alert alert-' + model.transientMessageClass">{{model.transientMessage}}</p>
+    </transition>
+</template>
