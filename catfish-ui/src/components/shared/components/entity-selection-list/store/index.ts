e4610491d0543261c062ee679f73e9f7fc43af10
mruaini <melaniar@hotmail.com>
2022-10-17 13:38:55 -0600

    Update entity-selection-list/store/index and EntitySelectionList.vue

--- catfish-ui/src/components/shared/components/entity-selection-list/store/index.ts
+++ catfish-ui/src/components/shared/components/entity-selection-list/store/index.ts
@@ -3,7 +3,7 @@ import { eEntityType, eSearchTarget } from "@/components/shared/constants";
 import { defineStore } from "pinia";
 import { default as config } from "@/appsettings";
 
-export const useEntitySelectStore = (storeId: string) => defineStore(`entitySelect/${storeId}`, {
+/*export const useEntitySelectStore = (storeId: string) => defineStore(`entitySelect/${storeId}`, {
     state: () => ({
       entities: {} as EntityData[],
       entitySearchResult: null as unknown as EntitySearchResult
@@ -24,4 +24,28 @@ export const useEntitySelectStore = (storeId: string) => defineStore(`entitySele
             });
           }
     }
-  })(); 
+  })(); */
+
+  export const useEntitySelectStore = defineStore('EntitySelectStore', {
+    state: () => ({
+        entities: {} as EntityData[],
+        entitySearchResult: null as unknown as EntitySearchResult
+      }),
+      
+      actions:{
+            seach(entityType: eEntityType, searchTarget: eSearchTarget, searchText: string){
+              let api = config.dataRepositoryApiRoot + "/api/entities/"+ entityType + "/" + searchTarget + "/" + searchText + "/" +offset + "/" + max ;
+              fetch(api, {
+                  method: 'GET'
+              })
+              .then(response => response.json())
+              .then(data => {
+                      this.entitySearchResult = data as EntitySearchResult;
+              })
+              .catch((error) => {
+                  console.error('Load Entities API Error:', error);
+              });
+            }
+        }
+
+  })
