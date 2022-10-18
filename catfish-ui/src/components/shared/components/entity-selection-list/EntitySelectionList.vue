ca9a5849588da79d019d8bf1abd108340e28eff4
mruaini <melaniar@hotmail.com>
2022-10-18 16:28:06 -0600

    commented unused import statement

--- catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
+++ catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
       import { Guid } from 'guid-typescript';
-      import {computed} from 'vue'
+      //import {computed} from 'vue'
       import { Pinia, storeToRefs } from 'pinia' 
       import { useEntitySelectStore } from './store';
-      import { EntityData, EntitySearchResult, EntityEntry } from "@/components/entity-editor/models";
+     //import { EntityData, EntitySearchResult, EntityEntry } from "@/components/entity-editor/models";
 import { eEntityType, eSearchTarget } from '../../constants';
 
        const props = defineProps<{
@@ -12,11 +12,10 @@ import { eEntityType, eSearchTarget } from '../../constants';
     //const tableId = props.table.id // Just as an example
         const storeId =Guid.create();
        const entityListStore = useEntitySelectStore(props.piniaInstance);
+
        entityListStore.seach(eEntityType.Item, eSearchTarget.Title, "title");
        const {entitySearchResult} = storeToRefs(entityListStore);
-       //const entityEntries= computed(()=> entitySearchResult.value.result)
-
-       console.log(JSON.stringify(entitySearchResult))
+       
 </script>
 
 <template>
