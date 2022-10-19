76a04a94b84249bf5511011f81ae51e4de79208f
mruaini <melaniar@hotmail.com>
2022-10-19 12:34:57 -0600

    Allow creating multiple store with unique id

--- catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
+++ catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
@@ -11,8 +11,8 @@ import { eEntityType, eSearchTarget } from '../../constants';
     }>();
     //const tableId = props.table.id // Just as an example
         const storeId =Guid.create();
-       const entityListStore = useEntitySelectStore(props.piniaInstance);
-
+      // const entityListStore = useEntitySelectStore(props.piniaInstance);
+       const entityListStore = useEntitySelectStore("store1");
        entityListStore.seach(eEntityType.Item, eSearchTarget.Title, "title");
        const {entitySearchResult} = storeToRefs(entityListStore);
        
--- catfish-ui/src/components/shared/components/entity-selection-list/store/index.ts
+++ catfish-ui/src/components/shared/components/entity-selection-list/store/index.ts
@@ -3,29 +3,49 @@ import { eEntityType, eSearchTarget } from "@/components/shared/constants";
 import { defineStore } from "pinia";
 import { default as config } from "@/appsettings";
 
-/*export const useEntitySelectStore = (storeId: string) => defineStore(`entitySelect/${storeId}`, {
-    state: () => ({
-      entities: {} as EntityData[],
-      entitySearchResult: null as unknown as EntitySearchResult
-    }),
-    
-    actions:{
-          seach(entityType: eEntityType, searchTarget: eSearchTarget, searchText: string){
-            let api = config.dataRepositoryApiRoot + "/api/entities/"+ entityType + "/" + searchTarget + "/" + searchText + "/" +offset + "/" + max ;
-            fetch(api, {
-                method: 'GET'
-            })
-            .then(response => response.json())
-            .then(data => {
-                    this.entitySearchResult = data as EntitySearchResult;
-            })
-            .catch((error) => {
-                console.error('Load Entities API Error:', error);
-            });
-          }
-    }
-  })(); */
 
+const definedStores = new Map<string, ReturnType<typeof defineEntitySelectStore>>();
+const entitySelectStoreFactory = (storeId: string) => {
+  if (!definedStores.has(storeId)) {
+    definedStores.set(storeId, defineEntitySelectStore(storeId));
+  }
+
+  return definedStores.get(storeId) as ReturnType<
+    typeof defineEntitySelectStore
+  >;
+};
+
+function defineEntitySelectStore<Id extends string>(storeId: Id) {
+  return defineStore(`entitySelectStore/${storeId}`, {
+    state: () => ({
+        entities: {} as EntityData[],
+        entitySearchResult: null as unknown as EntitySearchResult
+      }),
+      
+      actions:{
+            seach(entityType: eEntityType, searchTarget: eSearchTarget, searchText: string){
+                let offset=0;
+                let max=10;
+                let api = config.dataRepositoryApiRoot + "/api/entities/"+ entityType + "/" + searchTarget + "/" + searchText + "/" +offset + "/" + max ;
+              fetch(api, {
+                  method: 'GET'
+              })
+              .then(response => response.json())
+              .then(data => {
+                      this.entitySearchResult = data as EntitySearchResult;
+              })
+              .catch((error) => {
+                  console.error('Load Entities API Error:', error);
+              });
+            }
+        }
+  });
+}
+export const useEntitySelectStore = (storeId: string) => {
+  return entitySelectStoreFactory(storeId)();
+};
+
+  /* standard pinia store 
   export const useEntitySelectStore = defineStore('EntitySelectStore', {
     state: () => ({
         entities: {} as EntityData[],
@@ -51,3 +71,4 @@ import { default as config } from "@/appsettings";
         }
 
   })
+*/
