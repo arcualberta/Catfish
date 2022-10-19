fc9e5d8afde3e5bc57d964e6f5c08e04891d60a8
mruaini <melaniar@hotmail.com>
2022-10-19 14:55:23 -0600

    Task 1945: Add EntitySearchBox component

--- catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
+++ catfish-ui/src/components/shared/components/entity-selection-list/EntitySelectionList.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
       import { Guid } from 'guid-typescript';
-      //import {computed} from 'vue'
+      import {computed} from 'vue'
       import { Pinia, storeToRefs } from 'pinia' 
       import { useEntitySelectStore } from './store';
      //import { EntityData, EntitySearchResult, EntityEntry } from "@/components/entity-editor/models";
     import { eEntityType, eSearchTarget } from '../../constants';
+    import {default as EntitySearchBox} from './EntitySearchBox.vue'
+    
 
        const props = defineProps<{
         storeId: string
     }>();
-    //const tableId = props.table.id // Just as an example
-        const storeId =Guid.create();
+    
+       // const storeId = computed(()=> props.storeId)
       // const entityListStore = useEntitySelectStore(props.piniaInstance);
        const entityListStore = useEntitySelectStore(props.storeId);
-       entityListStore.seach(eEntityType.Item, eSearchTarget.Title, "title");
+      // entityListStore.seach(eEntityType.Item, eSearchTarget.Title, "title");
        const {entitySearchResult} = storeToRefs(entityListStore);
        
 </script>
 
 <template>
+     <EntitySearchBox :storeId="storeId" :entityType="eEntityType.Item" />
     <h4>Entity Selection List</h4>
     <div v-for="entry in entitySearchResult?.result" v-bind:key="entry.id.toString()">
         <div>{{entry.title}}  => >{{entry.description}}</div>
