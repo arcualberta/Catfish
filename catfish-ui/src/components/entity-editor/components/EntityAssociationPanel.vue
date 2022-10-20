29e172180c05f350722d22b5b2a199223a688e38
mruaini <melaniar@hotmail.com>
2022-10-20 16:33:27 -0600

    TASK 1861: modify the EntityAssociationPanel to display the selected entity

--- catfish-ui/src/components/entity-editor/components/EntityAssociationPanel.vue
+++ catfish-ui/src/components/entity-editor/components/EntityAssociationPanel.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-    import { Pinia } from 'pinia'
+    import { Pinia, storeToRefs } from 'pinia'
     import { computed } from "vue"
     import { eEntityType } from "../../shared/constants"
     import { useEntityEditorStore } from "../store"
     import { EntityData } from '../models'
     import { EntitySelectionList } from "../../shared/components/"
+    import { Guid } from 'guid-typescript'
+
     const store = useEntityEditorStore();
     const props = defineProps<{
         entity: EntityData,
         relationshipType: string,
         panelTitle:string
     }>();
+
+    const {entity, storeId} = storeToRefs(store);
     const dataList = computed(() => props.entity?.subjectRelationships.filter(form => form.name == props.relationshipType))
     
+    const addRelationship= ()=>{store.AddToRelationObject()};
     
 </script>
 
@@ -31,7 +36,11 @@
     <b-row>
         <b-col class="col-sm-5">
             <div class="form-field-border">
-                
+                <div v-if="entity.subjectRelationships">
+                  <div v-for="rel in entity.subjectRelationships" :key="rel.subjectEntityId">
+                    {{rel.subjectEntityId}}
+                  </div>
+                </div>
             </div>
         </b-col>
         <b-col class="col-sm-2">
@@ -40,7 +49,7 @@
 
                 </b-col>
                 <b-col class="col-sm-4">
-                    <button class="btn btn-primary"><font-awesome-icon icon="fa-solid fa-arrow-left" /></button>
+                    <button class="btn btn-primary" @click="addRelationship"><font-awesome-icon icon="fa-solid fa-arrow-left" /></button>
                 </b-col>
                 <b-col class="col-sm-4">
 
@@ -49,7 +58,7 @@
         </b-col>
         <b-col class="col-sm-5">
             <div class="form-field-border">
-                <EntitySelectionList></EntitySelectionList>
+                <EntitySelectionList :storeId="storeId.toString()" :entityType="eEntityType.Item"></EntitySelectionList>
             </div>
         </b-col>
     </b-row>
