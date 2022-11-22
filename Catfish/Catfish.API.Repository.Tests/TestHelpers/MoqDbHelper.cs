9f61072c5e274cf1b1d02ae3787405a8f707148d
KamalRanaweera <ranaweer@ualberta.ca>
2022-11-22 09:31:52 -0700

    Added Moq framework references.

---
+++ Catfish/Catfish.API.Repository.Tests/TestHelpers/MoqDbHelper.cs
@@ -0,0 +1,25 @@
+﻿using Catfish.API.Repository.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.API.Repository.Tests.TestHelpers
+{
+    public static class MoqDbHelper
+    {
+        //public static Mock<RepoDbContext>
+        private static Mock<RepoDbContext> CreateEmptyContext()
+        {
+            Mock<RepoDbContext> context = new Mock<RepoDbContext>(MockBehavior.Strict);
+
+
+            return context;
+        }
+
+    }
+}
