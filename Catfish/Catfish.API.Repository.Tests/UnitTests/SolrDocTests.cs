7f6de156f4cf42e549648d59465999a536256128
KamalRanaweera <ranaweer@ualberta.ca>
2022-11-21 19:13:29 -0700

    Created skeleton for the Catfish.API.Repository.Tests project.

---
+++ Catfish/Catfish.API.Repository.Tests/UnitTests/SolrDocTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish.API.Repository.Tests.UnitTests
+{
+    public class SolrDocTests
+    {
+        [Fact]
+        public void BuildSolrDoc()
+        {
+           //Use Moq to build a dummy EntityData object
+           //Create the SolrDoc from the EntityData
+           //Read some fields for each field type SolrDoc and verify that they contins the correct values
+
+        }
+    }
+}
