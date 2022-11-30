239d2d578c31d70d7d08f8b11abb89b0f9865fa1
KamalRanaweera <ranaweer@ualberta.ca>
2022-11-30 14:51:19 -0700

    Added the FontAwesome to the exported library.

--- catfish-ui/src/library-exports.ts
+++ catfish-ui/src/library-exports.ts
@@ -12,5 +12,19 @@
 /**
  * Exporting the external Bootstrap Components
  */
- export {BootstrapVue3} from './external/bootstrap-vue-3/BootstrapVue'
+export {BootstrapVue3} from './external/bootstrap-vue-3/BootstrapVue'
 export {BootstrapVue3Icons} from './external/bootstrap-vue-3-icons/BootstrapVueIcons'
+
+/**
+ * Exporting Font Awesome librarty
+ */
+import * as faIcons from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faIcons.faCircleCheck)
+library.add(faIcons.faCircleXmark)
+library.add(faIcons.faPenToSquare)
+library.add(faIcons.faCirclePlus)
+library.add(faIcons.faQuestionCircle)
+library.add(faIcons.faThList)
+library.add(faIcons.faArrowLeft)
+export {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
