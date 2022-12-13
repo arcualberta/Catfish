1a7f8e73338d572688d14963e0b063f327d03f85
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-12-13 10:13:36 -0700

    model folder renamed to models field selector vue created

--- catfish-ui/src/components/solr-search-panel/App.vue
+++ catfish-ui/src/components/solr-search-panel/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+    import { default as FieldSelector } from './components/FieldSelector.vue'
+    import { SolrFields } from './appsettings';
     </script>
 <template>
     Solr search
+    <FieldSelector :searchFields="SolrFields"></FieldSelector>
 
 </template>
 
---
+++ catfish-ui/src/components/solr-search-panel/components/FieldSelector.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { Pinia } from 'pinia'
+    import { SearchFieldDefinition } from '../models'
+    import { computed, ref } from 'vue';
+    import { eFieldType } from '../../shared/constants'
+
+    const props = defineProps<{
+        searchFields: SearchFieldDefinition[]
+    }>();
+
+    const type = ref(eFieldType.Text);
+    const readOnly = ref(false);
+    </script>
+<template>
+    Field Selector
+    <b-row>
+        <b-col class="col-sm-4">
+
+        </b-col>
+        <b-col class="col-sm-4">
+           <input type=text  readonly="readonly"/>
+        </b-col>
+        <b-col class="col-sm-4">
+            <select class="form-select">
+                <option v-for="opt in searchFields">{{opt.label}}</option>
+            </select>
+        </b-col>
+    </b-row>
+
+</template>
