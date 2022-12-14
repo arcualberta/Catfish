396a3d8f24e72415cc865b97e2ceb3698ce5115e
KamalRanaweera <ranaweer@ualberta.ca>
2022-12-14 10:10:34 -0700

    Ensured that we use one instance of the HttpClient class for SolrService within the application. Added a basic query example to the solr-search-panel/App.vue

--- catfish-ui/src/components/solr-search-panel/App.vue
+++ catfish-ui/src/components/solr-search-panel/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
     import { default as FieldConstraint } from './components/FieldConstraint.vue'
     import { SolrFields } from './appsettings';
+import { useSolrSearchStore } from './store';
+
+    const store = useSolrSearchStore();
+
+    const query = () => store.query();
+
     </script>
 <template>
     Solr search
     <FieldConstraint :searchFields="SolrFields"></FieldConstraint>
 
+    <button @click="query">Query</button>
+    <div>
+        Query time: {{store.queryTime}} seconds<br /><br />
+        {{store.queryResult}}
+    </div>
 </template>
 
