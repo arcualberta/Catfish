71ce4ba3f93729f125724fd8ce7fe19f9884b16e
KamalRanaweera <ranaweer@ualberta.ca>
2022-12-20 09:51:34 -0700

    Enabled swagger on repository production for now.

--- Catfish/Catfish.API.Repository/Program.cs
+++ Catfish/Catfish.API.Repository/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddScoped<ISolrService, SolrService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
