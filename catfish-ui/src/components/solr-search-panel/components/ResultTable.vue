c218aa9aac5574d9549fca1382206d69cfc899f7
mruaini <melaniar@hotmail.com>
2022-12-22 13:41:10 -0700

    TASK 2202: add download CSV button

--- catfish-ui/src/components/solr-search-panel/components/ResultTable.vue
+++ catfish-ui/src/components/solr-search-panel/components/ResultTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">import { computed } from 'vue';
-import { SearchResult, SolrFieldData } from '../models';
+import { SearchResult, SolrFieldData, SolrResultEntry } from '../models';
 import { useSolrSearchStore } from '../store';
 import { faListDots } from '@fortawesome/free-solid-svg-icons';
 
@@ -20,14 +20,38 @@ import { faListDots } from '@fortawesome/free-solid-svg-icons';
        
             const flData = data.find((dt)=>{
                     return dt.key === fdkey
-            });
-            
+            }); 
           return flData? flData.value: "";
     }
+
+    
+ const downloadCSVData= () => {
+    let csv = '';
+    //header labels
+    fieldDefs.value.forEach(fldef => {
+        csv += fldef.label
+    });
+     //data
+    props.model.resultEntries.forEach((row: SolrResultEntry) => {
+          
+            fieldDefs.value.forEach(fldef => {
+                    
+                    csv += getFieldValue(row.data, fldef.name) + ","
+            });
+            csv += "\n";
+    });
+ 
+    const anchor = document.createElement('a');
+    anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+    anchor.target = '_blank';
+    anchor.download = 'showtimesSearchResult.csv';
+    anchor.click();
+}
 
 </script>
 
 <template>
+   <div><button v-if="model.resultEntries.length > 0" @click="downloadCSVData">Download CSV</button></div>
     <div class="mt-2">
         <span v-if="hasPrev" class="link" @click="store.previous()">&lt;&lt;&lt;</span>
         {{ first }} to {{ last }} of {{ model.totalMatches }}
@@ -42,10 +66,7 @@ import { faListDots } from '@fortawesome/free-solid-svg-icons';
         <tbody>
             <tr v-for="row in model.resultEntries">
                 
-               <!-- <td v-for="fld in row.data" :key="fld.key">
-                     {{fld.value}}
-                </td>-->
-                <td v-for="fh in fieldDefs" :key="fh.name">
+                <td v-for="fh in fieldDefs" :key="fh.name" :class="fh.name==='synopsis_t' || fh.name === 'casts_ts'?'truncate':''">
                     
                    {{getFieldValue(row.data, fh.name)}}
                 </td>
