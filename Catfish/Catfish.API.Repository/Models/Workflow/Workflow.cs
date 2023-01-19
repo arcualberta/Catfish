ee2262845a20d09bce093da377abef6501009e4c
mruaini <melaniar@hotmail.com>
2023-01-18 17:42:02 -0700

    Update Workflow model

--- Catfish/Catfish.API.Repository/Models/Workflow/Workflow.cs
+++ Catfish/Catfish.API.Repository/Models/Workflow/Workflow.cs
@@ -7,5 +7,11 @@
         public string Description { get; set; }
         public Guid EntityTemplateId { get; set; }
         public List<Trigger> Triggers { get; set; } = new List<Trigger>();
+
+        public List<WorkflowAction>  Actions { get; set; } = new List<WorkflowAction>();
+        public List<WorkflowState>  States { get; set; } = new List<WorkflowState>();
+        public List<Role> Roles { get;set; } = new List<Role>();
+        public List<EmailTemplate> EmailTemplates { get; set; }= new List<EmailTemplate>();
+        public object Popups { get; set; }
     }
 }
