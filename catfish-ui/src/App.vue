6681634e7b8695e78d3eb6b06df72cfba058d2e0
KamalRanaweera <ranaweer@ualberta.ca>
2023-01-30 23:26:57 -0700

    Bug fix

--- catfish-ui/src/App.vue
+++ catfish-ui/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
     // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-    import { computed } from '@vue/runtime-core';
+    import { computed } from 'vue';
     import { useRouter } from 'vue-router'
     import { useLoginStore } from './components/login/store';
 
