2351c9bb9e79ebed01cf441cc24d2abdf0074433
Isuru Wickramasinghe <iwickram@ualberta.ca>
2023-01-31 11:48:20 -0700

    accordion started

--- catfish-ui/src/components/workflow-builder/components/WorkflowActionList.vue
+++ catfish-ui/src/components/workflow-builder/components/WorkflowActionList.vue
@@ -31,7 +31,19 @@
             </b-list-group-item>
         </b-list-group>
     </div>
-    
+    <div class="accordion" role="tablist" v-for="action1 in store.workflow?.actions" :key="action1.name">
+        <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.{accordion-1} variant="info">{{action1.name}}</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
+          <b-card-text></b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+    </div>
     <div class="header-style">Actions <font-awesome-icon icon="fa-solid fa-circle-plus" style="color:#1ca5b8" @click="Toggle()"/></div>
     <div v-if="store.showActionPanel">
         <add-action  :editMode="editMode" :editActionId="(actionId as Guid)"/>
