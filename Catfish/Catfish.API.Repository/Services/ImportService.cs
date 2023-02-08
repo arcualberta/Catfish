969d2f258d454d4306dd9d39ffa0f79087b7b044
mruaini <melaniar@hotmail.com>
2023-02-08 12:57:18 -0700

    TASK 2424-2425: Add basic skeleton api controller/interface/model for Import

---
+++ Catfish/Catfish.API.Repository/Services/ImportService.cs
@@ -0,0 +1,43 @@
+﻿using Catfish.API.Repository.Interfaces;
+
+using Catfish.API.Repository.Models.Import;
+using ExcelDataReader;
+using System.Data;
+
+namespace Catfish.API.Repository.Services
+{
+    public class ImportService : IImportService
+    {
+        public bool ImportFromExcel(ExcelData dataModel)
+        {
+           System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            //DEBUG
+            string folderRoot = "C:\\Projects\\JLPT";
+            string sourceFile = Path.Combine(folderRoot, "all-data-groupize.xlsx");
+
+            using (var stream = System.IO.File.Open(sourceFile, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader excelDataReader = ExcelDataReader.ExcelReaderFactory.CreateReader(stream);
+                var conf = new ExcelDataSetConfiguration()
+                {
+                    ConfigureDataTable = a => new ExcelDataTableConfiguration
+                    {
+                        UseHeaderRow = true
+                    }
+                };
+                DataSet dataSet = excelDataReader.AsDataSet(conf);
+
+                DataRowCollection rows = dataSet.Tables["Attendees"].Rows;// dataSet.Tables[dataModel.PrimarySheet].Rows;//read all row in a sheet
+
+                for (int i = 0; i < rows.Count; i++)//foreach (DataRow row in rows)
+                {
+                    var cols = rows[i].ItemArray; //get all cols in that row
+                }
+            }
+
+            return true;
+        }
+    } 
+    
+}
