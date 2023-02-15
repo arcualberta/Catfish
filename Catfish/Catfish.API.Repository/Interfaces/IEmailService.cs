953d6467e782feefb3cd8c95dadb63e24a762651
Isuru Wickramasinghe <iwickram@ualberta.ca>
2023-02-15 11:24:10 -0700

    Email service class and interface implemented

---
+++ Catfish/Catfish.API.Repository/Interfaces/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿namespace Catfish.API.Repository.Interfaces
+{
+    public interface IEmailService
+    {
+        void SendEmail(Email email);
+    }
+    public class Email
+    {
+        public string FromEmail { get; set; }
+        public string Body { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        public string RecipientEmail { get; set; }
+    }
+}
