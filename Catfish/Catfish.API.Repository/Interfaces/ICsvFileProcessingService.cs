c5205a88bb5af2a799b71921863a97685d7c8a08
mruaini <melaniar@hotmail.com>
2023-03-01 13:28:56 -0700

    TASK 2518 : Add CSVFileProcessing Interface/Service

---
+++ Catfish/Catfish.API.Repository/Interfaces/ICsvFileProcessingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Catfish.API.Repository.Interfaces
+{
+    public interface ICsvFileProcessingService
+    {
+        public EntityTemplate ImportEntityTemplateSchema(string templateName,IFormFile file);
+        public HttpStatusCode ImportDataFromExcel(Guid templateId, IFormFile file);
+
+    }
+}
