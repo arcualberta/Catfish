ca8efe4e3ee386b998ad7f91e9a5ddaba97283e6
Isuru Wickramasinghe <iwickram@ualberta.ca>
2023-03-13 14:14:16 -0600

    Carousel completed

---
+++ Catfish/Catfish.Web.Extensions/Constants/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatfishWebExtensions.Constants
+{
+    public class Enums
+    {
+        public enum eCarouselLayout
+        {
+            Slide = 0,
+            Overlay,
+            SideBySide
+        }
+    }
+}
