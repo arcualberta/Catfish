08b6dc8857c075fe87e515fee66b7f5726b34c78
Isuru Wickramasinghe <iwickram@ualberta.ca>
2023-04-12 20:36:27 -0600

    fixed few UI bugs

--- Catfish/Catfish.Web.Extensions/Models/Sites/Headers/LowerLevel.cs
+++ Catfish/Catfish.Web.Extensions/Models/Sites/Headers/LowerLevel.cs
@@ -11,8 +11,14 @@ namespace CatfishWebExtensions.Models.Sites.Headers
         [Field(Title = "Header Logo")]
         public ImageField Logo { get; set; }
 
+        [Field(Title = "Title")]
+        public TextField Title { get; set; }
+
         [Field(Title = "Header Title")]
         public TextField SiteTitle { get; set; }
+
+        [Field(Title = "Site URL")]
+        public TextField SiteURL { get; set; }
 
         [Field(Title = "Css Classes")]
         public TextField CssClasses { get; set; }
