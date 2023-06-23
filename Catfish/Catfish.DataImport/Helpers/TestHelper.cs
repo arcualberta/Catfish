8c84a77d6ab6d85affa029e79acedc8fb2855f47
KamalRanaweera <ranaweer@ualberta.ca>
2023-06-23 10:15:48 -0600

    Added data import command-line project.

---
+++ Catfish/Catfish.DataImport/Helpers/TestHelper.cs
@@ -0,0 +1,58 @@
+﻿using Catfish.API.Repository;
+using Catfish.API.Repository.Services;
+using Catfish.API.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catfish.DataImport.Helpers
+{
+    public class TestHelper
+    {
+        public IServiceProvider Seviceprovider { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="outputPath"></param>
+        /// <returns></returns>
+        public TestHelper()
+        {
+            //Creating a service collection
+            var services = new ServiceCollection();
+
+            //Registering configuration object
+            IConfiguration configuration = new ConfigurationBuilder()
+                .Add(new JsonConfigurationSource() { Path = "appsettings.test.json" })
+                .Build();
+            services.AddSingleton(typeof(IConfiguration), configuration);
+         
+            //Registering application DB Context
+            string dbConnectionString = configuration.GetConnectionString("catfish3");
+            services.AddDbContext<RepoDbContext>(options => options
+                .UseSqlServer(dbConnectionString)
+                );
+
+            //Registering other services
+            //Registering other services
+            services.AddScoped<ISolrService, SolrService>();
+
+            ////services.AddScoped<SolrService>();
+            // Solr services
+            string solrString = configuration.GetSection("SolarConfiguration:solrCore").Value;
+
+            //Creating a service provider and assigning it to the member variable so that it can be used by 
+            //test methods.
+            Seviceprovider = services.BuildServiceProvider();
+
+          
+            
+        }
+
+        public RepoDbContext Db => Seviceprovider.GetService<RepoDbContext>()!;
+        public IConfiguration Configuration => Seviceprovider.GetService<IConfiguration>()!;
+        public ISolrService Solr => Seviceprovider.GetService<ISolrService>()!;
+
+    }
+}
