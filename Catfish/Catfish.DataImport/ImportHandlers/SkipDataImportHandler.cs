3a4c3929e1dda4bfdb6eee643fe9841fc0e1a366
Isurupmw <iwickram@ualberta.ca>
2023-07-14 13:51:56 -0600

    SKIPP data import added

--- Catfish/Catfish.DataImport/ImportHandlers/SkipDataImportHandler.cs
+++ Catfish/Catfish.DataImport/ImportHandlers/SkipDataImportHandler.cs
@@ -4,16 +4,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using CatfishExtensions.DTO.Forms;
 
 namespace Catfish.DataImport.ImportHandlers
 {
     public class SkipDataImportHandler : IImportHnadler
     {
-        private readonly Guid NameId = Guid.Parse("xxx-xxx-xxx");
-        private readonly Guid EmailId = Guid.Parse("xxx-xxx-xxx");
+        string _apiKey = "";
+        private readonly Guid FORM_ID = Guid.Parse("8d9a6bc9-863d-2ee8-ea93-d5544778f090");
+        private readonly Guid NATIONORCOMMUNITY = Guid.Parse("af27cd1f-4df1-fc15-4ef4-286eb2002816");
+        private readonly Guid NAME = Guid.Parse("77ef4194-c2bf-686c-a05d-e3a63ab53750");
+        private readonly Guid EMAIL = Guid.Parse("6c130003-7792-10c5-cdf5-372f8fe237bd");
+        private readonly Guid POSITION = Guid.Parse("b0620da7-6bf1-5c9c-b70d-b96aadeb0413");
+        private readonly Guid FACULTY = Guid.Parse("93f55bd0-8620-515e-411e-3abb2abf66e4");
+        private readonly Guid ADDITIONAL_FACULTY = Guid.Parse("8f097f5a-d234-53e0-1399-968cf6ea2243");
+        private readonly Guid PRONOUNS = Guid.Parse("bf33b1ab-ebd5-2452-e313-46a3f0f8d5eb");
+        private readonly Guid DISABILITY = Guid.Parse("c27ebf23-8765-3e9f-a0eb-35553d71e9f0");
+        private readonly Guid INDIGENOUS = Guid.Parse("3138944e-82e7-3ff0-831a-b17cc2c950fe");
+        private readonly Guid COMMUNITIESNATIONSORGANIZATIONS = Guid.Parse("be9ebb54-2971-0267-80b2-3d963b56b8a4");
+        private readonly Guid LOCATION = Guid.Parse("a5582c7f-5c0b-b373-32ee-ad8584abe106");
+        private readonly Guid ETHNICITY = Guid.Parse("71ae6003-556c-2ed0-d197-ff382ce75c45");
+        private readonly Guid THEME = Guid.Parse("dca2335d-3c60-8785-6c07-c9e9079aa5c8");
+        private readonly Guid GENDER_IDENTITY = Guid.Parse("f503da45-f9ec-6942-269d-45cdea413f84");
+        private readonly Guid CURRENTPROJECTS = Guid.Parse("020e9f66-911d-55dd-3eff-3bb3aec182ee");
+        private readonly Guid INITIATION = Guid.Parse("03d1ffdd-0736-d8c8-418f-58691f22c19b");
+        private readonly Guid ROLES = Guid.Parse("0b89ad9f-7a76-7594-b932-9a20bfa44b4b");
+        private readonly Guid WEBSITELINKS = Guid.Parse("0b559321-498d-95b3-0734-2ae9ae876b93");
+        private readonly Guid ADDITIONAL_KEYWORDS = Guid.Parse("aeaf264c-06e0-7bec-1073-832cbdb88bca");
+        //private readonly Guid SOCIALMEDIA = Guid.Parse("");
+        private readonly Guid ADITIONAL_CONTACT = Guid.Parse("260bedf2-ef68-c0be-be4b-e0e98ddcaf65");
+        private readonly string TIMESTAMP = "TimeStamp_dt";
+
+
         public void Execute()
         {
-            throw new NotImplementedException();
+            int rowCount = 1;
+            bool headerRow = true;
+            string[] colHeadings = null;
+
+            foreach (RowData row in ReadGoogleSheet())
+            {
+                if (row.Values.Count == 0 || string.IsNullOrEmpty(row.Values[0].FormattedValue))
+                    continue;
+
+                if (headerRow)
+                {
+                    colHeadings = row.Values.Select(x => x.FormattedValue).ToArray();
+                    headerRow = false;
+                    continue;
+                }
+
+
+                for (int i = 0; i < row.Values.Count; ++i)
+                {
+                    string colHeading = colHeadings[i];
+                    string colValue = row.Values.ElementAt(i).FormattedValue;
+
+                    if (string.IsNullOrEmpty(colValue))
+                        continue;
+
+                    string colValueString = "";
+
+                    if (colHeading == "Timestamp")
+                    {
+                        colValueString = "Timestamp";
+
+                    }
+                    else if(colHeading == "Email Address")
+                    {
+                        colValueString = "Email Address";
+                    }
+                }
+            }
+
+        }
+        public List<RowData> ReadGoogleSheet()
+        {
+            //https://docs.google.com/spreadsheets/d/e/2PACX-1vSPTFgPPcCiCngUPXFE8PdsOgxg7Xybq91voXFxHMFd4JpjUIZGLj7U_piRJZV4WZx3YEW31Pln7XV4/pubhtml => this is my own copy
+            String spreadsheetId = "1x6CeEfZiZcGxtnmkoluaZ6GjUhSmutf5GjwjI6dMOyw"; // "1m -oYJH-15DbqhE31dznAldB-gz75BJu1XAV5p5WJwxo";//==>google sheet Id
+            String ranges = "A1:AC"; // "A2:AC";// read from col A to AI, starting 2rd row
+
+            SheetsService sheetsService = new SheetsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = GetCredential(),
+                ApplicationName = "Google-Sheets",
+                ApiKey = _apiKey
+            });
+
+
+            bool includeGridData = true;
+
+            SpreadsheetsResource.GetRequest request = sheetsService.Spreadsheets.Get(spreadsheetId);
+            request.Ranges = ranges;
+            request.IncludeGridData = includeGridData;
+
+
+            // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+            Google.Apis.Sheets.v4.Data.Spreadsheet response = request.Execute(); //await request.ExecuteAsync();
+
+
+            // Read all the rows
+            var values = response.Sheets[0].Data.Select(d => d).ToList();
+
+            List<RowData> rows = values[0].RowData.ToList();
+
+            return rows;
+        }
+        public static UserCredential GetCredential()
+        {
+            // TODO: Change placeholder below to generate authentication credentials. See:
+            // https://developers.google.com/sheets/quickstart/dotnet#step_3_set_up_the_sample
+            //
+            // Authorize using one of the following scopes:
+            //     "https://www.googleapis.com/auth/drive"
+            //     "https://www.googleapis.com/auth/drive.file"
+            //     "https://www.googleapis.com/auth/drive.readonly"
+            //     "https://www.googleapis.com/auth/spreadsheets"
+            //     "https://www.googleapis.com/auth/spreadsheets.readonly"
+            return null;
         }
     }
 }
--- Catfish/DataProcessing/SkippDataProcessing.cs
+++ Catfish/DataProcessing/SkippDataProcessing.cs
@@ -1,4 +1,6 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿using Catfish.API.Repository.Solr;
+using Catfish.Test.Helpers;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
@@ -12,30 +14,35 @@ namespace DataProcessing
 {
     public class SkippDataProcessing
     {
-        string _apiKey = "";
-        public readonly Guid FORM_ID = Guid.Parse("8d9a6bc9-863d-2ee8-ea93-d5544778f090");
-        public readonly Guid NATIONORCOMMUNITY = Guid.Parse("af27cd1f -4df1-fc15-4ef4-286eb2002816");
-        public readonly Guid NAME = Guid.Parse("77ef4194-c2bf-686c-a05d-e3a63ab53750");
-        public readonly Guid EMAIL = Guid.Parse("6c130003-7792-10c5-cdf5-372f8fe237bd");
-        public readonly Guid POSITION = Guid.Parse("b0620da7-6bf1-5c9c-b70d-b96aadeb0413");
-        public readonly Guid FACULTY = Guid.Parse("93f55bd0-8620-515e-411e-3abb2abf66e4");
-        public readonly Guid ADDITIONAL_FACULTY = Guid.Parse("8f097f5a-d234-53e0-1399-968cf6ea2243");
-        public readonly Guid PRONOUNS = Guid.Parse("bf33b1ab-ebd5-2452-e313-46a3f0f8d5eb");
-        public readonly Guid DISABILITY = Guid.Parse("c27ebf23-8765-3e9f-a0eb-35553d71e9f0");
-        public readonly Guid INDIGENOUS = Guid.Parse("3138944e-82e7-3ff0-831a-b17cc2c950fe_ts");
-        public readonly Guid COMMUNITIESNATIONSORGANIZATIONS = Guid.Parse("be9ebb54-2971-0267-80b2-3d963b56b8a4_ss");
-        public readonly Guid LOCATION = Guid.Parse("a5582c7f-5c0b-b373-32ee-ad8584abe106_ts");
-        public readonly Guid ETHNICITY = Guid.Parse("71ae6003-556c-2ed0-d197-ff382ce75c45_ts");
-        public readonly Guid THEME = Guid.Parse("dca2335d-3c60-8785-6c07-c9e9079aa5c8_ts");
-        public readonly Guid GENDER_IDENTITY = Guid.Parse("f503da45-f9ec-6942-269d-45cdea413f84_ss");
-        public readonly Guid CURRENTPROJECTS = Guid.Parse("020e9f66-911d-55dd-3eff-3bb3aec182ee_ts");
-        public readonly Guid INITIATION = Guid.Parse("03d1ffdd-0736-d8c8-418f-58691f22c19b_ts");
-        public readonly Guid ROLES = Guid.Parse("0b89ad9f-7a76-7594-b932-9a20bfa44b4b_ts");
-        public readonly Guid WEBSITELINKS = Guid.Parse("0b559321-498d-95b3-0734-2ae9ae876b93_ts");
-        public readonly Guid ADDITIONAL_KEYWORDS = Guid.Parse("aeaf264c-06e0-7bec-1073-832cbdb88bca_ts");
-        public readonly Guid SOCIALMEDIA = Guid.Parse("");
-        public readonly Guid ADITIONAL_CONTACT = Guid.Parse("260bedf2-ef68-c0be-be4b-e0e98ddcaf65");
-
+        public readonly TestHelper _testHelper;
+        string _apiKey = "AIzaSyBb0mHe17jNMZbI4Ydei57t00ZpUvbvUoY";
+        private readonly Guid FORM_ID = Guid.Parse("8d9a6bc9-863d-2ee8-ea93-d5544778f090");
+        private readonly Guid NATION_OR_COMMUNITY = Guid.Parse("af27cd1f-4df1-fc15-4ef4-286eb2002816");
+        private readonly Guid NAME = Guid.Parse("77ef4194-c2bf-686c-a05d-e3a63ab53750");
+        private readonly Guid EMAIL = Guid.Parse("6c130003-7792-10c5-cdf5-372f8fe237bd");
+        private readonly Guid POSITION = Guid.Parse("b0620da7-6bf1-5c9c-b70d-b96aadeb0413");
+        private readonly Guid FACULTY = Guid.Parse("93f55bd0-8620-515e-411e-3abb2abf66e4");
+        private readonly Guid ADDITIONAL_FACULTY = Guid.Parse("8f097f5a-d234-53e0-1399-968cf6ea2243");
+        private readonly Guid PRONOUNS = Guid.Parse("bf33b1ab-ebd5-2452-e313-46a3f0f8d5eb");
+        private readonly Guid DISABILITY = Guid.Parse("c27ebf23-8765-3e9f-a0eb-35553d71e9f0");
+        private readonly Guid INDIGENOUS = Guid.Parse("3138944e-82e7-3ff0-831a-b17cc2c950fe");
+        private readonly Guid COMMUNITIES_NATIONS_ORGANIZATIONS = Guid.Parse("be9ebb54-2971-0267-80b2-3d963b56b8a4");
+        private readonly Guid LOCATION = Guid.Parse("a5582c7f-5c0b-b373-32ee-ad8584abe106");
+        private readonly Guid ETHNICITY = Guid.Parse("71ae6003-556c-2ed0-d197-ff382ce75c45");
+        private readonly Guid THEME = Guid.Parse("dca2335d-3c60-8785-6c07-c9e9079aa5c8");
+        private readonly Guid GENDER_IDENTITY = Guid.Parse("f503da45-f9ec-6942-269d-45cdea413f84");
+        private readonly Guid CURRENT_PROJECTS = Guid.Parse("020e9f66-911d-55dd-3eff-3bb3aec182ee");
+        private readonly Guid INITIATION = Guid.Parse("03d1ffdd-0736-d8c8-418f-58691f22c19b");
+        private readonly Guid ROLES = Guid.Parse("0b89ad9f-7a76-7594-b932-9a20bfa44b4b");
+        private readonly Guid WEBSITE_LINKS = Guid.Parse("0b559321-498d-95b3-0734-2ae9ae876b93");
+        private readonly Guid ADDITIONAL_KEYWORDS = Guid.Parse("aeaf264c-06e0-7bec-1073-832cbdb88bca");
+        //private readonly Guid SOCIAL_MEDIA = Guid.Parse("");
+        private readonly Guid ADITIONAL_CONTACT = Guid.Parse("260bedf2-ef68-c0be-be4b-e0e98ddcaf65");
+        private readonly string TIMESTAMP = "TimeStamp_dt";
+        public SkippDataProcessing()
+        {
+            _testHelper = new TestHelper();
+        }
         [Fact]
         public void ImportData()
         {
@@ -43,11 +50,125 @@ namespace DataProcessing
             bool headerRow = true;
             string[] colHeadings = null;
 
+            var solrService = _testHelper.Solr;
+
+            List<SolrDoc> solrDocs = new List<SolrDoc>();
+
+            foreach (RowData row in ReadGoogleSheet())
+            {
+                if (row.Values.Count == 0 || string.IsNullOrEmpty(row.Values[0].FormattedValue))
+                    continue;
+
+                if (headerRow)
+                {
+                    colHeadings = row.Values.Select(x => x.FormattedValue).ToArray();
+                    headerRow = false;
+                    continue;
+                }
+
+                SolrDoc solrDoc = new SolrDoc();
+                solrDoc.AddId(Guid.NewGuid().ToString());
+                for (int i = 0; i < row.Values.Count; ++i)
+                {
+                    string colHeading = colHeadings[i];
+                    string colValue = row.Values.ElementAt(i).FormattedValue;
+
+                    if (string.IsNullOrEmpty(colValue))
+                        continue;
+
+                    if (colHeading == "Timestamp")
+                    {
+                        solrDoc.AddField(TIMESTAMP, colValue);
+
+                    }
+                    else if (colHeading == "Email Address")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{EMAIL}_t", colValue);
+                    }
+                    else if (colHeading == "Name")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{NAME}_ts", colValue);
+                    }
+                    else if (colHeading == "Additional Contact Information")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{ADITIONAL_CONTACT}_ts", colValue);
+                    }
+                    else if (colHeading == "Position")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{POSITION}_t", colValue);
+                    }
+                    else if (colHeading == "Faculty or Organizational Affiliation")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{FACULTY}_t", colValue);
+                    }
+                    else if (colHeading == "Additional Faculties or Organizations")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{ADDITIONAL_FACULTY}_ts", colValue);
+                    }
+                    else if (colHeading == "Pronouns")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{PRONOUNS}_t", colValue);
+                    }
+                    else if (colHeading == "Gender identity")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{GENDER_IDENTITY}_t", colValue);
+                    }
+                    else if (colHeading == "Indigenous Self-Declaration")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{INDIGENOUS}_t", colValue);
+                    }
+                    else if (colHeading == "Indigenous Nation and/or Community")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{NATION_OR_COMMUNITY}_t", colValue);
+                    }
+                    else if (colHeading == "Ethnicity")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{ETHNICITY}_t", colValue);
+                    }
+                    else if (colHeading == "Do you identify as living with disability")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{DISABILITY}_t", colValue);
+                    }
+                    else if (colHeading == "Where are they located")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{LOCATION}_ts", colValue);
+                    }
+                    else if (colHeading == "What theme(s) does your research fall under")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{THEME}_ts", colValue);
+                    }
+                    else if (colHeading == "Please briefly describe your current or recent research project(s)")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{CURRENT_PROJECTS}_ts", colValue);
+                    }
+                    else if (colHeading == "Research keywords")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{ADDITIONAL_KEYWORDS}_ts", colValue);
+                    }
+                    else if (colHeading == "Who initiated your research project")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{INITIATION}_ts", colValue);
+                    }
+                    else if (colHeading == "Do you have links to a professional website you would like to share")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{WEBSITE_LINKS}_ts", colValue);
+                    }
+                    else if (colHeading == "What role do community members play in your project")
+                    {
+                        solrDoc.AddField($"data_{FORM_ID}_{ROLES}_ts", colValue);
+                    }
+                    
+
+                }
+                solrDocs.Add(solrDoc);
+                solrService.Index(solrDocs);
+                solrService.CommitAsync();
+            }
         }
         public List<RowData> ReadGoogleSheet()
         {
             //https://docs.google.com/spreadsheets/d/e/2PACX-1vSPTFgPPcCiCngUPXFE8PdsOgxg7Xybq91voXFxHMFd4JpjUIZGLj7U_piRJZV4WZx3YEW31Pln7XV4/pubhtml => this is my own copy
-            String spreadsheetId = "1x6CeEfZiZcGxtnmkoluaZ6GjUhSmutf5GjwjI6dMOyw"; // "1m -oYJH-15DbqhE31dznAldB-gz75BJu1XAV5p5WJwxo";//==>google sheet Id
+            String spreadsheetId = "1A4BegCI9mqSkBCedJKVUBueW6u7hG6OLS0KszK5gLLE"; // "1m -oYJH-15DbqhE31dznAldB-gz75BJu1XAV5p5WJwxo";//==>google sheet Id
             String ranges = "A1:AC"; // "A2:AC";// read from col A to AI, starting 2rd row
 
             SheetsService sheetsService = new SheetsService(new BaseClientService.Initializer
