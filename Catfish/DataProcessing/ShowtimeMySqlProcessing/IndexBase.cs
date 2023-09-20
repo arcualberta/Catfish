977c8f00f86e3ecee1f656f54438c8135ceda199
KamalRanaweera <ranaweer@ualberta.ca>
2023-09-20 17:45:17 -0600

    Ignores duplicate cast values.

---
+++ Catfish/DataProcessing/ShowtimeMySqlProcessing/IndexBase.cs
@@ -0,0 +1,89 @@
+﻿using Catfish.Test.Helpers;
+using CliWrap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessing.ShowtimeMySqlProcessing
+{
+    public class IndexBase
+    {
+        protected readonly TestHelper _testHelper = new ();
+        public delegate Task<bool> FileProcessingDelegate(string fileName, string? outputFolder = null, int? fileIndex = null, string? errorLogFile = null);
+
+
+        protected async Task<bool> ExportTextFromInserts(string sqlFile, string modelName, string outFile)
+        {
+            int lineCount = 0;
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(sqlFile))
+                {
+                    using (StreamWriter writer = File.CreateText(outFile))
+                    {
+                        string line = string.Empty;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            ++lineCount;
+
+                            if (!line.StartsWith($"INSERT INTO `{modelName}` VALUES "))
+                                continue;
+
+                            var valueStr = line.Substring(line.IndexOf("VALUES (") + 8);
+                            valueStr = valueStr.TrimEnd(new char[] { ')', ';' });
+                            string[] values = valueStr.Split("),(");
+
+                            foreach (var val in values)
+                                await writer.WriteLineAsync(val);
+                        }
+
+                        writer.Close();
+                    }
+
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Export Text Error on line {lineCount} in {sqlFile}\n", ex);
+            }
+
+            return true;
+        }
+
+        protected async Task IngestTextDataFile(string txtDataFile, string database)
+        {
+            string host = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Host").Value;
+            string user = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:User").Value;
+            string password = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Password").Value;
+            string port = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Port").Value;
+
+            try
+            {
+                await Cli.Wrap("mysqlimport.exe")
+                    .WithArguments(new[] {
+                    "--protocol=tcp",
+                    $"--host={host}",
+                    $"--user={user}",
+                    $"--password={password}",
+                    $"--port={port}",
+                    "--default-character-set=utf8",
+                    "--fields-optionally-enclosed-by='",
+                    "--fields-terminated-by=,",
+                    "--lines-terminated-by=\\r\\n",
+                    database,
+                    txtDataFile
+                    })
+                    .ExecuteAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- Catfish/DataProcessing/ShowtimeMySqlProcessing/IndexShowtimes.cs
+++ Catfish/DataProcessing/ShowtimeMySqlProcessing/IndexShowtimes.cs
@@ -6,12 +6,10 @@ using Google.Protobuf.WellKnownTypes;
 
 namespace DataProcessing.ShowtimeMySqlProcessing
 {
-    public class IndexShowtimes
+    public class IndexShowtimes : IndexBase
     {
-        private readonly TestHelper _testHelper = new TestHelper();
 
         public delegate void CreateMySqlModel(string concatenatedFieldValues);
-        public delegate Task<bool> FileProcessingDelegate(string fileName, string? outputFolder = null, int? fileIndex = null, string? errorLogFile = null);
 
         protected void ProcessSqlInserts(string sourceSqlDumpFileName, string modelName, CreateMySqlModel createInstance)
         {
@@ -441,45 +439,10 @@ namespace DataProcessing.ShowtimeMySqlProcessing
 
         protected async Task<bool> ExportTextFromInserts(string sqlFile, string? outputFolder, int? fileIndex, string? ___)
         {
-            int lineCount = 0;
+            string outFile = $"showtime.txt_{string.Format("{0:d3}", fileIndex)}";
+            outFile = Path.Combine(outputFolder!, outFile);
 
-            try
-            {
-                string outFile = $"showtime.txt_{string.Format("{0:d3}", fileIndex)}";
-                outFile = Path.Combine(outputFolder!, outFile);
-
-                using (StreamReader sr = File.OpenText(sqlFile))
-                {
-                    using (StreamWriter writer = File.CreateText(outFile))
-                    {
-                        string line = string.Empty;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            ++lineCount;
-
-                            if (!line.StartsWith($"INSERT INTO `Showtime` VALUES "))
-                                continue;
-
-                            var valueStr = line.Substring(line.IndexOf("VALUES (") + 8);
-                            valueStr = valueStr.TrimEnd(new char[] { ')', ';' });
-                            string[] values = valueStr.Split("),(");
-
-                            foreach(var val in values)
-                                await writer.WriteLineAsync(val);
-                        }
-
-                        writer.Close();
-                    }
-
-                    sr.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Export Text Error on line {lineCount} in {sqlFile}\n", ex);
-            }
-
-            return true;
+            return await ExportTextFromInserts(sqlFile, "Showtime", outFile);
         }
 
         #endregion
@@ -511,36 +474,8 @@ namespace DataProcessing.ShowtimeMySqlProcessing
 
         protected async Task<bool> IngestTextDataFile(string txtDataFile, string? _, int? __, string? ___)
         {
-            string host = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Host").Value;
-            string user = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:User").Value;
-            string password = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Password").Value;
-            string port = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Port").Value;
             string database = _testHelper.Configuration.GetSection("OldShowtimeDataIngestion:MySqlServer:Database").Value;
-
-            try
-            {
-                await Cli.Wrap("mysqlimport.exe")
-                    .WithArguments(new[] {
-                    "--protocol=tcp",
-                    $"--host={host}",
-                    $"--user={user}",
-                    $"--password={password}",
-                    $"--port={port}",
-                    "--default-character-set=utf8",
-                    "--fields-optionally-enclosed-by='",
-                    "--fields-terminated-by=,",
-                    "--lines-terminated-by=\\r\\n",
-                    database,
-                    txtDataFile
-                    })
-                    .ExecuteAsync();
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-         
+            await IngestTextDataFile(txtDataFile, database);       
             return true;
         }
         #endregion
@@ -701,9 +636,8 @@ namespace DataProcessing.ShowtimeMySqlProcessing
             doc.AddField("official_site_s", movie.URL);
             doc.AddField("star_rating_d", movie.Star_Rating.HasValue ? (decimal)movie.Star_Rating.Value : null);
 
-            //Note: we keep one-one correspondence with the Cast_Name and Cast_Type in the two array fields below
-            doc.AddField("casts_ts",        casts.Where(x => x.Cast_Name!= null || x.Cast_Type != null).Select(x => x.Cast_Name == null ? "-" : x.Cast_Name).ToArray());
-            doc.AddField("cast_types_ts",   casts.Where(x => x.Cast_Name != null || x.Cast_Type != null).Select(x => x.Cast_Type == null ? "-" : x.Cast_Type).ToArray());
+            doc.AddField("casts_ts",        casts.Select(x => x.Cast_Name == null ? "" : x.Cast_Name).Distinct().ToArray());
+            doc.AddField("cast_types_ts",   casts.Select(x => x.Cast_Type == null ? "" : x.Cast_Type).Distinct().ToArray());
 
             //set the union of move.genre and genre.Movie_Genre array as the genres_ts field's value
             string[] genre_vals = string.IsNullOrEmpty(movie.Genre)
