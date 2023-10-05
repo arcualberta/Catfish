50f56a220bad2e9dd030a90ff390658dc5d4b507
KamalRanaweera <ranaweer@ualberta.ca>
2023-10-04 18:11:13 -0600

    Updated FormProxy to use CrudProxy from arc-foundation npm package.

--- catfish-ui/src/api/formProxy.ts
+++ catfish-ui/src/api/formProxy.ts
@@ -1,8 +1,8 @@
 import { default as config } from "@/appsettings";
 
-import {CrudProxy} from '@arc/arc-foundation'
+import {api} from '@arc/arc-foundation'
 
-export class FormProxy extends CrudProxy {   
+export class FormProxy extends api.CrudProxy {   
     constructor(jwtToken?: string) {
         super(`${config.dataRepositoryApiRoot}/api/forms`, jwtToken)
     }
